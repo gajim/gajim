60fdfc66e5a91b9ec78220ac3cdd3ddb2ace959e
Yann Leboulanger <asterix@lagaule.org>
2004-07-08 19:56:30 +0000

    new indentation

--- common/hub.py
+++ common/hub.py
@@ -24,45 +24,45 @@ import common.thread
 """ Hub definitions """
 
 class GajimHub:
-    def __init__(self):
-        self.queues = {}
-        """ {event1:[queue1, queue2]} """
-        self.events = {}
-        self.queueIn = self.newQueue('in', 100)
-    # END __init__
+	def __init__(self):
+		self.queues = {}
+		# {event1:[queue1, queue2]}
+		self.events = {}
+		self.queueIn = self.newQueue('in', 100)
+	# END __init__
 
-    def newQueue(self, name, size):
-        """ Creates a new queue """
-        qu = Queue.Queue(size)
-        self.queues[name] = qu
-        return qu
-    # END newQueue
-	
-    def newPlugin(self, name):
-        """Creates a new Plugin """
-        qu = self.newQueue(name, 100)
-        pl = common.plugin.GajimPlugin(name, qu, self.queueIn)
-        return pl
-    # END newPlugin
+	def newQueue(self, name, size):
+		""" Creates a new queue """
+		qu = Queue.Queue(size)
+		self.queues[name] = qu
+		return qu
+	# END newQueue
 
-    def register(self, name, event):
-        """ Records a plugin from an event """
-        qu = self.queues[name]
-	if self.events.has_key(event) :
-		self.events[event].append(qu)
-	else :
-		self.events[event] = [qu]
-    # END register
+	def newPlugin(self, name):
+		"""Creates a new Plugin """
+		qu = self.newQueue(name, 100)
+		pl = common.plugin.GajimPlugin(name, qu, self.queueIn)
+		return pl
+	# END newPlugin
 
-    def sendPlugin(self, event, con, data):
-        """ Sends an event to registered plugins
+	def register(self, name, event):
+		""" Records a plugin from an event """
+		qu = self.queues[name]
+		if self.events.has_key(event) :
+			self.events[event].append(qu)
+		else :
+			self.events[event] = [qu]
+	# END register
+
+	def sendPlugin(self, event, con, data):
+		""" Sends an event to registered plugins
 		NOTIFY : ('NOTIFY', (user, status, message))
 		MSG : ('MSG', (user, msg))
 		ROSTER : ('ROSTER', {jid:{'status':_, 'name':_, 'show':_, 'groups':[], 'online':_, 'ask':_, 'sub':_} ,jid:{}})
 		SUBSCRIBED : ('SUBSCRIBED', {'jid':_, 'nom':_, 'server':_, 'resource':_, 'status':_, 'show':_})"""
 
-        if self.events.has_key(event):
-            for i in self.events[event]:
-                i.put((event, con, data))
-    # END sendPlugin
+		if self.events.has_key(event):
+			for i in self.events[event]:
+				i.put((event, con, data))
+	# END sendPlugin
 # END GajimHub
