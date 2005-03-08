e6b04e8159005c1b75f34494b8eb64748143f572
Yann Leboulanger <asterix@lagaule.org>
2005-03-08 18:06:18 +0000

    show the account modification window when we launch gajim and no account configured

--- plugins/gtkgui/gtkgui.py
+++ plugins/gtkgui/gtkgui.py
@@ -2728,6 +2728,10 @@ class roster_window:
 
 		self.hidden_lines = self.plugin.config['hiddenlines'].split('\t')
 		self.draw_roster()
+		if len(self.plugin.accounts) == 0:
+			self.plugin.windows['accounts_window'] = Accounts_window(self.plugin)
+			self.plugin.windows['account_modification_window'] = \
+				Account_modification_window(self.plugin)
 
 class systrayDummy:
 	"""Class when we don't want icon in the systray"""
