d9cf8bb1a02ce051505b3eacaef14a831ee984f7
Yann Leboulanger <asterix@lagaule.org>
2005-05-06 19:56:48 +0000

    remove unneeded code

--- src/config.py
+++ src/config.py
@@ -119,9 +119,6 @@ class Preferences_window:
 		icon_string = model[active][0]
 		gajim.config.set('iconset', icon_string)
 		self.plugin.roster.reload_jabber_state_images()
-		for a in gajim.connections: # FIXME: so this can be removed?? [see roster_window.py update_state_images call in reload_jabber_state_images()]
-			for w in self.plugin.windows[a]['gc'].values():
-				w.update_state_images()
 		self.plugin.save_config()
 	
 	def on_roster_widget_color_set(self, widget, text):
