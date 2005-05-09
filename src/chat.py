5bb1df8a1938a2bce954cdbf0b62353a21559193
Nikos Kouremenos <kourem@gmail.com>
2005-05-09 10:20:08 +0000

    for /mebalbla u want a space after /me right? :)

--- src/chat.py
+++ src/chat.py
@@ -644,7 +644,7 @@ class Chat:
 
 		if kind == 'status':
 			print_all_special = True
-		elif text.split()[0] == '/me':
+		elif text.startswith('/me '):
 			text = name + text[3:]
 			print_all_special = True
 
