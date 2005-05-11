c22e2c5f7a058895d34aeae7f95cd1e306a9b6e9
Yann Leboulanger <asterix@lagaule.org>
2005-05-11 15:14:43 +0000

    bugfix: when we have an error, we sane an error to the GUI, not a notify

--- src/common/connection.py
+++ src/common/connection.py
@@ -264,9 +264,8 @@ class Connection:
 			if errcode == '409':	#conflict :	Nick Conflict
 				self.dispatch('ERROR', errmsg)
 			else:
-				self.dispatch('NOTIFY', (prs.getFrom().getStripped(), 'error', \
-					errmsg, prs.getFrom().getResource(), prio, keyID, None, None, \
-					None,  None, None, None))
+				self.dispatch('ERROR_ANSWER', (prs.getFrom().getStripped(), errmsg,
+																					errcode))
 		if ptype == 'available' or ptype == 'unavailable':
 			gajim.logger.write('status', status, prs.getFrom().getStripped(), show)
 			self.dispatch('NOTIFY', (prs.getFrom().getStripped(), show, status, \
