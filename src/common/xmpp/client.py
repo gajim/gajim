69a9f6f08acf2521fc2f1a786a709ee0927b65e4
Nikos Kouremenos <kourem@gmail.com>
2005-06-02 18:15:29 +0000

    [xmpppy] some jabber servers have ssl on 443 (eg. jabber80.com) so check for that too, and return ssl and not tls as string for return value in connect()

--- src/common/xmpp/client.py
+++ src/common/xmpp/client.py
@@ -156,9 +156,9 @@ class CommonClient:
         if not connected: return
         self._Server,self._Proxy=server,proxy
         self.connected='tcp'
-        if self.Connection.getPort()==5223:
+        if self.Connection.getPort() in (5223, 443):
             transports.TLS().PlugIn(self,now=1)
-            self.connected='tls'
+            self.connected='ssl'
         dispatcher.Dispatcher().PlugIn(self)
         while self.Dispatcher.Stream._document_attrs is None: self.Process(1)
         if self.Dispatcher.Stream._document_attrs.has_key('version') and self.Dispatcher.Stream._document_attrs['version']=='1.0':
