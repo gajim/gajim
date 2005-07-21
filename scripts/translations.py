d54c388ca06452b98b7a16e3666777b76d6a3112
Dimitur Kirov <dkirov@gmail.com>
2005-07-21 18:51:23 +0000

    Added scripts/gajim-remote.py

--- scripts/translations.py
+++ scripts/translations.py
@@ -30,7 +30,8 @@ def show_help():
 
 def update_pot():
 	os.system('intltool-extract --type=gettext/glade ../src/gtkgui.glade')
-	os.system('xgettext -k_ -kN_ -o ../gajim.pot ../src/*.py ../src/common/*.py ../src/gtkgui.glade.h')
+	os.system('xgettext -k_ -kN_ -o ../gajim.pot ../src/*.py ../src/common/*.py \
+		../src/gtkgui.glade.h ../scripts/gajim-remote.py')
 	print 'gajim.pot was updated successfully'
 
 if __name__ == '__main__':
