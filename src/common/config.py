c84973327d27fde99e40dbfb63a73902dd0a6b92
Dimitur Kirov <dkirov@gmail.com>
2005-08-01 15:00:47 +0000

    property 'notify_on_file_complete'

--- src/common/config.py
+++ src/common/config.py
@@ -120,6 +120,7 @@ class Config:
 		'send_receive_chat_state_notifications': [opt_bool, True],
 		'print_ichat_every_foo_minutes': [opt_int, 5], # default is every 5 minutes
 		'confirm_close_muc': [opt_bool, True], # confirm closing MUC window
+		'notify_on_file_complete': [opt_bool, True], # notif. on file complete
 	}
 
 	__options_per_key = {
