c944035684dd2e727dbc0f42aba57e830c107c1a
Nikos Kouremenos <kourem@gmail.com>
2005-08-08 14:56:25 +0000

    [nicfit] jep0070 now works as a charm

--- src/gajim.py
+++ src/gajim.py
@@ -204,7 +204,7 @@ class Interface:
 	def handle_event_http_auth(self, account, data):
 		#('HTTP_AUTH', account, (method, url, iq_obj))
 		dialog = dialogs.ConfirmationDialog(_('HTTP (%s) Authorization for %s') \
-			% (array[0], array[1]), _('Do you accept this request?'))
+			% (data[0], data[1]), _('Do you accept this request?'))
 		if dialog.get_response() == gtk.RESPONSE_OK:
 			answer = 'yes'
 		else:
@@ -1001,6 +1001,7 @@ class Interface:
 			self.handle_event_file_send_error)
 		con.register_handler('STANZA_ARRIVED', self.handle_event_stanza_arrived)
 		con.register_handler('STANZA_SENT', self.handle_event_stanza_sent)
+		con.register_handler('HTTP_AUTH', self.handle_event_http_auth)
 
 	def process_connections(self):
 		try:
