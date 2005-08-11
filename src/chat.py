128f6e1e67bb2d9dc22d9d8294070d71ede44877
Nikos Kouremenos <kourem@gmail.com>
2005-08-11 17:59:56 +0000

    only change trayicon if we use tabbedui

--- src/chat.py
+++ src/chat.py
@@ -1144,7 +1144,7 @@ class Chat:
 				if text.find(self.nicks[jid]) == -1:
 					return
 			self.nb_unread[jid] += 1
-			if self.plugin.systray_enabled and not gajim.config.get('autopopup'):
+			if self.plugin.systray_enabled and gajim.config.get('usetabbedchat'):
 				# if we 'pop it up' when we receive * [n] is enough
 				self.plugin.systray.add_jid(jid, self.account)
 			self.redraw_tab(jid)
