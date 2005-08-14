332bffc29fe7c3a56936a87472a6f1d3907f674b
Dimitur Kirov <dkirov@gmail.com>
2005-08-14 13:46:15 +0000

    changed Q__ to Q_

--- src/common/helpers.py
+++ src/common/helpers.py
@@ -87,7 +87,7 @@ def get_uf_show(show):
 	elif show == 'requested':
 		uf_show = Q_('?contact has status:Unknown')
 	else:
-		uf_show = Q__('?contact has status:Has errors')
+		uf_show = Q_('?contact has status:Has errors')
 	return unicode(uf_show)
 	
 def get_uf_sub(sub):
