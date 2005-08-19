bf92d33a5fd199b70f43238f77b7f97377144b14
Nikos Kouremenos <kourem@gmail.com>
2005-08-19 11:33:58 +0000

    fix bad strings part2

--- scripts/gajim-remote.py
+++ scripts/gajim-remote.py
@@ -110,8 +110,7 @@ have "sync with global status" option set'), False)
 					_('Show the chat dialog so that you can send message to a \
 contact'), 
 					[
-						#tell nkour that should be JID of the contact
-						('jid', _('jid of the contact that you want to chat \
+						('jid', _('JID of the contact that you want to chat \
 with'),
 							True), 
 						(_('account'), _('if specified, contact is taken from \
@@ -119,16 +118,13 @@ the contact list of this account'), False)
 					]
 				],
 			'send_message':[
-					#OpenPGP key here too (tlel that to nkour)
 					_('Send new message to a contact in the roster. Both OpenPGP \
 key and account are optional. If you want to set only \'account\', without \
-\'pgp key\', just set \'pgp key\' to \'\'.'), 
+\'OpenPGP key\', just set \'OpenPGP key\' to \'\'.'), 
 					[
-						#tell nkour that should be JID of the contact
-						('jid', _('jid of the contact that will receive the \
+						('jid', _('JID of the contact that will receive the \
 message'), True),
 						(_('message'), _('message contents'), True),
-						#tell to nkour it should be OpenPGP key
 						(_('pgp key'), _('if specified, the message will be \
 encrypted using this public key'), False),
 						(_('account'), _('if specified, the message will be \
@@ -138,8 +134,7 @@ sent using this account'), False),
 			'contact_info': [
 					_('Get detailed info on a contact'), 
 					[
-						#JID of the contact (tell that to nkour)
-						('jid', _('jid of the contact'), True)
+						('jid', _('JID of the contact'), True)
 					]
 				]
 			}
@@ -214,8 +209,7 @@ Please specify account for sending the message.') % sys.argv[2])
 			self.service = self.sbus.get_service(SERVICE)
 			interface = self.service.get_object(OBJ_PATH, INTERFACE)
 		else:
-			#say to nkour that this should be Unknown D-Bus version: %s (thanks nebulam)
-			send_error(_('Unknown dbus version: %s') % _version)
+			send_error(_('Unknown D-Bus version: %s') % _version)
 			
 		# get the function asked
 		self.method = interface.__getattr__(self.command)
