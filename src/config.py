bc8a20f907c7cef74c2ce51c0ee525ad796c9b3b
Nikos Kouremenos <kourem@gmail.com>
2005-08-25 20:31:49 +0000

    typo

--- src/config.py
+++ src/config.py
@@ -295,7 +295,7 @@ class PreferencesWindow:
 
 		# sound player
 		player = gajim.config.get('soundplayer')
-		self.xml.get_widget('soundplayer_entry').set_text(command)
+		self.xml.get_widget('soundplayer_entry').set_text(player)
 		if player == '': # only on first time Gajim starts
 			commands = ('esdplay', 'artsplay', 'aplay', 'play')
 			for command in commands:
