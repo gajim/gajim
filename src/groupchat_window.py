0f9f4040002707453f48fee46d0cf33398d84acb
Nikos Kouremenos <kourem@gmail.com>
2005-11-01 12:54:57 +0000

    please use tupples; only use list when need to append later etc

--- src/groupchat_window.py
+++ src/groupchat_window.py
@@ -576,7 +576,7 @@ class GroupchatWindow(chat.Chat):
 				_('You can manage your bookmarks via Actions menu in your roster.'))
 
 	def on_message_textview_key_press_event(self, widget, event):
-		if event.keyval not in [gtk.keysyms.ISO_Left_Tab, gtk.keysyms.Tab]:
+		if event.keyval not in (gtk.keysyms.ISO_Left_Tab, gtk.keysyms.Tab):
 			room_jid = self.get_active_jid()
 			self.last_key_tabs[room_jid] = False
 
