52eac71064f5ef5b315cce58dd597a98f6bfbb1e
Nikos Kouremenos <kourem@gmail.com>
2005-11-20 12:52:17 +0000

    minor

--- src/chat.py
+++ src/chat.py
@@ -825,7 +825,7 @@ class Chat:
 			(event.state & gtk.gdk.MOD1_MASK): # alt + E opens emoticons menu
 			if gajim.config.get('useemoticons'):
 				parent = self.message_textviews[jid]
-				def set_emoticons_menu_position(w, parent=parent):
+				def set_emoticons_menu_position(w, parent = parent):
 					window = parent.get_window(gtk.TEXT_WINDOW_WIDGET)
 					# get the window position
 					origin = window.get_origin()
@@ -834,7 +834,7 @@ class Chat:
 					# get the cursor position
 					cursor = parent.get_iter_location(buf.get_iter_at_mark(buf.get_insert()))
 					cursor =  parent.buffer_to_window_coords(gtk.TEXT_WINDOW_TEXT, cursor.x, cursor.y)
-					return (origin[0] + cursor[0], origin[1] + size[1], 0)
+					return (origin[0] + cursor[0], origin[1] + size[1], True) # push_in True
 				self.emoticons_menu.popup(None, None, set_emoticons_menu_position, 1, 0)
 		elif event.keyval == gtk.keysyms.Page_Down:
 			if event.state & gtk.gdk.SHIFT_MASK: # SHIFT + PAGE DOWN
