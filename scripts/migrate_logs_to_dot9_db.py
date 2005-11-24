0b3cece7a675f70a6705577d9d12547f1ad6716b
Nikos Kouremenos <kourem@gmail.com>
2005-11-24 13:20:53 +0000

    inform about not running gajim while migrating

--- scripts/migrate_logs_to_dot9_db.py
+++ scripts/migrate_logs_to_dot9_db.py
@@ -167,6 +167,10 @@ def visit(arg, dirname, filenames):
 				con.commit()
 
 if __name__ == '__main__':
+	print 'Starting Logs Migration'
+	print '======================='
+	print 'Please do NOT run Gajim until this script is over'
+	print 'For more read http://trac.gajim.org/wiki/MigrateLogToDot9DB'
 	os.path.walk(PATH_TO_LOGS_BASE_DIR, visit, None)
 	f = open(os.path.join(PATH_TO_LOGS_BASE_DIR, 'README'), 'w')
 	f.write('We do not use plain-text files anymore, because they do not scale.\n')
