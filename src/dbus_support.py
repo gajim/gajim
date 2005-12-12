32fbe2729c2cecc3752e1ac8e2a7234dfd4d1ad9
Nikos Kouremenos <kourem@gmail.com>
2005-12-12 14:05:01 +0000

    clean up logic; prevent a tb in Windoz

--- src/dbus_support.py
+++ src/dbus_support.py
@@ -32,21 +32,21 @@ _ = i18n._
 try:
 	import dbus
 	version = getattr(dbus, 'version', (0, 20, 0))
+	supported = True
 except ImportError:
 	version = (0, 0, 0)
-	
-if version >= (0, 41, 0):
-	import dbus.service
-	import dbus.glib # cause dbus 0.35+ doesn't return signal replies without it
-
-supported = True
-if 'dbus' not in globals() and not os.name == 'nt':
-	print _('D-Bus python bindings are missing in this computer')
-	print _('D-Bus capabilities of Gajim cannot be used')
 	supported = False
+	if not os.name == 'nt': # only say that to non Windows users
+		print _('D-Bus python bindings are missing in this computer')
+		print _('D-Bus capabilities of Gajim cannot be used')
+	
 # dbus 0.23 leads to segfault with threads_init()
 if sys.version[:4] >= '2.4' and version[1] < 30:
 	supported = False
+
+if version >= (0, 41, 0):
+	import dbus.service
+	import dbus.glib # cause dbus 0.35+ doesn't return signal replies without it
 
 class SessionBus:
 	'''A Singleton for the DBus SessionBus'''
