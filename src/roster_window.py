8607a9ecad8cd99a4c5ddd5ff15a5a4e325a0d04
Yann Leboulanger <asterix@lagaule.org>
2005-12-14 18:10:55 +0000

    load_iconset return {} if path doesn't exists

--- src/roster_window.py
+++ src/roster_window.py
@@ -2175,10 +2175,7 @@ _('If "%s" accepts this request you will know his or her status.') %jid)
 		if not iconset:
 			iconset = 'dcraven'
 		path = os.path.join(gajim.DATA_DIR, 'iconsets', iconset, '32x32')
-		if os.path.exists(path):
-			self.jabber_state_images['32'] = self.load_iconset(path)
-		else:
-			self.jabber_state_images['32'] = {}
+		self.jabber_state_images['32'] = self.load_iconset(path)
 
 		path = os.path.join(gajim.DATA_DIR, 'iconsets', iconset, '16x16')
 		self.jabber_state_images['16'] = self.load_iconset(path)
