9ac960c08761e2b7a89d6b54ef939e9c62c2dfd8
Nikos Kouremenos <kourem@gmail.com>
2005-12-19 12:22:22 +0000

    add missing header

--- src/common/migrate_logs_to_dot9_db.py
+++ src/common/migrate_logs_to_dot9_db.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python
+## Contributors for this file:
+## - Yann Le Boulanger <asterix@lagaule.org>
+## - Nikos Kouremenos <kourem@gmail.com>
+##
+## Copyright (C) 2003-2004 Yann Le Boulanger <asterix@lagaule.org>
+##                         Vincent Hanquez <tab@snarc.org>
+## Copyright (C) 2005 Yann Le Boulanger <asterix@lagaule.org>
+##                    Vincent Hanquez <tab@snarc.org>
+##                    Nikos Kouremenos <nkour@jabber.org>
+##                    Dimitur Kirov <dkirov@gmail.com>
+##                    Travis Shirk <travis@pobox.com>
+##                    Norman Rasmussen <norman@rasmussen.co.za>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published
+## by the Free Software Foundation; version 2 only.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+
 import os
 import sre
 import sys
