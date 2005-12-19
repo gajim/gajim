dc5a17385f562abeb106e526507fc14f4cad1e46
Nikos Kouremenos <kourem@gmail.com>
2005-12-19 22:07:05 +0000

    add some fixmes for bad strings

--- src/common/config.py
+++ src/common/config.py
@@ -165,6 +165,7 @@ class Config:
 		'show_status_msgs_in_roster': [opt_bool, True, _('If True, Gajim will display the status message, if not empty, for every contact under the contact name in roster window')],
 		'show_avatars_in_roster': [opt_bool, True],
 		'ask_avatars_on_startup': [opt_bool, True, _('If True, Gajim will ask for avatar each contact that did not have an avatar last time or has one cached that is too old.')],
+		#FIXME: remvoe you and make it Gajim will not; and/or his or *her* status messages
 		'print_status_in_chats': [opt_bool, True, _('If False, you will no longer see status line in chats when a contact changes his or her status and/or his status message.')],
 		'log_contact_status_changes': [opt_bool, False],
 		'restored_messages_color': [opt_str, 'grey'],
