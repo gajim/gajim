ba0425e4dd16db788e6227deb1e17514db40bed1
Yann Leboulanger <asterix@lagaule.org>
2005-12-22 07:46:04 +0000

    Jim] re-typo

--- src/common/sleepy.py
+++ src/common/sleepy.py
@@ -27,7 +27,7 @@ from common import gajim
 
 
 STATE_UNKNOWN  = 'OS probably not supported'
-STATE_XA   = 'extented away'
+STATE_XA   = 'extended away'
 STATE_AWAY   = 'away'
 STATE_AWAKE    = 'awake'
 
