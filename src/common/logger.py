815e07771b96b1899ffdcda56e8c5c9aad487a90
Nikos Kouremenos <kourem@gmail.com>
2005-12-26 18:23:57 +0000

    prevent a tb

--- src/common/logger.py
+++ src/common/logger.py
@@ -402,5 +402,11 @@ class Logger:
 			AND kind NOT IN (?, ?)
 			ORDER BY time DESC LIMIT 1
 			''', (jid_id, constants.KIND_STATUS, constants.KIND_GCSTATUS))
-		result = self.cur.fetchone()[0]
+
+		results = self.cur.fetchone()
+		if len(results) > 0:
+			result = results[0]
+		else:
+			result = None
+
 		return result
