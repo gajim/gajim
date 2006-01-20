1fc8805b47a9c582cdd4c261b785f0228738fa15
Travis Shirk <travis@pobox.com>
2006-01-20 02:18:35 +0000

    We now allow closing groupchats with the Esc key

--- src/message_window.py
+++ src/message_window.py
@@ -457,8 +457,7 @@ class MessageWindow:
 				ctrl.set_compact_view(not ctrl.compact_view_current)
 		# Close tab bindings
 		elif event.keyval == gtk.keysyms.Escape: # ESCAPE
-			if ctrl.type_id != message_control.TYPE_GC:
-				self.remove_tab(contact)
+			self.remove_tab(contact)
 		else:
 			# If the active control has a message_textview pass the event to it
 			active_ctrl = self.get_active_control()
