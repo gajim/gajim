0977a93c7ef1fedd5446a8efa5ac2252ba4bbf89
Travis Shirk <travis@pobox.com>
2006-02-03 07:48:10 +0000

    Fixed copyrights

--- src/message_window.py
+++ src/message_window.py
@@ -1,6 +1,15 @@
 ##	message_window.py
 ##
-## Copyright (C) 2005-2006 Travis Shirk <travis@pobox.com>
+## Copyright (C) 2003-2004 Yann Le Boulanger <asterix@lagaule.org>
+##                         Vincent Hanquez <tab@snarc.org>
+## Copyright (C) 2005 Yann Le Boulanger <asterix@lagaule.org>
+##                    Vincent Hanquez <tab@snarc.org>
+##                    Nikos Kouremenos <nkour@jabber.org>
+##                    Dimitur Kirov <dkirov@gmail.com>
+##                    Travis Shirk <travis@pobox.com>
+##                    Norman Rasmussen <norman@rasmussen.co.za>
+## Copyright (C) 2006 Travis Shirk <travis@pobox.com>
+## Copyright (C) 2006 Geobert Quach <geobert@gmail.com>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published
