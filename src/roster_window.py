a3fd1663d05dd8e62b1bda1f50bc58364d593fc8
Nikos Kouremenos <kourem@gmail.com>
2006-02-26 14:33:15 +0000

    add comment; improve varname

--- src/roster_window.py
+++ src/roster_window.py
@@ -2375,11 +2375,12 @@ _('If "%s" accepts this request you will know his or her status.') % jid)
 				if os.path.exists(file):
 					image.set_from_file(file)
 					if pixbuf2 and image.get_storage_type() == gtk.IMAGE_PIXBUF:
-						pix = image.get_pixbuf()
+						# add pixbuf2 on top-left corner of image
+						pixbuf1 = image.get_pixbuf()
 						pixbuf2.composite(pix, 0, 0, pixbuf2.get_property('width'),
 							pixbuf2.get_property('height'), 0, 0, 1.0, 1.0,
 							gtk.gdk.INTERP_HYPER, 255)
-						image.set_from_pixbuf(pix)
+						image.set_from_pixbuf(pixbuf1)
 					break
 		return imgs
 
