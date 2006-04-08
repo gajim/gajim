f30331339476c6323919f42910c7e4719095621d
Nikos Kouremenos <kourem@gmail.com>
2006-04-08 23:00:00 +0000

    fix trasnation.py to produce history manager header and update pos/po

--- scripts/dev/translations.py
+++ scripts/dev/translations.py
@@ -37,6 +37,8 @@ def show_help():
 def update_pot():
 	# create header for glade strings
 	os.system('intltool-extract --type=gettext/glade ../../src/gtkgui.glade')
+	os.system('intltool-extract --type=gettext/glade ../../src/'
+		'history_manager.glade')
 	# update the pot
 	os.system('make -C ../../po/ all gajim.pot')
 	print 'gajim.pot was updated successfully'
