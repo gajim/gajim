b28f22ac050252470300c9ad82c439c1b32fff0a
Yann Leboulanger <asterix@lagaule.org>
2006-06-15 07:18:55 +0000

    fix header in fuzzyclock.py

--- src/common/fuzzyclock.py
+++ src/common/fuzzyclock.py
@@ -1,21 +1,24 @@
-############################################################################
-#    Copyright (C) 2005 by Christoph Neuroth <delmonico@gmx.net>           #
-#                                                                          #
-#    This program is free software; you can redistribute it and/or modify  #
-#    it under the terms of the GNU General Public License as published by  #
-#    the Free Software Foundation; either version 2 of the License, or     #
-#    (at your option) any later version.                                   #
-#                                                                          #
-#    This program is distributed in the hope that it will be useful,       #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
-#    GNU General Public License for more details.                          #
-#                                                                          #
-#    You should have received a copy of the GNU General Public License     #
-#    along with this program; if not, write to the                         #
-#    Free Software Foundation, Inc.,                                       #
-#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
-############################################################################
+##      fuzzyclock.py
+##
+## Contributors for this file:
+##
+## - Yann Le Boulanger <asterix@lagaule.org>
+## - Christoph Neuroth <delmonico@gmx.net>
+##
+## Copyright (C) 2006 Christoph Neuroth <delmonico@gmx.net>
+##                    Yann Le Boulanger <asterix@lagaule.org>
+##                    Dimitur Kirov <dkirov@gmail.com>
+##                    Travis Shirk <travis@pobox.com>
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published
+## by the Free Software Foundation; version 2 only.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
 
 '''
 Python class to show a "fuzzy clock".
