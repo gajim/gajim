13a50ad633bbf55b83f02abfcc5036a9c2685458
Nikos Kouremenos <kourem@gmail.com>
2006-08-29 21:59:58 +0000

    dl module commit rocks, just put a comment, do it only in POSIX, pythonify (space after comma and all that stuff)

--- src/gajim.py
+++ src/gajim.py
@@ -41,12 +41,13 @@ from chat_control import ChatControlBase
 
 from common import exceptions
 
-try:
-	import dl 
-	libc = dl.open('/lib/libc.so.6') 
-	libc.call('prctl',15,'gajim\0',0,0,0) 
-except: 
-	pass
+if os.name == 'posix': # dl module is Unix Only
+	try: # rename the process name to gajim
+		import dl
+		libc = dl.open('/lib/libc.so.6')
+		libc.call('prctl', 15, 'gajim\0', 0, 0, 0)
+	except:
+		pass
 
 try:
 	import gtk
