b4b96c837cc6d524c569f04c33349844a61ee83e
Nikos Kouremenos <kourem@gmail.com>
2006-09-21 13:28:46 +0000

    kill support for old versions of dbus [noone runs it, noone tests it] fixes #1912

--- src/dbus_support.py
+++ src/dbus_support.py
@@ -23,7 +23,11 @@ from common import exceptions
 
 try:
 	import dbus
-	version = getattr(dbus, 'version', (0, 20, 0))
+	import dbus.service
+	# cause dbus 0.35+ doesn't return signal replies without it
+	import dbus.glib 
+
+	version = getattr(dbus, 'version')
 	supported = True
 except ImportError:
 	version = (0, 0, 0)
@@ -31,14 +35,6 @@ except ImportError:
 	if not os.name == 'nt': # only say that to non Windows users
 		print _('D-Bus python bindings are missing in this computer')
 		print _('D-Bus capabilities of Gajim cannot be used')
-	
-# dbus 0.23 leads to segfault with threads_init()
-if sys.version[:4] >= '2.4' and version[1] < 30:
-	supported = False
-
-if version >= (0, 41, 0):
-	import dbus.service
-	import dbus.glib # cause dbus 0.35+ doesn't return signal replies without it
 
 class SessionBus:
 	'''A Singleton for the DBus SessionBus'''
