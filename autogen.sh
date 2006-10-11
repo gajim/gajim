595f4bbf4b0246e4d7816519740404572e4051c0
Dimitur Kirov <dkirov@gmail.com>
2006-10-11 12:37:08 +0000

    adding Stefan to AUTHORS file cleaning Makefile.in-s in make maintainer-clean skip noisy message in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-set -x
   echo "[encoding: UTF-8]" > po/POTFILES.in \
   && ls data/gajim.desktop.in.in data/glade/*.glade \
   src/*py src/common/*py src/common/zeroconf/*.py -1 -U >> \
   po/POTFILES.in || exit 1
+  set -x
   intltoolize --force --automake \
   && aclocal -I ./m4 \
   && libtoolize --copy --force --automake \
