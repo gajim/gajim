f04b5c39028103ddecf3d4822c6004ae28a264a2
Dimitur Kirov <dkirov@gmail.com>
2006-10-15 10:32:17 +0000

    check for pkg-config in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,11 @@
   && ls -1 -U data/gajim.desktop.in.in data/glade/*.glade \
   src/*py src/common/*py src/common/zeroconf/*.py >> \
   po/POTFILES.in || exit 1
+  if test -z `which pkg-config 2>/dev/null`;then
+    echo "***Error: pkg-config not found***"
+	echo "See README.html for build requirements."
+	exit 1
+  fi
   set -x
   intltoolize --force --automake \
   && aclocal -I ./m4 \
