9097f9d311399a590a0fbd931dc8ec2312f35cc5
Yann Leboulanger <asterix@lagaule.org>
2006-11-28 08:14:19 +0000

    [dsh] use same python version to launch history manager than the one used to launch gajim itself. fixes #2720

--- src/roster_window.py
+++ src/roster_window.py
@@ -18,6 +18,7 @@
 import gtk
 import gobject
 import os
+import sys
 import time
 import urllib
 
@@ -744,9 +745,9 @@ class RosterWindow:
 			if os.path.exists('history_manager.exe'): # user is running stable
 				helpers.exec_command('history_manager.exe')
 			else: # user is running svn
-				helpers.exec_command('python history_manager.py')
+				helpers.exec_command('%s history_manager.py' % (sys.executable,))
 		else: # Unix user
-			helpers.exec_command('python history_manager.py &')
+			helpers.exec_command('%s history_manager.py &' % (sys.executable,))
 
 	def get_and_connect_advanced_menuitem_menu(self, account):
 		'''adds FOR ACCOUNT options'''
