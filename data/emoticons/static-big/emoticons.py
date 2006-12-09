19418743acece49af43a5c591885fa770ac4205d
Josef Vybíral <corneliu@vybiral.inf>
2006-12-09 17:02:24 +0000

    Package of small (16x16) emoticons and big (22x22) emoticons. The small one is set to be default.

---
+++ data/emoticons/static-big/emoticons.py
@@ -0,0 +1,84 @@
+emoticons = {
+	':-)': 'smile.png',
+	':)': 'smile.png',
+#
+	'B-)': 'coolglasses.png',
+	'8-)': 'coolglasses.png',
+#
+	';-)': 'wink.png',
+	';)': 'wink.png',
+#	
+	':-D': 'biggrin.png',
+	':->': 'biggrin.png',
+	':D': 'biggrin.png',
+	':>': 'biggrin.png',
+	'xD': 'biggrin.png',	
+#	
+	':-(': 'unhappy.png',
+	':(': 'unhappy.png',
+#
+	":'(": 'cry.png',
+	';-(': 'cry.png',
+	';(': 'cry.png',
+	";'-(": 'cry.png',
+#			
+	':-/': 'frowning.png',
+	':/': 'frowning.png',
+	':-S': 'frowning.png',
+	':S': 'frowning.png',
+#	
+	':-$': 'blush.png',
+	':$': 'blush.png',
+#	
+	':-@': 'angry.png',
+	':@': 'angry.png',
+#	
+	':-[': 'bat.png',
+#
+	':-*': 'kiss.png',
+	':*': 'kiss.png',
+#	
+	':-|': 'stare.png',
+	':|': 'stare.png',
+#
+	'>:-)': 'devil.png',
+	'>:)': 'devil.png',
+#
+	':-P': 'tongue.png',
+	':P': 'tongue.png',
+#	
+	':-O': 'oh.png',
+	':O': 'oh.png',
+#
+	'<3': 'heart.png',
+#		
+	':-[': 'bat.png',
+	':[': 'bat.png',
+#
+	#MSN ones
+	'(@)': 'pussy.png',
+	'(%)': 'cuffs.png',
+	'(S)': 'moon.png',
+	'(I)': 'lamp.png',
+	'(8)': 'music.png',
+	'(B)': 'beer.png',
+	'(L)': 'heart.png',
+	'(6)': 'devil.png',
+	'(W)': 'brflower.png',
+	'(Z)': 'boy.png',
+	'(E)': 'mail.png',
+	'(N)': 'no.png',
+	'(P)': 'photo.png',
+	'(K)': 'kiss.png',
+	'(Y)': 'yes.png',
+	'(})': 'hugleft.png',
+	'(U)': 'brheart.png',
+	'(F)': 'flower.png',
+	'(H)': 'coolglasses.png',
+	'(D)': 'drink.png',
+	'(T)': 'phone.png',
+	'(C)': 'coffee.png',
+	'({)': 'hugright.png',
+	'(*)': 'star.png',
+	'(R)': 'rainbow.png',
+}
