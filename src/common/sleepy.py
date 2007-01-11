0b035ee6cc2cbffc3e8964767d90b343c3217683
Yann Leboulanger <asterix@lagaule.org>
2007-01-11 21:27:05 +0000

    indent with tabs, and prevent TB when ctypes modules is not found.

--- src/common/sleepy.py
+++ src/common/sleepy.py
@@ -1,13 +1,13 @@
 ## common/sleepy.py
 ##
 ## Contributors for this file:
-##      - Yann Le Boulanger <asterix@lagaule.org>
-##      - Nikos Kouremenos <kourem@gmail.com>
+##	  - Yann Le Boulanger <asterix@lagaule.org>
+##	  - Nikos Kouremenos <kourem@gmail.com>
 ##
 ## Copyright (C) 2003-2004 Yann Le Boulanger <asterix@lagaule.org>
-##                         Vincent Hanquez <tab@snarc.org>
+##						 Vincent Hanquez <tab@snarc.org>
 ## Copyright (C) 2005-2006 Yann Le Boulanger <asterix@lagaule.org>
-##                    Nikos Kouremenos <kourem@gmail.com>
+##					Nikos Kouremenos <kourem@gmail.com>
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published
@@ -26,44 +26,46 @@ import os
 STATE_UNKNOWN  = 'OS probably not supported'
 STATE_XA   = 'extended away'
 STATE_AWAY   = 'away'
-STATE_AWAKE    = 'awake'
+STATE_AWAKE	= 'awake'
 
 SUPPORTED = True
 try:
-        if os.name == 'nt':
-                import ctypes
+		if os.name == 'nt':
+			import ctypes
 
-                GetTickCount = ctypes.windll.kernel32.GetTickCount
-                GetLastInputInfo = ctypes.windll.user32.GetLastInputInfo
+			GetTickCount = ctypes.windll.kernel32.GetTickCount
+			GetLastInputInfo = ctypes.windll.user32.GetLastInputInfo
 
-                class LASTINPUTINFO(ctypes.Structure):
-                        _fields_ = [('cbSize', ctypes.c_uint),
-                            ('dwTime', ctypes.c_uint)]
+			class LASTINPUTINFO(ctypes.Structure):
+				_fields_ = [('cbSize', ctypes.c_uint), ('dwTime', ctypes.c_uint)]
 
-                lastInputInfo = LASTINPUTINFO()
-                lastInputInfo.cbSize = ctypes.sizeof(lastInputInfo)
+			lastInputInfo = LASTINPUTINFO()
+			lastInputInfo.cbSize = ctypes.sizeof(lastInputInfo)
 
-        else: # unix
-        	import idle
+		else: # unix
+			import idle
 except:
 	gajim.log.debug('Unable to load idle module')
 	SUPPORTED = False
 
 class SleepyWindows:
-        def __init__(self, away_interval = 60, xa_interval = 120):
+		def __init__(self, away_interval = 60, xa_interval = 120):
 		self.away_interval = away_interval
 		self.xa_interval = xa_interval
 		self.state = STATE_AWAKE # assume we are awake
 
-        def getIdleSec(self):
-                GetLastInputInfo(ctypes.byref(lastInputInfo))
-                idleDelta = float(GetTickCount() - lastInputInfo.dwTime) / 1000
-                return idleDelta
+		def getIdleSec(self):
+			GetLastInputInfo(ctypes.byref(lastInputInfo))
+			idleDelta = float(GetTickCount() - lastInputInfo.dwTime) / 1000
+			return idleDelta
 
 	def poll(self):
 		'''checks to see if we should change state'''
+		if not SUPPORTED:
+			return False
+
 		idleTime = self.getIdleSec()
-		
+
 		# xa is stronger than away so check for xa first
 		if idleTime > self.xa_interval:
 			self.state = STATE_XA
@@ -80,7 +82,6 @@ class SleepyWindows:
 		self.state = val
 
 class SleepyUnix:
-
 	def __init__(self, away_interval = 60, xa_interval = 120):
 		self.away_interval = away_interval
 		self.xa_interval = xa_interval
@@ -91,8 +92,8 @@ class SleepyUnix:
 			SUPPORTED = False
 			self.state = STATE_UNKNOWN
 
-        def getIdleSec(self):
-                return idle.getIdleSec()
+		def getIdleSec(self):
+			return idle.getIdleSec()
 
 	def poll(self):
 		'''checks to see if we should change state'''
@@ -100,7 +101,7 @@ class SleepyUnix:
 			return False
 
 		idleTime = self.getIdleSec()
-		
+
 		# xa is stronger than away so check for xa first
 		if idleTime > self.xa_interval:
 			self.state = STATE_XA
@@ -117,6 +118,6 @@ class SleepyUnix:
 		self.state = val
 
 if os.name == 'nt':
-        Sleepy = SleepyWindows
+	Sleepy = SleepyWindows
 else:
-        Sleepy = SleepyUnix
+	Sleepy = SleepyUnix
