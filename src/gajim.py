bee1150747f84dc38f02413524bf7c15996fb037
Yann Leboulanger <asterix@lagaule.org>
2007-08-31 16:25:49 +0000

    prevent TB when we click on a popup notification of an already handled event

--- src/gajim.py
+++ src/gajim.py
@@ -1996,6 +1996,8 @@ class Interface:
 			if not event:
 				# default to jid without resource
 				event = gajim.events.get_first_event(account, jid, type_)
+				if not event:
+					return
 				# Open the window
 				self.roster.open_event(account, jid, event)
 			else:
