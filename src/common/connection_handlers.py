d536fc9648f7aec3a6436f5af30272d457f6b655
Brendan Taylor <bct@diffeq.com>
2007-09-10 21:59:27 +0000

    oops, my last checkin was incomplete

--- src/common/connection_handlers.py
+++ src/common/connection_handlers.py
@@ -1701,8 +1701,8 @@ class ConnectionHandlers(ConnectionVcard, ConnectionBytestream, ConnectionDisco,
 			
 			if not self.sessions[jid]:
 				del self.sessions[jid]
-		except KeyError
-			print "jid %s should have been in %s, but it wasn't. missing session?" % (repr(jid), repr(self.sessions.keys())
+		except KeyError:
+			print "jid %s should have been in %s, but it wasn't. missing session?" % (repr(jid), repr(self.sessions.keys()))
 
 	def move_session(self, original_jid, thread_id, to_resource):
 		'''moves a session to another resource.'''
