2623c75ca035c07ffa03e21ae8e88d1ff10bb1e7
Yann Leboulanger <asterix@lagaule.org>
2007-09-26 17:52:40 +0000

    [Vlad Glagolev] use sys.executable ti get python path. fixes #3463

--- src/roster_window.py
+++ src/roster_window.py
@@ -22,6 +22,7 @@ import gtk
 import pango
 import gobject
 import os
+import sys
 import time
 import urllib
 
@@ -845,9 +846,9 @@ class RosterWindow:
 			if os.path.exists('history_manager.exe'): # user is running stable
 				helpers.exec_command('history_manager.exe')
 			else: # user is running svn
-				helpers.exec_command('python history_manager.py')
+				helpers.exec_command('%s history_manager.py' % (sys.executable,))
 		else: # Unix user
-			helpers.exec_command('python history_manager.py &')
+			helpers.exec_command('%s history_manager.py &' % (sys.executable,))
 
 	def get_and_connect_advanced_menuitem_menu(self, account):
 		'''adds FOR ACCOUNT options'''
