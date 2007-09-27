2ff89ea2bcc8291bfe80588e8b41654bec9f8c96
Yann Leboulanger <asterix@lagaule.org>
2007-09-27 06:50:34 +0000

    revert [8868]. see #3463

--- src/roster_window.py
+++ src/roster_window.py
@@ -846,9 +846,9 @@ class RosterWindow:
 			if os.path.exists('history_manager.exe'): # user is running stable
 				helpers.exec_command('history_manager.exe')
 			else: # user is running svn
-				helpers.exec_command('%s history_manager.py' % (sys.executable,))
+				helpers.exec_command('python history_manager.py')
 		else: # Unix user
-			helpers.exec_command('%s history_manager.py &' % (sys.executable,))
+			helpers.exec_command('python history_manager.py &')
 
 	def get_and_connect_advanced_menuitem_menu(self, account):
 		'''adds FOR ACCOUNT options'''
