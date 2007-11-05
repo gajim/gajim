7c0e70acdefba977fb5906d54d486296039a88db
Yann Leboulanger <asterix@lagaule.org>
2007-11-05 22:41:12 +0000

    fix TB on gajim startup. fixes #3533

--- src/common/passwords.py
+++ src/common/passwords.py
@@ -42,7 +42,8 @@ class SimplePasswordStorage(PasswordStorage):
 
 	def save_password(self, account_name, password):
 		gajim.config.set_per('accounts', account_name, 'password', password)
-		gajim.connections[account_name].password = password
+		if account_name in gajim.connections:
+			gajim.connections[account_name].password = password
 
 
 class GnomePasswordStorage(PasswordStorage):
