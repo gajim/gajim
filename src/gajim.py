4974773895c5c3ee7216debed542f9d95aff708e
Stephan Erb <steve-e@h3c.de>
2008-02-08 22:15:03 +0000

    Remove workaround old, non xep-complient MSN transports.

--- src/gajim.py
+++ src/gajim.py
@@ -720,13 +720,6 @@ class Interface:
 			elif new_show > 1: # Status change (not connected/disconnected or error (<1))
 				notify.notify('status_change', jid, account, [new_show,
 					status_message])
-		else:
-			# FIXME: Msn transport (CMSN1.2.1 and PyMSN0.10) doesn't follow the JEP
-			# remove in 2007
-			# It's maybe a GC_NOTIFY (specialy for MSN gc)
-			self.handle_event_gc_notify(account, (jid, array[1], status_message,
-				array[3], None, None, None, None, None, [], None, None))
-
 
 	def handle_event_msg(self, account, array):
 		# 'MSG' (account, (jid, msg, time, encrypted, msg_type, subject,
