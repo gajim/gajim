11e4945158810672203480948c382b19b6d55d83
Jean-Marie Traissard <jim@lapin.org>
2008-03-24 00:45:53 +0000

    Use appropriate function to remove a line in unread_message table. See #3793.

--- src/common/logger.py
+++ src/common/logger.py
@@ -371,12 +371,7 @@ class Logger:
 			results = self.cur.fetchall()
 			if len(results) == 0:
 				# Log line is no more in logs table. remove it from unread_messages
-				sql = 'DELETE FROM unread_messages WHERE message_id=%s' % msg_id
-				self.cur.execute(sql)
-				try:
-					self.con.commit()
-				except sqlite.OperationalError, e:
-					print >> sys.stderr, str(e)
+				self.set_read_messages([msg_id])
 				continue
 			all_messages.append(results[0])
 		return all_messages
