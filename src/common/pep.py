24203f367ffda34b822144b7b4447e58caa1279d
Brendan Taylor <bct@diffeq.com>
2008-05-11 01:48:10 +0000

    fixed use of get_control

--- src/common/pep.py
+++ src/common/pep.py
@@ -253,12 +253,10 @@ def user_nickname(items, name, jid):
 		if nick is not None:
 			contact.contact_name = nick
 			gajim.interface.roster.draw_contact(user, name)
-			ctrl = gajim.interface.msg_win_mgr.get_control(user, name)
-			if ctrl:
+			for ctrl in gajim.interface.msg_win_mgr.get_chat_controls(user, name):
 				ctrl.update_ui()
-				win = gajim.interface.msg_win_mgr.get_window(user, name)
-				win.redraw_tab(ctrl)
-				win.show_title()
+				ctrl.parent_win.redraw_tab(ctrl)
+				ctrl.parent_win.show_title()
 	elif retract:
 		contact.contact_name = ''
 
