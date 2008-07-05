32bb251381fab2a2252330fa6bafc33fcd4e045c
Yann Leboulanger <asterix@lagaule.org>
2008-07-05 18:25:09 +0000

    typo

--- src/common/connection.py
+++ src/common/connection.py
@@ -617,8 +617,9 @@ class Connection(ConnectionHandlers):
 		'warn_when_plaintext_connection'):
 			self.dispatch('PLAIN_CONNECTION', (con,))
 			return True
+		print hasattr(con.Connection, '_sslContext')
 		if _con_type in ('tls', 'ssl') and not hasattr(con.Connection,
-		'_sslcontext') and gajim.config.get_per('accounts', self.name,
+		'_sslContext') and gajim.config.get_per('accounts', self.name,
 		'warn_when_insecure_ssl_connection'):
 			# Pyopenssl is not used
 			self.dispatch('INSECURE_SSL_CONNECTION', (con, _con_type))
