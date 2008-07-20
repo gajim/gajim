41e3b00dbeaf03c888858b2aa7023f8c47fbd6eb
js <js-gajim@webkeks.org>
2008-07-20 14:44:08 +0000

    This should automatically include the revision in our version.

--- src/common/defs.py
+++ src/common/defs.py
@@ -3,6 +3,9 @@ docdir = '../'
 datadir = '../'
 
 version = '0.11.4.4-svn'
+rev = '$Rev$'.replace('$', '').replace('Rev', '').replace(': ', '')
+if rev is not '':
+	version += ' r' + rev
 
 import sys, os.path
 for base in ('.', 'common'):
