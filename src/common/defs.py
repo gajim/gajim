6239ae57c36b54fe32e8ab8b58984a7a2c713de4
js <js-gajim@webkeks.org>
2008-07-21 22:55:03 +0000

    Remove rev keyword code again as this doesn't work. It's only updated when the defs.py file is changed.

--- src/common/defs.py
+++ src/common/defs.py
@@ -5,10 +5,6 @@ datadir = '../'
 
 version = '0.11.4.4-svn'
 
-rev = re.sub('[^0-9]', '', '$Rev$')
-if rev is not '':
-	version += ' r' + rev
-
 import sys, os.path
 for base in ('.', 'common'):
 	sys.path.append(os.path.join(base, '.libs'))
