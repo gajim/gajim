4674480e612f4d2672a5fda58cdacd57b34a1077
Stephan Erb <steve-e@h3c.de>
2008-07-25 10:50:56 +0000

    Fix wrong paranthesis. Fixes #4137

--- src/htmltextview.py
+++ src/htmltextview.py
@@ -564,7 +564,7 @@ class HtmlHandler(xml.sax.handler.ContentHandler):
 			try: 
 				f = urllib2.urlopen(attrs['src']) 
 			except Exception, ex: 
-				gajim.log.debug('Error loading image %s ' % attrs['src']  + str(ex)))
+				gajim.log.debug('Error loading image %s ' % attrs['src']  + str(ex))
 				pixbuf = None 
 				alt = attrs.get('alt', 'Broken image') 
 			else: 
