31ff68e67c8f69dcca63ee39bac96281f0c2adfa
js <js-gajim@webkeks.org>
2008-08-09 16:14:07 +0000

    Better XEP-0184 warning text.
    Thanks for doing my work, roidelapluie :)

--- src/conversation_textview.py
+++ src/conversation_textview.py
@@ -536,9 +536,10 @@ class ConversationTextview:
 		if xep0184_warning and not self.xep0184_warning_tooltip.win:
 			# check if the current pointer is still over the line
 			position = self.tv.window.get_origin()
-			self.xep0184_warning_tooltip.show_tooltip(_('This message '
-			'was not delivered'),
-				8, position[1] + pointer[1])
+			self.xep0184_warning_tooltip.show_tooltip(_('This icon indicates that '
+				'this message has not been delivered yet.\nIf this icon stays for '
+				"a long time, it's likely the message got lost."), 8,
+				position[1] + pointer[1])
 
 	def show_line_tooltip(self):
 		pointer = self.tv.get_pointer()
