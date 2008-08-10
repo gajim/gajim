3c7e7f505236c01fff0dde3d52968579e2eefd3b
Yann Leboulanger <asterix@lagaule.org>
2008-08-10 20:28:22 +0000

    idle.c is GPLv3 too. Fixed #4187

--- src/common/idle.c
+++ src/common/idle.c
@@ -8,14 +8,19 @@
  * Copyright (C) 2005-2006 Yann Le Boulanger <asterix@lagaule.org>
  *                    Nikos Kouremenos <nkour@jabber.org>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation; version 2 only.
+ * This file is part of Gajim.
  *
- * This program is distributed in the hope that it will be useful,
+ * Gajim is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; version 3 only.
+ *
+ * Gajim is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public Licens
+ * along with Gajim.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef _WIN32
