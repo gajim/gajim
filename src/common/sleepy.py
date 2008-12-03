d1a0a436c7703d45787fea6fb779289a01640373
Yann Leboulanger <asterix@lagaule.org>
2008-12-03 22:46:55 +0000

    [thorstenp] use ctype to have idle. No need to compile it from C. Fixes #4260

--- src/common/sleepy.py
+++ src/common/sleepy.py
@@ -47,7 +47,7 @@ try:
 	elif sys.platform == 'darwin':
 		import osx.idle as idle
 	else: # unix
-		import idle
+		from common import idle
 except Exception:
 	gajim.log.debug('Unable to load idle module')
 	SUPPORTED = False
@@ -91,11 +91,6 @@ class SleepyUnix:
 		self.away_interval = away_interval
 		self.xa_interval = xa_interval
 		self.state = STATE_AWAKE # assume we are awake
-		try:
-			idle.init()
-		except Exception:
-			SUPPORTED = False
-			self.state = STATE_UNKNOWN
 
 	def getIdleSec(self):
 		return idle.getIdleSec()
