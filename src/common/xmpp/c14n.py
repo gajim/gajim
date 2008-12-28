e98a648908262788485f09e56e302c51989570eb
Brendan Taylor <bct@diffeq.com>
2008-12-28 01:39:39 +0000

    added licence notice to common/xmpp/c14n.py

--- src/common/xmpp/c14n.py
+++ src/common/xmpp/c14n.py
@@ -1,6 +1,23 @@
 ''' XML canonicalisation methods (for XEP-0116) '''
-# FIXME: Add licence, copyright
-
+# -*- coding:utf-8 -*-
+## src/common/stanza_session.py
+##
+## Copyright (C) 2007-2008 Brendan Taylor <whateley AT gmail.com>
+##
+## This file is part of Gajim.
+##
+## Gajim is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published
+## by the Free Software Foundation; version 3 only.
+##
+## Gajim is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Gajim. If not, see <http://www.gnu.org/licenses/>.
+##
 from simplexml import ustr
 
 def c14n(node):
