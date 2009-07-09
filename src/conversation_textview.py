4917ffdbcc046095bcc582e9afccf17027ec234e
Yann Leboulanger <asterix@lagaule.org>
2009-07-09 19:06:08 +0200

    Fix traceback when right click on email in history viewer. Fixes #5131

--- src/conversation_textview.py
+++ src/conversation_textview.py
@@ -873,12 +873,13 @@ class ConversationTextview(gobject.GObject):
 			self.handlers[id_] = childs[6]
 
 			allow_add = False
-			c = gajim.contacts.get_first_contact_from_jid(self.account, text)
-			if c and not gajim.contacts.is_pm_from_contact(self.account, c):
-				if _('Not in Roster') in c.groups:
+			if self.account:
+				c = gajim.contacts.get_first_contact_from_jid(self.account, text)
+				if c and not gajim.contacts.is_pm_from_contact(self.account, c):
+					if _('Not in Roster') in c.groups:
+						allow_add = True
+				else: # he or she's not at all in the account contacts
 					allow_add = True
-			else: # he or she's not at all in the account contacts
-				allow_add = True
 
 			if allow_add:
 				id_ = childs[7].connect('activate', self.on_add_to_roster_activate,
