5cf6cab7ad93ae034a8c5de1ada8db19f66fb9e4
Yann Leboulanger <asterix@lagaule.org>
2009-07-31 10:54:35 +0200

    print error message when PyGTK fails to be imported

--- src/gajim.py
+++ src/gajim.py
@@ -178,7 +178,9 @@ try:
 except Warning, msg:
 	if str(msg) == 'could not open display':
 		print >> sys.stderr, _('Gajim needs X server to run. Quiting...')
-		sys.exit()
+	else:
+		print >> sys.stderr, 'importing PyGTK failed: %s' % msg
+	sys.exit()
 warnings.resetwarnings()
 
 if os.name == 'nt':
