086469a594e6ecd32b024e478253d82a81b22370
Yann Leboulanger <asterix@lagaule.org>
2009-09-21 21:31:40 +0200

    [g.livid] fix xfce4 notifd usage with git version of notifyd. Fixes #5280

--- src/notify.py
+++ src/notify.py
@@ -650,7 +650,7 @@ class DesktopNotification:
 	def version_reply_handler(self, name, vendor, version, spec_version=None):
 		if spec_version:
 			version = spec_version
-		elif vendor == 'Xfce' and version == '0.1.0':
+		elif vendor == 'Xfce' and version.startswith('0.1.0'):
 			version = '0.9'
 		version_list = version.split('.')
 		self.version = []
