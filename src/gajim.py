5698327831e2115e5a62d143c28bde0ecbc8f050
Yann Leboulanger <asterix@lagaule.org>
2009-10-27 19:36:36 +0100

    [Stepan Kasal] fix italic detection with //. Fixes #4217

--- src/gajim.py
+++ src/gajim.py
@@ -2646,7 +2646,7 @@ class Interface:
 		#detects eg. *b* *bold* *bold bold* test *bold* *bold*! (*bold*)
 		#doesn't detect (it's a feature :P) * bold* *bold * * bold * test*bold*
 		formatting = r'|(?<!\w)' r'\*[^\s*]' r'([^*]*[^\s*])?' r'\*(?!\w)|'\
-			r'(?<!\w|\<)' r'/[^\s/]' r'([^/]*[^\s/])?' r'/(?!\w)|'\
+			r'(?<!\S)' r'/[^\s/]' r'([^/]*[^\s/])?' r'/(?!\S)|'\
 			r'(?<!\w)' r'_[^\s_]' r'([^_]*[^\s_])?' r'_(?!\w)'
 
 		latex = r'|\$\$[^$\\]*?([\]\[0-9A-Za-z()|+*/-]|[\\][\]\[0-9A-Za-z()|{}$])(.*?[^\\])?\$\$'
