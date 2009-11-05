dec49976c4057a54c53917e73c13bdd64a8dd6d0
Yann Leboulanger <asterix@lagaule.org>
2009-11-05 22:20:45 +0100

    Use a separate label in RIE dialog instead of a stock button. This way we can change label easily.

--- src/dialogs.py
+++ src/dialogs.py
@@ -2862,7 +2862,7 @@ class RosterItemExchangeWindow:
 		self.window = self.xml.get_widget('roster_item_exchange_window')
 
 		# Add Widgets.
-		for widget_to_add in ['cancel_button', 'accept_button', 'type_label',
+		for widget_to_add in ['accept_button_label', 'type_label',
 		'body_scrolledwindow', 'body_textview', 'items_list_treeview']:
 			self.__dict__[widget_to_add] = self.xml.get_widget(widget_to_add)
 
@@ -2930,9 +2930,7 @@ class RosterItemExchangeWindow:
 					model.set(iter, 0, True, 1, jid, 2, name, 3, groups)
 
 			# Change label for accept_button to action name instead of 'OK'.
-			accept_button_label = self.accept_button.get_children()[0].\
-				get_children()[0].get_children()[1]
-			accept_button_label.set_label(_('Add'))
+			self.accept_button_label.set_label(_('Add'))
 		elif action == 'modify':
 			for jid in self.exchange_list:
 				groups = ''
@@ -2963,9 +2961,7 @@ class RosterItemExchangeWindow:
 					model.set(iter, 0, True, 1, jid, 2, name, 3, groups)
 
 			# Change label for accept_button to action name instead of 'OK'.
-			accept_button_label = self.accept_button.get_children()[0].\
-				get_children()[0].get_children()[1]
-			accept_button_label.set_label(_('Modify'))
+			self.accept_button_label.set_label(_('Modify'))
 		elif action == 'delete':
 			for jid in self.exchange_list:
 				groups = ''
@@ -2989,9 +2985,7 @@ class RosterItemExchangeWindow:
 					model.set(iter, 0, True, 1, jid, 2, name, 3, groups)
 
 			# Change label for accept_button to action name instead of 'OK'.
-			accept_button_label = self.accept_button.get_children()[0].\
-				get_children()[0].get_children()[1]
-			accept_button_label.set_label(_('Delete'))
+			self.accept_button_label.set_label(_('Delete'))
 
 		if show_dialog:
 			self.window.show_all()
