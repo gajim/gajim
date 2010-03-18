03f6c9f380f5655377ace7f19d5a9b4965a780fe
Alexander Cherniuk <ts33kr@gmail.com>
2010-03-18 08:32:47 +0200

    Missed some copyrights, updating

--- src/command_system/__init__.py
+++ src/command_system/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2009  Alexander Cherniuk <ts33kr@gmail.com>
+# Copyright (C) 2009-2010  Alexander Cherniuk <ts33kr@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
