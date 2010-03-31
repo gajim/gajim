da6b4c031e41bbf01345ae3f483eb918da33b5de
Alexander Cherniuk <ts33kr@gmail.com>
2010-03-31 09:05:17 +0300

    Added a call to gobject.set_prgname

--- src/gajim.py
+++ src/gajim.py
@@ -218,6 +218,7 @@ if not hasattr(gobject, 'timeout_add_seconds'):
         return gobject.timeout_add(time_sec * 1000, *args)
     gobject.timeout_add_seconds = timeout_add_seconds_fake
 
+gobject.set_prgname('gajim')
 
 import signal
 import gtkgui_helpers
