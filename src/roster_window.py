323feb9aac442c770faaf4e3086388a6fb64f7d1
Yann Leboulanger <asterix@lagaule.org>
2010-08-12 22:11:11 +0200

    don't show roster list when we hide it, minimize window to systray, and restore it. Fixes #5850

--- src/roster_window.py
+++ src/roster_window.py
@@ -1890,11 +1890,13 @@ class RosterWindow:
 ################################################################################
 
     def show_roster_vbox(self, active):
+        vb = self.xml.get_object('roster_vbox2')
         if active:
-            self.xml.get_object('roster_vbox2').show()
+            vb.set_no_show_all(False)
+            vb.show()
         else:
             self.xml.get_object('roster_vbox2').hide()
-
+            vb.set_no_show_all(True)
 
     def show_tooltip(self, contact):
         pointer = self.tree.get_pointer()
