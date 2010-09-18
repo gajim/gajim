cb76b11b9822ee28003eeff5ba1b4378a92b7bf6
Yann Leboulanger <asterix@lagaule.org>
2010-09-18 23:06:36 +0200

    [Dicson] Don't always show Tray icon if "only when pending events" option is used. Fixes #5921

--- src/config.py
+++ src/config.py
@@ -853,12 +853,10 @@ class PreferencesWindow:
             gajim.config.set('trayicon', 'on_event')
             gajim.interface.systray_enabled = True
             gajim.interface.systray.show_icon()
-            gajim.interface.systray.set_img()
         else:
             gajim.config.set('trayicon', 'always')
             gajim.interface.systray_enabled = True
             gajim.interface.systray.show_icon()
-            gajim.interface.systray.set_img()
 
     def on_advanced_notifications_button_clicked(self, widget):
         dialogs.AdvancedNotificationsWindow()
