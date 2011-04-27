0645a6e85902f89a59a5a47df5f37a7b752d9548
Yann Leboulanger <asterix@lagaule.org>
2011-04-27 17:19:42 +0200

    allow WM to use several sizes of gajim icon so that it's nicer. Fixes #6839

--- src/gui_interface.py
+++ src/gui_interface.py
@@ -2786,10 +2786,14 @@ class Interface:
         import statusicon
         self.systray = statusicon.StatusIcon()
 
-        pix = gtkgui_helpers.get_icon_pixmap('gajim', 32)
-        if pix is not None:
+        pixs = []
+        for size in (16, 32, 48, 64, 128):
+            pix = gtkgui_helpers.get_icon_pixmap('gajim', size)
+            if pix:
+                pixs.append(pix)
+        if pixs:
             # set the icon to all windows
-            gtk.window_set_default_icon(pix)
+            gtk.window_set_default_icon_list(*pixs)
 
         self.init_emoticons()
         self.make_regexps()
