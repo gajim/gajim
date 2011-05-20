6287ebbe2f14e0beadcb2ca0f6850165151ee494
Yann Leboulanger <asterix@lagaule.org>
2011-05-20 22:49:45 +0200

    remove call to non-existing function

--- src/groupchat_control.py
+++ src/groupchat_control.py
@@ -1650,8 +1650,6 @@ class GroupchatControl(ChatControlBase):
 
         label = self.get_seclabel()
         if message != '' or message != '\n':
-            self.save_sent_message(message)
-
             # Send the message
             gajim.connections[self.account].send_gc_message(self.room_jid,
                     message, xhtml=xhtml, label=label)
