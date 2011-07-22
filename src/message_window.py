735032e25088ad58db0ebaf23da7cd918e699073
Yann Leboulanger <asterix@lagaule.org>
2011-07-22 12:05:38 +0200

    fix weird traceback

--- src/message_window.py
+++ src/message_window.py
@@ -623,7 +623,10 @@ class MessageWindow(object):
             self.notebook.set_show_tabs(show_tabs_if_one_tab)
 
     def redraw_tab(self, ctrl, chatstate = None):
-        hbox = self.notebook.get_tab_label(ctrl.widget).get_children()[0]
+        tab = self.notebook.get_tab_label(ctrl.widget)
+        if not tab:
+            return
+        hbox = tab.get_children()[0]
         status_img = hbox.get_children()[0]
         nick_label = hbox.get_children()[1]
 
