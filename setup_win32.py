dd09b8f8bfb83c8f272178eef69df18de2d1337b
Yann Leboulanger <asterix@lagaule.org>
2011-12-17 23:08:33 +0100

    add port and goocanvas to gajim built

--- setup_win32.py
+++ setup_win32.py
@@ -31,7 +31,8 @@ if 'gtk' in os.listdir('.'):
 
 options = {
    'build_exe': {
-       'includes': ['gtk.keysyms', 'dumbdbm', 'dbhash', 'bsddb', 'new'],
+       'includes': ['gtk.keysyms', 'dumbdbm', 'dbhash', 'bsddb', 'new', 'potr',
+            'goocanvas'],
        'base': 'Win32GUI',
 	   'bin_excludes': [
             'iconv.dll', 'intl.dll', 'libatk-1.0-0.dll',
