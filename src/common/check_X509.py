6fbb0b78477e60dc026996be6b3bce572fc43cc2
Yann Leboulanger <asterix@lagaule.org>
2012-03-27 14:57:49 +0200

    fix pyopenssl version checking. Fixes #7124

--- src/common/check_X509.py
+++ src/common/check_X509.py
@@ -5,8 +5,7 @@ try:
     import OpenSSL.SSL
     import OpenSSL.crypto
     ver = OpenSSL.__version__
-    ver_l = [int(i) for i in ver.split('.')]
-    if ver_l < [0, 12]:
+    if ver < '0.12':
         raise ImportError
     from pyasn1.type import univ, constraint, char, namedtype, tag
     from pyasn1.codec.der.decoder import decode
