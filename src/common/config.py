781691169ef538604631e9052c3eb3196455e11a
Yann Leboulanger <asterix@lagaule.org>
2012-04-19 12:32:31 +0200

    prevent a traceback

--- src/common/config.py
+++ src/common/config.py
@@ -785,7 +785,8 @@ class Config:
             self.__options[1][opt] = self.__options[0][opt][OPT_VAL]
 
     def _really_save(self):
-        gajim.interface.save_config()
+        if gajim.interface:
+            gajim.interface.save_config()
         self.save_timeout_id = None
         return False
 
