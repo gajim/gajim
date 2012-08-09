40edf1d0f20b19ebfea08c0a39c9375f74a9731e
Denis Fomin <fominde@gmail.com>
2012-08-09 21:55:35 +0400

    typo

--- src/plugins/pluginmanager.py
+++ src/plugins/pluginmanager.py
@@ -100,7 +100,7 @@ class PluginManager(object):
         '''
         Registered handlers of GUI extension points.
         '''
-        for path in [gajim.PLUGINS_DIRS[1], gajim.PLUGINS_DIRS[0]):
+        for path in [gajim.PLUGINS_DIRS[1], gajim.PLUGINS_DIRS[0]]:
             pc = PluginManager.scan_dir_for_plugins(path)
             self.add_plugins(pc)
         self._activate_all_plugins_from_global_config()
