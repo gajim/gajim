e9c2dfacc3cced069daa7d6a79dbfde3d3f43006
Yann Leboulanger <asterix@lagaule.org>
2012-08-28 11:10:42 +0200

    version will be 0.15.1

--- src/common/defs.py
+++ src/common/defs.py
@@ -27,7 +27,7 @@ docdir = '../'
 basedir   = '../'
 localedir = '../po'
 
-version = '0.15.0.1'
+version = '0.15.1'
 import subprocess
 try:
     node = subprocess.Popen('hg tip --template "{node|short}"', shell=True,
