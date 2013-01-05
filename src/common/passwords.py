e0b63d4a4004bfa12fff0a5770cb391574837011
Yann Leboulanger <asterix@lagaule.org>
2013-01-05 10:58:51 +0100

    fix gnomekeyring import

--- src/common/passwords.py
+++ src/common/passwords.py
@@ -200,7 +200,7 @@ def get_storage():
             global GnomeKeyring
             try:
                 gir = __import__('gi.repository', globals(), locals(),
-                    ['GnomeKeyring'], 0)
+                    ['GnomeKeyring'], 1)
                 GnomeKeyring = gir.GnomeKeyring
             except ImportError:
                 pass
