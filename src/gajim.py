f8a0333ad77590dd84f3ce4ccc72156db3b42f29
Yann Leboulanger <asterix@lagaule.org>
2013-03-11 20:21:59 +0100

    more fix for ipython import. Fixes #7316

--- src/gajim.py
+++ src/gajim.py
@@ -73,7 +73,7 @@ demandimport.enable()
 demandimport.ignore += ['gobject._gobject', 'libasyncns', 'i18n',
     'logging.NullHandler', 'dbus.service', 'OpenSSL.SSL', 'OpenSSL.crypto',
     'common.sleepy', 'DLFCN', 'dl', 'xml.sax', 'xml.sax.handler', 'ic',
-    'Crypto.PublicKey', 'IPython', 'gst.interfaces']
+    'Crypto.PublicKey', 'IPython', 'contextlib', 'imp', 'gst.interfaces']
 
 if os.name == 'nt':
     import locale
