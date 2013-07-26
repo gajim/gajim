18c4edb1d0b8090cdb3ee2e9e4d6c86e4bd821a6
Yann Leboulanger <asterix@lagaule.org>
2013-07-26 20:29:50 +0200

    go to end iter after each print_special_text if we don't have an argument with the iter where we have to insert text. See #7372

--- src/conversation_textview.py
+++ src/conversation_textview.py
@@ -1085,6 +1085,8 @@ class ConversationTextview(gobject.GObject):
             special_text = otext[start:end]
             if start > index:
                 text_before_special_text = otext[index:start]
+                if not iter_:
+                    end_iter = buffer_.get_end_iter()
                 # we insert normal text
                 insert_tags_func(end_iter, text_before_special_text, *other_tags)
             index = end # update index
