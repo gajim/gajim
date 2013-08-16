41f4c110dbfa700215675b74b4442991faeca93d
Yann Leboulanger <asterix@lagaule.org>
2013-08-16 09:55:55 +0200

    add monotonic to the list of ignored modules by demandimport. Fixes #7409

--- src/gajim.py
+++ src/gajim.py
@@ -73,7 +73,7 @@ except ImportError:
 #demandimport.ignore += ['GObject._gobject', 'libasyncns', 'i18n',
 #    'logging.NullHandler', 'dbus.service', 'OpenSSL.SSL', 'OpenSSL.crypto',
 #    'common.sleepy', 'DLFCN', 'dl', 'xml.sax', 'xml.sax.handler', 'ic',
-#    'Crypto.PublicKey', 'IPython', 'contextlib', 'imp']
+#    'Crypto.PublicKey', 'IPython', 'contextlib', 'imp', 'monotonic']
 
 if os.name == 'nt':
     import locale
