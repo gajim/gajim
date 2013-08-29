ecbaa71e2f3caf074d81ef13e2684e90a3e80c05
Yann Leboulanger <asterix@lagaule.org>
2013-08-29 13:14:22 +0200

    force max width to 250px only when initializing the treeview. Fixes #7446

--- src/advanced_configuration_window.py
+++ src/advanced_configuration_window.py
@@ -116,7 +116,9 @@ class AdvancedConfigurationWindow(object):
         col.set_cell_data_func(renderer_text, self.cb_value_column_data)
 
         col.props.resizable = True
+        # set max width only for initializing the treeview.
         col.set_max_width(250)
+        gobject.idle_add(col.set_max_width, -1)
 
         renderer_text = gtk.CellRendererText()
         treeview.insert_column_with_attributes(-1, _('Type'),
