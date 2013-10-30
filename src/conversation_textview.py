c7219bede289b3f94bbc54a27bcf7867d16c0397
Yann Leboulanger <asterix@lagaule.org>
2013-10-30 14:02:21 +0100

    fix showing tooltips of emoticons. fixes #7453

--- src/conversation_textview.py
+++ src/conversation_textview.py
@@ -1117,7 +1117,8 @@ class ConversationTextview(gobject.GObject):
             # it's an emoticon
             emot_ascii = possible_emot_ascii_caps
             anchor = buffer_.create_child_anchor(end_iter)
-            img = TextViewImage(anchor, special_text)
+            img = TextViewImage(anchor,
+                gobject.markup_escape_text(special_text))
             animations = gajim.interface.emoticons_animations
             if not emot_ascii in animations:
                 animations[emot_ascii] = gtk.gdk.PixbufAnimation(
