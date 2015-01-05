7b662595d95638a042e38e849a6e91f4600104e8
Yann Leboulanger <asterix@lagaule.org>
2015-01-05 22:34:37 +0100

    [SamWhited] fix Gtk.MenuItem.new_with_label call. Fixes #7916

--- src/groupchat_control.py
+++ src/groupchat_control.py
@@ -593,7 +593,8 @@ class GroupchatControl(ChatControlBase):
 
         for nick in sorted(gajim.contacts.get_nick_list(self.account,
         self.room_jid)):
-            item = Gtk.MenuItem.new_with_label(nick, use_underline=False)
+            item = Gtk.MenuItem.new_with_label(nick)
+            item.set_use_underline(False)
             submenu.append(item)
             id_ = item.connect('activate', self.append_nick_in_msg_textview,
                 nick)
