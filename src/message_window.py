c82a5e7f94e1968e5d20e04d4da0a1d6924204d8
lovetox <forenjunkie@chelo.at>
2016-10-03 23:02:23 +0200

    Restore correct window size in single window mode

--- src/message_window.py
+++ src/message_window.py
@@ -997,16 +997,16 @@ class MessageWindowMgr(gobject.GObject):
         """
         Resizes window according to config settings
         """
+        hpaned = gajim.config.get('roster_hpaned_position')
         if self.mode in (self.ONE_MSG_WINDOW_ALWAYS,
                         self.ONE_MSG_WINDOW_ALWAYS_WITH_ROSTER):
             size = (gajim.config.get('msgwin-width'),
                     gajim.config.get('msgwin-height'))
             if self.mode == self.ONE_MSG_WINDOW_ALWAYS_WITH_ROSTER:
-                parent_size = win.window.get_size()
                 # Need to add the size of the now visible paned handle, otherwise
                 # the saved width of the message window decreases by this amount
                 handle_size = win.parent_paned.style_get_property('handle-size')
-                size = (parent_size[0] + size[0] + handle_size, size[1])
+                size = (hpaned + size[0] + handle_size, size[1])
         elif self.mode == self.ONE_MSG_WINDOW_PERACCT:
             size = (gajim.config.get_per('accounts', acct, 'msgwin-width'),
                     gajim.config.get_per('accounts', acct, 'msgwin-height'))
@@ -1020,7 +1020,7 @@ class MessageWindowMgr(gobject.GObject):
             return
         win.resize(size[0], size[1])
         if win.parent_paned:
-            win.parent_paned.set_position(gajim.config.get('roster_hpaned_position'))
+            win.parent_paned.set_position(hpaned)
 
     def _position_window(self, win, acct, type_):
         """
