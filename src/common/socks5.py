2cb861867309e20e1c13f64206c6d0d19b8a0bf3
lovetox <forenjunkie@chelo.at>
2016-11-05 18:17:55 +0100

    Replace NameError SSL with correctly-imported OpenSSL.SSL.

--- src/common/socks5.py
+++ src/common/socks5.py
@@ -897,8 +897,8 @@ class Socks5(object):
         """
         try:
             buff = self._recv()
-        except (SSL.WantReadError, SSL.WantWriteError,
-        SSL.WantX509LookupError), e:
+        except (OpenSSL.SSL.WantReadError, OpenSSL.SSL.WantWriteError,
+        OpenSSL.SSL.WantX509LookupError) as e:
             log.info("SSL rehandshake request : " + repr(e))
             raise e
         try:
