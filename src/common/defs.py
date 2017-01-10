bae08ea1c957a2207e02ff049b257e97d6b6490a
Philipp Hörist <forenjunkie@chello.at>
2017-01-10 18:20:02 +0100

    Remove line breaks from version string

--- src/common/defs.py
+++ src/common/defs.py
@@ -36,7 +36,7 @@ try:
     node = subprocess.Popen('git rev-parse --short=12 HEAD', shell=True,
         stdout=subprocess.PIPE).communicate()[0]
     if node:
-        version += '-' + node
+        version += '-' + node.replace('\n', '')
 except Exception:
     pass
 
