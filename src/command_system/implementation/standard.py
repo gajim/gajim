432ec7ff7d65aad1e88effd03ea205c788969022
Markus Böhme <markus.boehme@mailbox.org>
2017-03-07 15:57:50 +0100

    Fix usage of logger constants in command system
    The definition of various constants was refactored to use enumerations
    in commit 2fbadc91e9dc. One reference in the command system was not
    changed to use the new enum, however.

--- src/command_system/implementation/standard.py
+++ src/command_system/implementation/standard.py
@@ -24,17 +24,13 @@ import dialogs
 from common import gajim
 from common import helpers
 from common.exceptions import GajimGeneralException
-from common.logger import Constants
+from common.logger import KindConstant
 
 from ..errors import CommandError
 from ..framework import CommandContainer, command, doc
 from ..mapping import generate_usage
 
 from .hosts import ChatCommands, PrivateChatCommands, GroupChatCommands
-
-# This holds constants fron the logger, which we'll be using in some of our
-# commands.
-lc = Constants()
 
 class StandardCommonCommands(CommandContainer):
     """
@@ -107,7 +103,7 @@ class StandardCommonCommands(CommandContainer):
             contact, time, kind, show, message, subject = row
 
             if not contact:
-                if kind == lc.KIND_CHAT_MSG_SENT:
+                if kind == KindConstant.CHAT_MSG_SENT:
                     contact = gajim.nicks[self.account]
                 else:
                     contact = self.contact.name
