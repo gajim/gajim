cf3049f019bdcd5a869d4e5483a8874aa4af97dd
Philipp Hörist <forenjunkie@chello.at>
2017-03-31 22:22:20 +0200

    Dont notify on MUC-PM Sent-Carbons
    Additionally remove all previous Events if we receive a Sent-Carbon,
    because we know the user answered from another device

--- src/groupchat_control.py
+++ src/groupchat_control.py
@@ -1074,11 +1074,23 @@ class GroupchatControl(ChatControlBase):
                     correct_id=(obj.stanza.getID(), None))
         obj.needs_highlight = self.needs_visual_notification(obj.msgtxt)
 
-    def on_private_message(self, nick, msg, tim, xhtml, session, msg_log_id=None,
-    encrypted=False, displaymarking=None):
+    def on_private_message(self, nick, msg, tim, xhtml, session, sent_forwarded,
+    msg_log_id=None, encrypted=False, displaymarking=None):
         # Do we have a queue?
         fjid = self.room_jid + '/' + nick
         no_queue = len(gajim.events.get_events(self.account, fjid)) == 0
+
+        if sent_forwarded:
+            '''
+            We remove all muc-pm events for that jid because the
+            Message was answered from one of our other devices
+            '''
+            gajim.events.remove_events(self.account, fjid, types=['pm'])
+            if self.parent_win:
+                self.parent_win.show_title()
+                self.parent_win.redraw_tab(self)
+            self.update_ui()
+            return
 
         event = events.PmEvent(msg, '', 'incoming', tim, encrypted, '',
             msg_log_id, xhtml=xhtml, session=session, form_node=None,
@@ -1366,8 +1378,10 @@ class GroupchatControl(ChatControlBase):
             # We got a pm from this room
             nick = obj.resource
             typ = ''
+            sent_forwarded = False
             if obj.forwarded and obj.sent:
                 typ = 'out'
+                sent_forwarded = True
             if obj.session.control:
                 # print if a control is open
                 obj.session.control.print_conversation(obj.msgtxt, typ,
@@ -1377,7 +1391,7 @@ class GroupchatControl(ChatControlBase):
             else:
                 # otherwise pass it off to the control to be queued
                 self.on_private_message(nick, obj.msgtxt, obj.timestamp,
-                    obj.xhtml, self.session, msg_log_id=obj.msg_log_id,
+                    obj.xhtml, self.session, sent_forwarded, msg_log_id=obj.msg_log_id,
                     encrypted=obj.encrypted, displaymarking=obj.displaymarking)
 
     def _nec_ping_reply(self, obj):
