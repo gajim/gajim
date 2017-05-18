2cc9d6c51850a437340cda9f42da352c0e1946e2
Philipp Hörist <forenjunkie@chello.at>
2017-05-18 22:02:18 +0200

    Use GTK stock icon for LMC

--- src/conversation_textview.py
+++ src/conversation_textview.py
@@ -168,12 +168,8 @@ class ConversationTextview(GObject.GObject):
             )
     )
 
-    MESSAGE_CORRECTED_PIXBUF = gtkgui_helpers.get_icon_pixmap('gtk-spell-check',
-        quiet=True)
-    if not MESSAGE_CORRECTED_PIXBUF:
-        MESSAGE_CORRECTED_PIXBUF = gtkgui_helpers.get_icon_pixmap(
-            'gajim-message_corrected')
-
+    MESSAGE_CORRECTED_PIXBUF = gtkgui_helpers.get_icon_pixmap(
+        'document-edit-symbolic')
 
     # smooth scroll constants
     MAX_SCROLL_TIME = 0.4 # seconds
