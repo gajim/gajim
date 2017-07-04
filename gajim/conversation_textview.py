gajim/gajim

commit 79716f421f0a153588fc7bcc6505d1ab0d0bac7f

Rework emoticon menu

gajim/conversation_textview.py
@@ -48,6 +48,7 @@ from common import helpers
 from common import i18n
 from calendar import timegm
 from common.fuzzyclock import FuzzyClock
+import emoticons
 
 from htmltextview import HtmlTextView
 from common.exceptions import GajimGeneralException
@@ -952,22 +953,17 @@ class ConversationTextview(GObject.GObject):
             end_iter = iter_
         else:
             end_iter = buffer_.get_end_iter()
-        if gajim.config.get('emoticons_theme') and \
-        possible_emot_ascii_caps in gajim.interface.emoticons.keys() and graphics:
-            # it's an emoticon
-            emot_ascii = possible_emot_ascii_caps
-            anchor = buffer_.create_child_anchor(end_iter)
-            img = TextViewImage(anchor,
-                GLib.markup_escape_text(special_text))
-            animations = gajim.interface.emoticons_animations
-            if not emot_ascii in animations:
-                animations[emot_ascii] = GdkPixbuf.PixbufAnimation.new_from_file(
-                    gajim.interface.emoticons[emot_ascii])
-            img.set_from_animation(animations[emot_ascii])
-            img.show()
-            self.images.append(img)
-            # add with possible animation
-            self.tv.add_child_at_anchor(img, anchor)
+        if gajim.config.get('emoticons_theme') and graphics:
+            pixbuf = emoticons.get_pixbuf(possible_emot_ascii_caps)
+            if pixbuf:
+                # it's an emoticon
+                anchor = buffer_.create_child_anchor(end_iter)
+                img = TextViewImage(anchor,
+                    GLib.markup_escape_text(special_text))
+                img.set_from_pixbuf(pixbuf)
+                img.show()
+                self.images.append(img)
+                self.tv.add_child_at_anchor(img, anchor)
         elif special_text.startswith('www.') or \
             special_text.startswith('ftp.') or \
             text_is_valid_uri and not is_xhtml_link:

data/emoticons/Makefile.am
@@ -1,7 +1,7 @@
 emoticonsdir = $(pkgdatadir)/data/emoticons
 nobase_dist_emoticons_DATA = \
 	$(srcdir)/*/*.png \
-	$(srcdir)/*/*.gif \
-	$(srcdir)/*/emoticons.py
+	$(srcdir)/*/LICENSE \
+	$(srcdir)/*/emoticons_theme.py
 
 MAINTAINERCLEANFILES = Makefile.in

data/gui/chat_control.ui
@@ -664,23 +664,17 @@
                 <property name="visible">True</property>
                 <property name="can_focus">False</property>
                 <child>
-                  <object class="GtkButton" id="emoticons_button">
+                  <object class="GtkMenuButton" id="emoticons_button">
                     <property name="visible">True</property>
                     <property name="can_focus">True</property>
-                    <property name="focus_on_click">False</property>
                     <property name="receives_default">True</property>
-                    <property name="events">GDK_POINTER_MOTION_MASK | GDK_POINTER_MOTION_HINT_MASK | GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK</property>
-                    <property name="has_tooltip">True</property>
-                    <property name="tooltip_markup" translatable="yes">Show a list of emoticons (Alt+M)</property>
                     <property name="tooltip_text" translatable="yes">Show a list of emoticons (Alt+M)</property>
                     <property name="relief">none</property>
                     <child>
-                      <object class="GtkImage" id="emoticons_button_image">
+                      <object class="GtkImage">
                         <property name="visible">True</property>
                         <property name="can_focus">False</property>
-                        <property name="events">GDK_POINTER_MOTION_MASK | GDK_POINTER_MOTION_HINT_MASK | GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK</property>
-                        <property name="stock">gtk-missing-image</property>
-                        <property name="icon_size">1</property>
+                        <property name="icon_name">face-smile</property>
                       </object>
                     </child>
                   </object>

data/gui/groupchat_control.ui
@@ -224,20 +224,17 @@
             <property name="visible">True</property>
             <property name="can_focus">False</property>
             <child>
-              <object class="GtkButton" id="emoticons_button">
+              <object class="GtkMenuButton" id="emoticons_button">
                 <property name="visible">True</property>
                 <property name="can_focus">True</property>
                 <property name="receives_default">True</property>
-                <property name="events">GDK_POINTER_MOTION_MASK | GDK_POINTER_MOTION_HINT_MASK | GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK</property>
-                <property name="has_tooltip">True</property>
                 <property name="tooltip_text" translatable="yes">Show a list of emoticons (Alt+M)</property>
                 <property name="relief">none</property>
                 <child>
-                  <object class="GtkImage" id="emoticons_button_image">
+                  <object class="GtkImage">
                     <property name="visible">True</property>
                     <property name="can_focus">False</property>
-                    <property name="stock">gtk-missing-image</property>
-                    <property name="icon_size">1</property>
+                    <property name="icon_name">face-smile</property>
                   </object>
                 </child>
               </object>

data/style/gajim.css
@@ -5,3 +5,10 @@
 
 /* VCardWindow */
 .VCard-GtkLinkButton { padding-left: 5px; border-left: none; }
+
+popover#EmoticonPopover button { background: none; border: none; box-shadow:none; padding: 0px;}
+popover#EmoticonPopover button > label { font-size: 24px; }
+popover#EmoticonPopover flowboxchild > label { font-size: 24px; }
+popover#EmoticonPopover notebook label { font-size: 24px; }
+popover#EmoticonPopover flowbox { padding-left: 5px; padding-right: 6px; }
+popover#EmoticonPopover flowboxchild { padding-top: 5px; padding-bottom: 5px; }

gajim/chat_control.py
@@ -102,6 +102,8 @@ class ChatControl(ChatControlBase):
         self.handlers[id_] = self.actions_button
 
         self._formattings_button = self.xml.get_object('formattings_button')
+        self.emoticons_button = self.xml.get_object('emoticons_button')
+        self.toggle_emoticons()
 
         self._add_to_roster_button = self.xml.get_object(
             'add_to_roster_button')
@@ -325,8 +327,6 @@ class ChatControl(ChatControlBase):
         if (gajim.connections[self.account].connected > 1 and not \
         self.TYPE_ID == 'pm') or (self.contact.show != 'offline' and \
         self.TYPE_ID == 'pm'):
-            emoticons_button = self.xml.get_object('emoticons_button')
-            emoticons_button.set_sensitive(True)
             send_button = self.xml.get_object('send_button')
             send_button.set_sensitive(True)
         # Formatting
@@ -1663,15 +1663,11 @@ class ChatControl(ChatControlBase):
         if contact:
             self.contact = contact
         self.draw_banner()
-        emoticons_button = self.xml.get_object('emoticons_button')
-        emoticons_button.set_sensitive(True)
         send_button = self.xml.get_object('send_button')
         send_button.set_sensitive(True)
 
     def got_disconnected(self):
         # Emoticons button
-        emoticons_button = self.xml.get_object('emoticons_button')
-        emoticons_button.set_sensitive(False)
         send_button = self.xml.get_object('send_button')
         send_button.set_sensitive(False)
         # Add to roster

gajim/chat_control_base.py
@@ -43,6 +43,7 @@ import history_window
 import notify
 import re
 
+import emoticons
 from common import events
 from common import gajim
 from common import helpers
@@ -263,12 +264,6 @@ class ChatControlBase(MessageControl, ChatCommandProcessor, CommandTools):
         id_ = widget.connect('clicked', self._on_history_menuitem_activate)
         self.handlers[id_] = widget
 
-        # when/if we do XHTML we will put formatting buttons back
-        widget = self.xml.get_object('emoticons_button')
-        widget.set_sensitive(False)
-        id_ = widget.connect('clicked', self.on_emoticons_button_clicked)
-        self.handlers[id_] = widget
-
         # Create banner and connect signals
         widget = self.xml.get_object('banner_eventbox')
         id_ = widget.connect('button-press-event',
@@ -364,13 +359,7 @@ class ChatControlBase(MessageControl, ChatCommandProcessor, CommandTools):
         self.received_history_pos = 0
         self.orig_msg = None
 
-        # Emoticons menu
-        # set image no matter if user wants at this time emoticons or not
-        # (so toggle works ok)
-        img = self.xml.get_object('emoticons_button_image')
-        img.set_from_file(os.path.join(gajim.DATA_DIR, 'emoticons', 'static',
-            'smile.png'))
-        self.toggle_emoticons()
+        self.set_emoticon_popover()
 
         # Attach speller
         if gajim.config.get('use_speller') and HAS_GTK_SPELL:
@@ -568,6 +557,7 @@ class ChatControlBase(MessageControl, ChatCommandProcessor, CommandTools):
         When send button is pressed: send the current message
         """
         message_buffer = self.msg_textview.get_buffer()
+        emoticons.replace_with_codepoint(message_buffer)
         start_iter = message_buffer.get_start_iter()
         end_iter = message_buffer.get_end_iter()
         message = message_buffer.get_text(start_iter, end_iter, False)
@@ -587,12 +577,6 @@ class ChatControlBase(MessageControl, ChatCommandProcessor, CommandTools):
             return False
         self.parent_win.notebook.event(event)
         return True
-
-    def show_emoticons_menu(self):
-        if not gajim.config.get('emoticons_theme'):
-            return
-        gajim.interface.emoticon_menuitem_clicked = self.append_emoticon
-        gajim.interface.emoticons_menu.popup(None, None, None, None, 1, 0)
 
     def _on_message_textview_key_press_event(self, widget, event):
         if event.keyval == Gdk.KEY_space:
@@ -683,6 +667,7 @@ class ChatControlBase(MessageControl, ChatCommandProcessor, CommandTools):
         event.keyval == Gdk.KEY_KP_Enter:  # ENTER
             message_textview = widget
             message_buffer = message_textview.get_buffer()
+            emoticons.replace_with_codepoint(message_buffer)
             start_iter, end_iter = message_buffer.get_bounds()
             message = message_buffer.get_text(start_iter, end_iter, False)
             xhtml = self.msg_textview.get_xhtml()
@@ -1012,31 +997,26 @@ class ChatControlBase(MessageControl, ChatCommandProcessor, CommandTools):
 
     def toggle_emoticons(self):
         """
-        Hide show emoticons_button and make sure emoticons_menu is always there
-        when needed
+        Hide show emoticons_button
         """
-        emoticons_button = self.xml.get_object('emoticons_button')
         if gajim.config.get('emoticons_theme'):
-            emoticons_button.show()
-            emoticons_button.set_no_show_all(False)
+            self.emoticons_button.set_no_show_all(False)
+            self.emoticons_button.show()
         else:
-            emoticons_button.hide()
-            emoticons_button.set_no_show_all(True)
+            self.emoticons_button.set_no_show_all(True)
+            self.emoticons_button.hide()
 
-    def append_emoticon(self, str_):
-        buffer_ = self.msg_textview.get_buffer()
-        if buffer_.get_char_count():
-            buffer_.insert_at_cursor(' %s ' % str_)
-        else: # we are the beginning of buffer
-            buffer_.insert_at_cursor('%s ' % str_)
-        self.msg_textview.grab_focus()
+    def set_emoticon_popover(self):
+        if not gajim.config.get('emoticons_theme'):
+            return
 
-    def on_emoticons_button_clicked(self, widget):
-        """
-        Popup emoticons menu
-        """
-        gajim.interface.emoticon_menuitem_clicked = self.append_emoticon
-        gajim.interface.popup_emoticons_under_button(widget, self.parent_win)
+        if not self.parent_win:
+            return
+
+        popover = emoticons.get_popover()
+        popover.set_callbacks(self.msg_textview)
+        emoticons_button = self.xml.get_object('emoticons_button')
+        emoticons_button.set_popover(popover)
 
     def on_color_menuitem_activate(self, widget):
         color_dialog = Gtk.ColorChooserDialog(None, self.parent_win.window)
@@ -1139,6 +1119,7 @@ class ChatControlBase(MessageControl, ChatCommandProcessor, CommandTools):
 
     def set_control_active(self, state):
         if state:
+            self.set_emoticon_popover()
             jid = self.contact.jid
             if self.was_at_the_end:
                 # we are at the end

gajim/config.py
@@ -657,7 +657,7 @@ class PreferencesWindow:
         else:
             gajim.config.set('emoticons_theme', emot_theme)
 
-        gajim.interface.init_emoticons(need_reload = True)
+        gajim.interface.init_emoticons()
         gajim.interface.make_regexps()
         self.toggle_emoticons()
 

commit ab85729fe54e4f427a7dbaf3f7e2a83b6920930a

Fix displaying links in textview
Regression from https://dev.gajim.org/gajim/gajim/commit/79716f421f0a153588fc7bcc6505d1ab0d0bac7f

gajim/conversation_textview.py
@@ -953,17 +953,17 @@ class ConversationTextview(GObject.GObject):
             end_iter = iter_
         else:
             end_iter = buffer_.get_end_iter()
-        if gajim.config.get('emoticons_theme') and graphics:
-            pixbuf = emoticons.get_pixbuf(possible_emot_ascii_caps)
-            if pixbuf:
-                # it's an emoticon
-                anchor = buffer_.create_child_anchor(end_iter)
-                img = TextViewImage(anchor,
-                    GLib.markup_escape_text(special_text))
-                img.set_from_pixbuf(pixbuf)
-                img.show()
-                self.images.append(img)
-                self.tv.add_child_at_anchor(img, anchor)
+
+        pixbuf = emoticons.get_pixbuf(possible_emot_ascii_caps)
+        if gajim.config.get('emoticons_theme') and pixbuf and graphics:
+            # it's an emoticon
+            anchor = buffer_.create_child_anchor(end_iter)
+            img = TextViewImage(anchor,
+                GLib.markup_escape_text(special_text))
+            img.set_from_pixbuf(pixbuf)
+            img.show()
+            self.images.append(img)
+            self.tv.add_child_at_anchor(img, anchor)
         elif special_text.startswith('www.') or \
             special_text.startswith('ftp.') or \
             text_is_valid_uri and not is_xhtml_link: