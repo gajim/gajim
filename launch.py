b11b7a4a95b7aed50abaf4e7e63d76492babc06a
André Apitzsch <git@apitzsch.eu>
2017-07-17 21:36:17 +0200

    Check for root only on unix systems

--- launch.py
+++ launch.py
@@ -3,8 +3,9 @@
 import os
 import sys
 
-if os.geteuid() == 0:
-    sys.exit("You must not launch gajim as root, it is insecure.")
+if sys.platform != 'win32':
+    if os.geteuid() == 0:
+        sys.exit("You must not launch gajim as root, it is insecure.")
 
 import gajim.gajim as g
 
