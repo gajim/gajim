f97b799f9a4b603aad451238785f6c734ba8d165
André Apitzsch <git@apitzsch.eu>
2017-11-23 00:22:40 +0100

    Fix invalid dev version string

--- gajim/__init__.py
+++ gajim/__init__.py
@@ -6,7 +6,7 @@ try:
     node = subprocess.Popen('git rev-parse --short=12 HEAD', shell=True,
         stdout=subprocess.PIPE).communicate()[0]
     if node:
-        __version__ += '-' + node.decode('utf-8').strip()
+        __version__ += '+' + node.decode('utf-8').strip()
 except Exception:
     pass
 
