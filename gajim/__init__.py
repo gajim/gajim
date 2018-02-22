84748928d41e11bc57d5b7de7d5f0fb59ffaf7b9
André Apitzsch <git@apitzsch.eu>
2018-02-23 00:16:24 +0100

    Silence 'git unavailable' message

--- gajim/__init__.py
+++ gajim/__init__.py
@@ -3,10 +3,10 @@ import subprocess
 __version__ = "0.99.2"
 
 try:
-    node = subprocess.Popen('git rev-parse --short=12 HEAD', shell=True,
-        stdout=subprocess.PIPE).communicate()[0]
+    p = subprocess.Popen('git rev-parse --short=12 HEAD', shell=True,
+                          stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    node = p.communicate()[0]
     if node:
         __version__ += '+' + node.decode('utf-8').strip()
 except Exception:
     pass
-
