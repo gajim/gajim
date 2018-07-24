f903470130959aeeef9fa9a74674699765a237a0
Philipp Hörist <forenjunkie@chello.at>
2018-07-24 23:32:27 +0200

    Windows build: Install ctypes from git
    The version on pip is broken with python 3.7

--- win/_base.sh
+++ win/_base.sh
@@ -92,6 +92,7 @@ function install_deps {
     PIP_REQUIREMENTS="\
 git+https://dev.gajim.org/gajim/python-nbxmpp.git
 git+https://dev.gajim.org/lovetox/pybonjour-python3.git
+git+https://github.com/enthought/pywin32-ctypes.git
 keyring
 python-gnupg
 python-axolotl
