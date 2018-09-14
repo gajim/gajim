64840cd0b0477799f1cbc63c91c7ad1dbbf56d90
Philipp Hörist <forenjunkie@chello.at>
2018-09-14 23:17:16 +0200

    Update win/dev_env.sh

--- win/dev_env.sh
+++ win/dev_env.sh
@@ -5,38 +5,25 @@ set -e
 function main {
     pacman --noconfirm -S --needed \
         git \
-        mingw-w64-i686-toolchain \
-        mingw-w64-i686-gdk-pixbuf2 \
-        mingw-w64-i686-gtk3 \
-        mingw-w64-i686-gstreamer \
-        intltool \
-        mingw-w64-i686-sqlite3 \
-        mingw-w64-i686-python3 \
-        mingw-w64-i686-python3-gobject \
-        mingw-w64-i686-python3-pip \
-        mingw-w64-i686-sqlite3
-
-    pip3 install setuptools_scm
+        mingw-w64-x86_64-python3 \
+        mingw-w64-x86_64-python3-gobject \
+        mingw-w64-x86_64-python3-pip \
+        mingw-w64-x86_64-toolchain \
+        mingw-w64-x86_64-gtk3 \
+        mingw-w64-x86_64-python3-pyopenssl \
+        mingw-w64-x86_64-python3-pillow
 
     PIP_REQUIREMENTS="\
 certifi
 git+https://dev.gajim.org/gajim/python-nbxmpp.git
-git+https://dev.gajim.org/lovetox/pybonjour-python3.git 
-protobuf
-cryptography
-pyopenssl
+git+https://dev.gajim.org/lovetox/pybonjour-python3.git
+python-axolotl
 python-gnupg
-docutils
-qrcode
 keyring
-pillow==4.3.0
 cssutils
 "
 
-    pip3 install --no-binary ":all:" \
-        --force-reinstall $(echo "$PIP_REQUIREMENTS" | tr ["\\n"] [" "])
-
-    pip3 install python-axolotl
+pip3 install $(echo "$PIP_REQUIREMENTS" | tr ["\\n"] [" "])
 
 }
 
