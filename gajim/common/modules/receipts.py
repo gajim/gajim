gajim/gajim

commit e30506979899bcb338010a4f59e0c2133fa778f4

Fix receipts for MUC-PMs

gajim/common/modules/receipts.py
@@ -72,14 +72,14 @@ class Receipts:
         self._con.connection.send(receipt)
 
     def _get_contact(self, event):
-        if event.mtype == 'chat':
-            contact = app.contacts.get_contact(self._account, event.jid)
-            if contact and contact.sub not in ('to', 'none'):
-                return contact
-        else:
+        if event.muc_pm:
             return app.contacts.get_gc_contact(self._account,
                                                event.jid,
                                                event.resource)
+
+        contact = app.contacts.get_contact(self._account, event.jid)
+        if contact is None and contact.sub not in ('to', 'none'):
+            return contact
 
     @staticmethod
     def _build_answer_receipt(to, receipt_id):
@@ -95,11 +95,16 @@ class Receipts:
             log.warning('Receipt without ID: %s', event.stanza)
             return
         log.info('Received %s', receipt_id)
+
+        jid = event.jid
+        if event.muc_pm:
+            jid = event.fjid
+
         app.nec.push_incoming_event(
             NetworkIncomingEvent('receipt-received',
                                  conn=self._con,
                                  receipt_id=receipt_id,
-                                 jid=event.jid))
+                                 jid=jid))
 
 
 def get_instance(*args, **kwargs):

commit f942e2c70faf2229a59a24b8293712ac7916337a

Fix receipts for chat
This is a regression from e3050697

gajim/common/modules/receipts.py
@@ -78,7 +78,7 @@ class Receipts:
                                                event.resource)
 
         contact = app.contacts.get_contact(self._account, event.jid)
-        if contact is None and contact.sub not in ('to', 'none'):
+        if contact is not None and contact.sub not in ('to', 'none'):
             return contact
 
     @staticmethod