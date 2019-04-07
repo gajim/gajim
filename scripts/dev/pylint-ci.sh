d55cb97dd294e30aa5b65185e62edf9b7268d56f
Philipp Hörist <philipp@hoerist.com>
2019-04-07 20:41:27 +0200

    Update pylintrc
    - Ignore some pylint errors only in CI, not locally
    - Set max line length to 80

--- scripts/dev/pylint-ci.sh
+++ scripts/dev/pylint-ci.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 PYLINT=${PYLINT:-pylint}
 
-"$PYLINT" --disable=C0103,C0302,C0330,C0411,C0412,C0413,E0203,E0401,E0611,E0710,E0712,E1101,E1102,E1133,E1136,R0201,R0901,R0904,R0913,R0916,R1702,W0201,W0212,W0221,W0223,W0311,W0401,W0603,W0613 "$@"
+"$PYLINT" --disable=C0103,C0302,C0301,C0330,C0411,C0412,C0413,E0203,E0401,E0611,E0710,E0712,E1101,E1102,E1133,E1136,R0201,R0901,R0904,R0912,R0913,R0914,R0915,R0916,R1702,R1710,W0201,W0212,W0221,W0223,W0231,W0233,W0311,W0401,W0603,W0613 "$@"
 
 # C0103 invalid-name
+# C0301 line-too-long
 # C0302 too-many-lines
 # C0330 bad-continuation
 # C0411 wrong-import-order
@@ -22,12 +23,18 @@ PYLINT=${PYLINT:-pylint}
 # R0901 too-many-ancestors
 # R0904 too-many-public-methods
 # R0913 too-many-arguments
+# R0912 too-many-branches
+# R0914 too-many-locals
+# R0915 too-many-statements
 # R0916 too-many-boolean-expressions
 # R1702 too-many-nested-blocks
+# R1710 inconsistent-return-statements
 # W0201 attribute-defined-outside-init
 # W0212 protected-access
 # W0221 arguments-differ
 # W0223 abstract-method
+# W0231 super-init-not-called
+# W0233 non-parent-init-called
 # W0311 bad-indentation
 # W0401 wildcard-import
 # W0603 global-statement
