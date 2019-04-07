32168c60f572c800ec4f6fc3bd1f612264883484
Philipp Hörist <philipp@hoerist.com>
2019-04-07 17:11:30 +0200

    Fix license header

--- gajim/gtk/adhoc.py
+++ gajim/gtk/adhoc.py
@@ -1,18 +1,16 @@
-# Copyright (C) 2019 Philipp Hörist <philipp AT hoerist.com>
+# This file is part of Gajim.
 #
-# This file is part of the OpenPGP Gajim Plugin.
-#
-# OpenPGP Gajim Plugin is free software; you can redistribute it and/or modify
+# Gajim is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published
 # by the Free Software Foundation; version 3 only.
 #
-# OpenPGP Gajim Plugin is distributed in the hope that it will be useful,
+# Gajim is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with OpenPGP Gajim Plugin. If not, see <http://www.gnu.org/licenses/>.
+# along with Gajim. If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 from enum import IntEnum
