54513a172c62463a2a06862eac4446f4890da85e
Philipp Hörist <philipp@hoerist.com>
2019-12-29 17:09:48 +0100

    Remove unused import

--- gajim/gtk/filetransfer_progress.py
+++ gajim/gtk/filetransfer_progress.py
@@ -12,7 +12,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Gajim. If not, see <http://www.gnu.org/licenses/>.
 
-import os
 import time
 
 from gi.repository import Gtk
