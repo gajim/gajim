f8e137fa6df31e456190d93ae6e7583127129c80
lovetox <philipp@hoerist.com>
2020-06-02 10:34:01 +0200

    Update pre-push-tests.sh

--- scripts/dev/pre-push-tests.sh
+++ scripts/dev/pre-push-tests.sh
@@ -1,4 +1,15 @@
-SCRIPTDIR=$(dirname $0)
+#!/bin/sh
 
+# exit when any command fails
+set -e
+
+GREEN='\033[0;32m'
+
+
+./scripts/dev/pylint-ci.sh gajim
+echo "${GREEN}pylint: \t OK"
 mypy gajim
-${SCRIPTDIR}/pylint-ci.sh --jobs=0 gajim
+echo "${GREEN}mypy: \t\t OK"
+codespell -I codespell.conf --skip="*__pycache__*,gajim/data/icons,gajim/data/sounds,gajim/data/emoticons" gajim
+echo "${GREEN}codespell: \t OK"
+echo "${GREEN}ready to push"
