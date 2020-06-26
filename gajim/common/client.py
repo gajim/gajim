06a7db81fdfe6eee1117285ab0b5dbf18d01156a
lovetox <philipp@hoerist.com>
2020-06-26 19:48:48 +0200

    Remove unused import

--- gajim/common/client.py
+++ gajim/common/client.py
@@ -12,7 +12,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Gajim.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
 import logging
 
 import nbxmpp
