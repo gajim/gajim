88376197854199ecacf2afca2d479e259b74d2ae
Philipp Hörist <philipp@hoerist.com>
2020-12-06 14:08:05 +0100

    Update dev_env.sh

--- win/dev_env.sh
+++ win/dev_env.sh
@@ -5,19 +5,19 @@ set -e
 function main {
     pacman --noconfirm -S --needed \
         git \
-        mingw-w64-x86_64-python3 \
-        mingw-w64-x86_64-python3-gobject \
-        mingw-w64-x86_64-python3-pip \
+        mingw-w64-x86_64-python \
+        mingw-w64-x86_64-python-gobject \
+        mingw-w64-x86_64-python-pip \
         mingw-w64-x86_64-toolchain \
         mingw-w64-x86_64-adwaita-icon-theme \
         mingw-w64-x86_64-gtk3 \
-        mingw-w64-x86_64-python3-setuptools-scm \
-        mingw-w64-x86_64-python3-cryptography \
-        mingw-w64-x86_64-python3-certifi \
-        mingw-w64-x86_64-python3-pyopenssl \
-        mingw-w64-x86_64-python3-pillow \
-        mingw-w64-x86_64-python3-six \
-        mingw-w64-x86_64-python3-pygments \
+        mingw-w64-x86_64-python-setuptools-scm \
+        mingw-w64-x86_64-python-cryptography \
+        mingw-w64-x86_64-python-certifi \
+        mingw-w64-x86_64-python-pyopenssl \
+        mingw-w64-x86_64-python-pillow \
+        mingw-w64-x86_64-python-six \
+        mingw-w64-x86_64-python-pygments \
         mingw-w64-x86_64-libwebp \
         mingw-w64-x86_64-goocanvas \
         mingw-w64-x86_64-gspell \
