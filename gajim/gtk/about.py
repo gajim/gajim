b894e2ba8c35a1c026e91498887fcc0ec1ec5be9
André Apitzsch <git@apitzsch.eu>
2021-01-14 23:15:49 +0100

    Update year

--- gajim/gtk/about.py
+++ gajim/gtk/about.py
@@ -34,7 +34,7 @@ class AboutDialog(Gtk.AboutDialog):
         self.set_transient_for(app.interface.roster.window)
         self.set_name('Gajim')
         self.set_version(app.version)
-        self.set_copyright('Copyright © 2003-2020 Gajim Team')
+        self.set_copyright('Copyright © 2003-2021 Gajim Team')
         self.set_license_type(Gtk.License.GPL_3_0_ONLY)
         self.set_website('https://gajim.org/')
 
