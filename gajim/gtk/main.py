22110d6e900b9d0718b5225748a1d3c24c08e425
lovetox <philipp@hoerist.com>
2021-01-17 21:49:13 +0100

    Store and Open chats

--- gajim/gtk/main.py
+++ gajim/gtk/main.py
@@ -5,7 +5,6 @@ from gajim.common import app
 from gajim.gui.util import get_builder
 from gajim.gui.chat_list_stack import ChatListStack
 from gajim.gui.chat_stack import ChatStack
-from gajim.gui.util import get_app_window
 
 
 class MainWindow(Gtk.ApplicationWindow):
@@ -19,6 +18,10 @@ class MainWindow(Gtk.ApplicationWindow):
         self.window = self
         app.window = self
 
+        self._active_workspace = None
+
+        self._startup_finished = False
+
         self._ui = get_builder('main.ui')
 
         self.add(self._ui.main_grid)
@@ -29,8 +32,6 @@ class MainWindow(Gtk.ApplicationWindow):
         self._ui.left_grid.add(self._chat_list_stack)
         self._ui.right_grid.add(self._chat_stack)
         self._ui.connect_signals(self)
-
-        self._chat_list_stack.add_chat_list('default')
 
         self.show_all()
 
@@ -51,20 +52,30 @@ class MainWindow(Gtk.ApplicationWindow):
     def get_active_jid(self, *args):
         pass
 
+    def get_active_workspace(self):
+        # TODO
+        return app.settings.get_workspaces()[0]
+
     def add_chat(self, account, jid):
+        workspace_id = self.get_active_workspace()
+        self.add_chat_for_workspace(workspace_id, account, jid)
+
+    def add_chat_for_workspace(self, workspace_id, account, jid):
         self._chat_stack.add_chat(account, jid)
-        self._chat_list_stack.add_chat('default', account, jid)
-        self._chat_list_stack.select_chat('default', account, jid)
+        self._chat_list_stack.add_chat(workspace_id, account, jid)
+
+        if self._startup_finished:
+            self._chat_list_stack.select_chat(workspace_id, account, jid)
+            self._chat_list_stack.store_open_chats(workspace_id)
 
     def get_control(self, account, jid):
         return self._chat_stack.get_control(account, jid)
 
     def _load_chats(self):
-        for account in app.settings.get_accounts():
-            jids = app.settings.get_account_setting(
-                account, 'opened_chat_controls')
-            if not jids:
-                continue
+        for workspace_id in app.settings.get_workspaces():
+            open_chats = app.settings.get_workspace_setting(workspace_id,
+                                                            'open_chats')
+            for account, jid in open_chats:
+                self.add_chat_for_workspace(workspace_id, account, jid)
 
-            for jid in jids.split(','):
-                self.add_chat(account, jid)
+        self._startup_finished = True
