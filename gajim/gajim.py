0b6ff25aae44a7d9c78d413e05326bdac79c6be4
lovetox <philipp@hoerist.com>
2021-02-06 21:19:56 +0100

    Raise nbxmpp version

--- gajim/gajim.py
+++ gajim/gajim.py
@@ -28,7 +28,7 @@ import gajim.gui
 from gajim.common import i18n
 
 
-_MIN_NBXMPP_VER = '2.0.0'
+_MIN_NBXMPP_VER = '2.0.1'
 _MIN_GTK_VER = '3.22.27'
 _MIN_CAIRO_VER = '1.16.0'
 _MIN_PYGOBJECT_VER = '3.32.0'
