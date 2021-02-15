bb5d0b9b5fd195580d5675421e180ebfdbfbf601
lovetox <philipp@hoerist.com>
2021-02-15 16:22:04 +0100

    Remove unused code

--- gajim/common/modules/vcard_temp.py
+++ gajim/common/modules/vcard_temp.py
@@ -42,10 +42,6 @@ class VCardTemp(BaseModule):
         self.supported = True
         self._log.info('Discovered vcard-temp: %s', info.jid)
 
-        app.nec.push_incoming_event(NetworkEvent('feature-discovered',
-                                                 account=self._account,
-                                                 feature=Namespace.VCARD))
-
 
 def get_instance(*args, **kwargs):
     return VCardTemp(*args, **kwargs), 'VCardTemp'
