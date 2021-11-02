9058a28a700f8f8f700687fff7261a4114954079
lovetox <philipp@hoerist.com>
2021-11-02 09:56:24 +0100

    Bookmarks: Make Dialog read only

--- gajim/gtk/bookmarks.py
+++ gajim/gtk/bookmarks.py
@@ -13,30 +13,17 @@
 # along with Gajim. If not, see <http://www.gnu.org/licenses/>.
 
 import logging
-from enum import IntEnum
 
 from gi.repository import Gtk
 from gi.repository import Gdk
-from nbxmpp.structs import BookmarkData
-from nbxmpp.protocol import validate_resourcepart
-from nbxmpp.protocol import JID
 
 from gajim.common import app
-from gajim.common.helpers import validate_jid
 from gajim.common.i18n import _
 
 from .util import get_builder
 
 
 log = logging.getLogger('gajim.gui.bookmarks')
-
-
-class Column(IntEnum):
-    ADDRESS = 0
-    NAME = 1
-    NICK = 2
-    PASSWORD = 3
-    AUTOJOIN = 4
 
 
 class Bookmarks(Gtk.ApplicationWindow):
@@ -71,89 +58,6 @@ class Bookmarks(Gtk.ApplicationWindow):
     def _on_key_press(self, _widget, event):
         if event.keyval == Gdk.KEY_Escape:
             self.destroy()
-
-    def _on_selection_changed(self, selection):
-        _, iter_ = selection.get_selected()
-        self._ui.remove_button.set_sensitive(iter_ is not None)
-
-    def _on_address_edited(self, _renderer, path, new_value):
-        iter_ = self._ui.bookmarks_store.get_iter(path)
-        if not new_value:
-            return
-
-        try:
-            jid = validate_jid(new_value)
-        except ValueError as error:
-            log.warning('Invalid JID: %s (%s)', error, new_value)
-            return
-
-        if not jid.is_bare:
-            log.warning('Invalid JID: only bare JIDs allowed (%s)', jid)
-            return
-
-        self._ui.bookmarks_store.set_value(iter_,
-                                           Column.ADDRESS,
-                                           new_value or None)
-
-    def _on_name_edited(self, _renderer, path, new_value):
-        iter_ = self._ui.bookmarks_store.get_iter(path)
-        self._ui.bookmarks_store.set_value(iter_,
-                                           Column.NAME,
-                                           new_value or None)
-
-    def _on_nick_edited(self, _renderer, path, new_value):
-        iter_ = self._ui.bookmarks_store.get_iter(path)
-
-        if new_value:
-            try:
-                validate_resourcepart(new_value)
-            except ValueError as error:
-                log.warning('Invalid nickname: %s', error)
-                return
-
-        self._ui.bookmarks_store.set_value(iter_,
-                                           Column.NICK,
-                                           new_value or None)
-
-    def _on_password_edited(self, _renderer, path, new_value):
-        iter_ = self._ui.bookmarks_store.get_iter(path)
-        self._ui.bookmarks_store.set_value(iter_,
-                                           Column.PASSWORD,
-                                           new_value or None)
-
-    def _on_autojoin_toggled(self, _renderer, path):
-        iter_ = self._ui.bookmarks_store.get_iter(path)
-        new_value = not self._ui.bookmarks_store[iter_][Column.AUTOJOIN]
-        self._ui.bookmarks_store.set_value(iter_, Column.AUTOJOIN, new_value)
-
-    def _on_add_clicked(self, _button):
-        iter_ = self._ui.bookmarks_store.append([None, None, None, None, False])
-        self._ui.bookmarks_view.get_selection().select_iter(iter_)
-        path = self._ui.bookmarks_store.get_path(iter_)
-        self._ui.bookmarks_view.scroll_to_cell(path, None, False)
-
-    def _on_remove_clicked(self, _button):
-        mod, paths = self._ui.bookmarks_view.get_selection().get_selected_rows()
-        for path in paths:
-            iter_ = mod.get_iter(path)
-            self._ui.bookmarks_store.remove(iter_)
-
-    def _on_apply_clicked(self, _button):
-        bookmarks = []
-        for row in self._ui.bookmarks_store:
-            if not row[Column.ADDRESS]:
-                continue
-
-            bookmark = BookmarkData(jid=JID.from_string(row[Column.ADDRESS]),
-                                    name=row[Column.NAME],
-                                    autojoin=row[Column.AUTOJOIN],
-                                    password=row[Column.PASSWORD],
-                                    nick=row[Column.NICK])
-            bookmarks.append(bookmark)
-
-        con = app.connections[self.account]
-        con.get_module('Bookmarks').store_difference(bookmarks)
-        self.destroy()
 
     @staticmethod
     def _search_func(model, _column, search_text, iter_):
