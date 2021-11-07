944ad9cc93ce1d93fc7d26528407bfe3d75a70ec
lovetox <philipp@hoerist.com>
2021-11-07 16:18:04 +0100

    Configpaths: Fix get_paths()
    _paths is not a dict we can’t use values()

--- gajim/common/configpaths.py
+++ gajim/common/configpaths.py
@@ -53,11 +53,12 @@ def get_plugin_dirs() -> List[Path]:
 
 
 def get_paths(type_: PathType) -> Generator[Path, None, None]:
-    for value in _paths.values():
+    # pylint: disable=unnecessary-dict-index-lookup
+    for key, value in _paths.items():
         path_type = value[2]
         if type_ != path_type:
             continue
-        yield value
+        yield _paths[key]
 
 
 def override_path(*args, **kwargs):
