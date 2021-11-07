064f77e9451f045773452b3788da77809f320e14
lovetox <philipp@hoerist.com>
2021-11-07 15:43:23 +0100

    Update pylint.sh

--- scripts/ci/pylint.sh
+++ scripts/ci/pylint.sh
@@ -5,7 +5,7 @@ set -e
 
 PYLINT=${PYLINT:-pylint}
 
-IGNORE_ALWAYS='R0801,C0209'
+IGNORE_ALWAYS='R0801,C0209,W0237,W0707,R1732,W1404,W1406,R1725'
 
 IGNORE_ERRORS=C0103,C0302,C0301,C0330,C0411,C0415,E0401,E0611,E1101,E1133,E1136,R0201,R0901,R0904,R0912,R0913,R0914,R0915,R0916,R1702,R1710,W0201,W0212,W0221,W0223,W0231,W0233,W0603,W0613
 
@@ -40,11 +40,17 @@ IGNORE_GTK_MODULE_ERRORS=C0103,C0301,C0330,C0415,E0401,E0611,E1101,E1133,E1136,R
 # R0916 too-many-boolean-expressions
 # R1702 too-many-nested-blocks
 # R1710 inconsistent-return-statements
+# R1732 consider-using-with
+# R1725 super-with-arguments
 # W0201 attribute-defined-outside-init
 # W0212 protected-access
 # W0221 arguments-differ
 # W0223 abstract-method
 # W0231 super-init-not-called
 # W0233 non-parent-init-called
+# W0237 arguments-renamed
 # W0603 global-statement
 # W0613 unused-argument
+# W0707 raise-missing-from
+# W1404 implicit-str-concat
+# W1406 redundant-u-string-prefix
