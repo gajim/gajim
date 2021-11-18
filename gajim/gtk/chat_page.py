085cb43484b89ee3835737b8186707fea14a2bf7
wurstsalat <mailtrash@posteo.de>
2021-11-18 23:21:10 +0100

    ChatPage: Typing

--- gajim/gtk/chat_page.py
+++ gajim/gtk/chat_page.py
@@ -12,16 +12,18 @@
 # You should have received a copy of the GNU General Public License
 # along with Gajim. If not, see <http://www.gnu.org/licenses/>.
 
-
 from gi.repository import Gtk
 from gi.repository import GLib
 from gi.repository import Gio
 from gi.repository import GObject
 
+from nbxmpp import JID
+
 from gajim.common import app
 
 from .util import get_builder
 from .chat_filter import ChatFilter
+from .chat_list import ChatList
 from .chat_list_stack import ChatListStack
 from .chat_stack import ChatStack
 from .search_view import SearchView
@@ -77,7 +79,7 @@ class ChatPage(Gtk.Box):
         self._ui.paned.set_position(app.settings.get('chat_handle_position'))
         self._ui.paned.connect('button-release-event', self._on_button_release)
 
-        self._startup_finished = False
+        self._startup_finished: bool = False
 
         self._add_actions()
 
@@ -97,13 +99,13 @@ class ChatPage(Gtk.Box):
             act.connect('activate', func)
             app.window.add_action(act)
 
-    def set_startup_finished(self):
+    def set_startup_finished(self) -> None:
         self._startup_finished = True
 
-    def get_chat_list_stack(self):
+    def get_chat_list_stack(self) -> ChatListStack:
         return self._chat_list_stack
 
-    def get_chat_stack(self):
+    def get_chat_stack(self) -> ChatStack:
         return self._chat_stack
 
     @staticmethod
@@ -126,7 +128,9 @@ class ChatPage(Gtk.Box):
     def _on_edit_workspace_clicked(_button):
         app.window.lookup_action('edit-workspace').activate()
 
-    def _on_chat_selected(self, _chat_list_stack, workspace_id, account, jid):
+    def _on_chat_selected(self, _chat_list_stack: ChatListStack,
+                          workspace_id: str, account: str,
+                          jid: JID) -> None:
         self._chat_stack.show_chat(account, jid)
         self._search_view.set_context(account, jid)
         self.emit('chat-selected', workspace_id, account, jid)
@@ -164,39 +168,39 @@ class ChatPage(Gtk.Box):
         self._chat_stack.process_event(event)
         self._chat_list_stack.process_event(event)
 
-    def add_chat_list(self, workspace_id):
+    def add_chat_list(self, workspace_id: str) -> None:
         self._chat_list_stack.add_chat_list(workspace_id)
 
-    def show_workspace_chats(self, workspace_id):
+    def show_workspace_chats(self, workspace_id: str) -> None:
         self._chat_list_stack.show_chat_list(workspace_id)
         self._ui.filter_bar_toggle.set_active(False)
         self._chat_filter.reset()
 
-    def update_workspace(self, workspace_id):
+    def update_workspace(self, workspace_id: str) -> None:
         name = app.settings.get_workspace_setting(workspace_id, 'name')
         self._ui.workspace_label.set_text(name)
 
-    def remove_chat_list(self, workspace_id):
+    def remove_chat_list(self, workspace_id: str) -> None:
         self._chat_list_stack.remove_chat_list(workspace_id)
 
-    def chat_exists(self, account, jid):
+    def chat_exists(self, account: str, jid: JID) -> bool:
         return self._chat_list_stack.contains_chat(account, jid)
 
-    def select_chat(self, account, jid):
+    def select_chat(self, account: str, jid: JID) -> None:
         self._chat_list_stack.select_chat(account, jid)
 
-    def chat_exists_for_workspace(self, workspace_id, account, jid):
+    def chat_exists_for_workspace(self, workspace_id: str, account: str,
+                                  jid: JID) -> bool:
         return self._chat_list_stack.contains_chat(
             account, jid, workspace_id=workspace_id)
 
     def add_chat_for_workspace(self,
-                               workspace_id,
-                               account,
-                               jid,
-                               type_,
-                               pinned=False,
-                               select=False):
-
+                               workspace_id: str,
+                               account: str,
+                               jid: JID,
+                               type_: str,
+                               pinned: bool = False,
+                               select: bool = False) -> None:
         if self.chat_exists(account, jid):
             if select:
                 self._chat_list_stack.select_chat(account, jid)
@@ -220,7 +224,7 @@ class ChatPage(Gtk.Box):
                 self._chat_list_stack.select_chat(account, jid)
             self._chat_list_stack.store_open_chats(workspace_id)
 
-    def load_workspace_chats(self, workspace_id):
+    def load_workspace_chats(self, workspace_id: str) -> None:
         open_chats = app.settings.get_workspace_setting(workspace_id,
                                                         'open_chats')
 
@@ -235,26 +239,29 @@ class ChatPage(Gtk.Box):
                                         type_,
                                         pinned=pinned)
 
-    def is_chat_active(self, account, jid):
+    def is_chat_active(self, account: str, jid: JID) -> bool:
         return self._chat_list_stack.is_chat_active(account, jid)
 
     def _remove_chat(self, _action, param):
         account, jid = param.unpack()
+        jid = JID.from_string(jid)
+
         self.remove_chat(account, jid)
 
-    def remove_chat(self, account, jid):
+    def remove_chat(self, account: str, jid: JID) -> None:
         for workspace_id in app.settings.get_workspaces():
             if self.chat_exists_for_workspace(workspace_id, account, jid):
                 self._chat_list_stack.remove_chat(workspace_id, account, jid)
                 return
 
-    def _on_chat_removed(self, _chat_list, account, jid, type_):
+    def _on_chat_removed(self, _chat_list: ChatList, account: str, jid: JID,
+                         type_: str) -> None:
         self._chat_stack.remove_chat(account, jid)
         if type_ == 'groupchat':
             client = app.get_client(account)
             client.get_module('MUC').leave(jid)
 
-    def remove_chats_for_account(self, account):
+    def remove_chats_for_account(self, account: str) -> None:
         chat_list = self._chat_list_stack.get_current_chat_list()
         if chat_list is not None:
             chat_list.unselect_all()
@@ -274,7 +281,7 @@ class ChatPage(Gtk.Box):
     def get_controls(self, account=None):
         return self._chat_stack.get_controls(account)
 
-    def hide_search(self):
+    def hide_search(self) -> bool:
         if self._search_revealer.get_reveal_child():
             self._search_revealer.set_reveal_child(False)
             return True
