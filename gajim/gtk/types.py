8fbc98406b6e55fbb74b53fd3dd90a0253ac85f2
wurstsalat <mailtrash@posteo.de>
2021-11-23 21:24:26 +0100

    Types: Add ConversationRowType

--- gajim/gtk/types.py
+++ gajim/gtk/types.py
@@ -13,3 +13,34 @@
 # along with Gajim.  If not, see <http://www.gnu.org/licenses/>.
 
 # Types for typechecking
+
+from typing import Union
+
+from .conversation.rows.call import CallRow
+from .conversation.rows.command_output import CommandOutputRow
+from .conversation.rows.date import DateRow
+from .conversation.rows.file_transfer_jingle import FileTransferJingleRow
+from .conversation.rows.file_transfer import FileTransferRow
+from .conversation.rows.info import InfoMessage
+from .conversation.rows.message import MessageRow
+from .conversation.rows.muc_join_left import MUCJoinLeft
+from .conversation.rows.muc_subject import MUCSubject
+from .conversation.rows.muc_user_status import MUCUserStatus
+from .conversation.rows.read_marker import ReadMarkerRow
+from .conversation.rows.scroll_hint import ScrollHintRow
+
+
+ConversationRowType = Union[
+    CallRow,
+    CommandOutputRow,
+    DateRow,
+    FileTransferJingleRow,
+    FileTransferRow,
+    InfoMessage,
+    MessageRow,
+    MUCJoinLeft,
+    MUCSubject,
+    MUCUserStatus,
+    ReadMarkerRow,
+    ScrollHintRow
+]
