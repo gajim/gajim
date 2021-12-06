8f4e2de26a91dc6be8c7205d7756ffcf8a1f24f0
lovetox <philipp@hoerist.com>
2021-12-06 23:24:46 +0100

    App: Add type annotations

--- gajim/common/app.py
+++ gajim/common/app.py
@@ -26,10 +26,10 @@
 
 from __future__ import annotations
 
-from typing import Any  # pylint: disable=unused-import
-from typing import Dict  # pylint: disable=unused-import
-from typing import List  # pylint: disable=unused-import
-from typing import Optional  # pylint: disable=unused-import
+from typing import Any
+from typing import NamedTuple
+from typing import Union
+from typing import Optional
 from typing import cast
 
 import gc
@@ -38,42 +38,37 @@ import sys
 import logging
 import weakref
 import pprint
-from collections import namedtuple
 from collections import defaultdict
 
 import nbxmpp
 from nbxmpp.idlequeue import IdleQueue
 from gi.repository import Gdk
 from gi.repository import GLib
+from nbxmpp.protocol import JID
 
 import gajim
+from gajim.common import types
 from gajim.common import config as c_config
 from gajim.common import configpaths
 from gajim.common import ged as ged_module
 from gajim.common.i18n import LANG
 from gajim.common.const import Display
-from gajim.common.types import NetworkEventsControllerT  # pylint: disable=unused-import
-from gajim.common.types import InterfaceT  # pylint: disable=unused-import
-from gajim.common.types import ConnectionT  # pylint: disable=unused-import
-from gajim.common.types import LegacyContactsAPIT  # pylint: disable=unused-import
-from gajim.common.types import SettingsT  # pylint: disable=unused-import
-from gajim.common.types import CSSConfigT  # pylint: disable=unused-import
 
 
-interface = cast(InterfaceT, None)
+interface = cast(types.InterfaceT, None)
 thread_interface = lambda *args: None # Interface to run a thread and then a callback
 config = c_config.Config()
-settings = cast(SettingsT, None)
+settings = cast(types.SettingsT, None)
 version = gajim.__version__
-connections = {}  # type: Dict[str, ConnectionT]
-avatar_cache = {}  # type: Dict[str, Dict[str, Any]]
-bob_cache = {} # type: Dict[str, bytes]
+connections: dict[str, types.Client] = {}
+avatar_cache: dict[str, dict[str, Any]] = {}
+bob_cache: dict[str, bytes] = {}
 ipython_window = None
 app = None  # Gtk.Application
 window = None # MainWindow
 
 ged = ged_module.GlobalEventsDispatcher() # Global Events Dispatcher
-nec = cast(NetworkEventsControllerT, None)
+nec = cast(types.NetworkEventsControllerT, None)
 plugin_manager = None # Plugins Manager
 
 class Storage:
@@ -83,41 +78,41 @@ class Storage:
 
 storage = Storage()
 
-css_config = cast(CSSConfigT, None)
+css_config = cast(types.CSSConfigT, None)
 
-transport_type = {}  # type: Dict[str, str]
+transport_type: dict[str, str] = {}
 
-contacts = cast(LegacyContactsAPIT, None)
+contacts = cast(types.LegacyContactsAPIT, None)
 
 # tell if we are connected to the room or not
 # {acct: {room_jid: True}}
-gc_connected = {}  # type: Dict[str, Dict[str, bool]]
+gc_connected: dict[str, dict[str, bool]] = {}
 
 # dict of rooms that must be automatically configured
 # and for which we have a list of invities
 # {account: {room_jid: {'invities': []}}}
-automatic_rooms = {}  # type: Dict[str, Dict[str, Dict[str, List[str]]]]
+automatic_rooms: dict[str, dict[str, dict[str, list[str]]]] = {}
 
  # dict of groups, holds if they are expanded or not
-groups = {}  # type: Dict[str, Dict[str, Dict[str, bool]]]
+groups: dict[str, dict[str, dict[str, bool]]] = {}
 
 # list of contacts that has just signed in
-newly_added = {}  # type: Dict[str, List[str]]
+newly_added: dict[str, list[str]] = {}
 
 # list of contacts that has just signed out
-to_be_removed = {}  # type: Dict[str, List[str]]
+to_be_removed: dict[str, list[str]] = {}
 
 notification = None
 
 # list of our nick names in each account
-nicks = {}  # type: Dict[str, str]
+nicks: dict[str, str] = {}
 
 # should we block 'contact signed in' notifications for this account?
 # this is only for the first 30 seconds after we change our show
 # to something else than offline
 # can also contain account/transport_jid to block notifications for contacts
 # from this transport
-block_signed_in_notifications = {}  # type: Dict[str, bool]
+block_signed_in_notifications: dict[str, bool] = {}
 
 proxy65_manager = None
 
@@ -151,32 +146,39 @@ _dependencies = {
     'AYATANA_APPINDICATOR': False,
 }
 
-_tasks = defaultdict(list)  # type: Dict[int, List[Any]]
+_tasks: dict[int, list[Any]] = defaultdict(list)
 
-def print_version():
+
+class RecentGroupchat(NamedTuple):
+    room: str
+    server: str
+    nickname: str
+
+
+def print_version() -> None:
     log('gajim').info('Gajim Version: %s', gajim.__version__)
 
 
-def get_client(account):
+def get_client(account: str) -> types.Client:
     return connections[account]
 
 
-def is_installed(dependency):
+def is_installed(dependency: str) -> bool:
     if dependency == 'ZEROCONF':
         # Alias for checking zeroconf libs
         return _dependencies['AVAHI'] or _dependencies['PYBONJOUR']
     return _dependencies[dependency]
 
 
-def is_flatpak():
+def is_flatpak() -> bool:
     return gajim.IS_FLATPAK
 
 
-def is_portable():
+def is_portable() -> bool:
     return gajim.IS_PORTABLE
 
 
-def is_display(display):
+def is_display(display: Display) -> bool:
     # XWayland reports as Display X11, so try with env var
     is_wayland = os.environ.get('XDG_SESSION_TYPE') == 'wayland'
     if is_wayland and display == Display.WAYLAND:
@@ -188,10 +190,12 @@ def is_display(display):
         return False
     return default.__class__.__name__ == display.value
 
-def disable_dependency(dependency):
+
+def disable_dependency(dependency: str) -> None:
     _dependencies[dependency] = False
 
-def detect_dependencies():
+
+def detect_dependencies() -> None:
     import gi
 
     # ZEROCONF
@@ -310,7 +314,8 @@ def detect_dependencies():
 
     log('gajim').info('Used language: %s', LANG)
 
-def detect_desktop_env():
+
+def detect_desktop_env() -> Optional[str]:
     if sys.platform in ('win32', 'darwin'):
         return sys.platform
 
@@ -325,12 +330,12 @@ def detect_desktop_env():
 desktop_env = detect_desktop_env()
 
 
-def get_server_from_jid(jid):
+def get_server_from_jid(jid: str) -> str:
     pos = jid.find('@') + 1 # after @
     return jid[pos:]
 
 
-def get_room_and_nick_from_fjid(jid):
+def get_room_and_nick_from_fjid(jid: str) -> list[str]:
     # fake jid is the jid for a contact in a room
     # gaim@conference.jabber.no/nick/nick-continued
     # return ('gaim@conference.jabber.no', 'nick/nick-continued')
@@ -339,19 +344,22 @@ def get_room_and_nick_from_fjid(jid):
         l.append('')
     return l
 
-def get_jid_without_resource(jid):
+
+def get_jid_without_resource(jid: str) -> str:
     return jid.split('/')[0]
 
 
-def get_number_of_accounts():
+def get_number_of_accounts() -> int:
     """
     Return the number of ALL accounts
     """
     return len(connections.keys())
 
-def get_number_of_connected_accounts(accounts_list=None):
+
+def get_number_of_connected_accounts(
+        accounts_list: Optional[list[str]] = None) -> int:
     """
-    Returns the number of CONNECTED accounts. Uou can optionally pass an
+    Returns the number of connected accounts. You can optionally pass an
     accounts_list and if you do those will be checked, else all will be checked
     """
     connected_accounts = 0
@@ -364,18 +372,20 @@ def get_number_of_connected_accounts(accounts_list=None):
             connected_accounts = connected_accounts + 1
     return connected_accounts
 
-def get_available_clients():
-    clients = []
+
+def get_available_clients() -> list[types.Client]:
+    clients: list[types.Client] = []
     for client in connections.values():
         if client.state.is_available:
             clients.append(client)
     return clients
 
-def get_connected_accounts(exclude_local=False):
+
+def get_connected_accounts(exclude_local: bool = False) -> list[str]:
     """
     Returns a list of CONNECTED accounts
     """
-    account_list = []
+    account_list: list[str] = []
     for account in connections:
         if account == 'Local' and exclude_local:
             continue
@@ -383,7 +393,8 @@ def get_connected_accounts(exclude_local=False):
             account_list.append(account)
     return account_list
 
-def get_accounts_sorted():
+
+def get_accounts_sorted() -> list[str]:
     '''
     Get all accounts alphabetically sorted with Local first
     '''
@@ -394,13 +405,16 @@ def get_accounts_sorted():
         account_list.insert(0, 'Local')
     return account_list
 
-def get_enabled_accounts_with_labels(exclude_local=True, connected_only=False,
-                                     private_storage_only=False):
+
+def get_enabled_accounts_with_labels(
+        exclude_local: bool = True,
+        connected_only: bool = False,
+        private_storage_only: bool = False) -> list[str]:
     """
     Returns a list with [account, account_label] entries.
     Order by account_label
     """
-    accounts = []
+    accounts: list[str] = []
     for acc in connections:
         if exclude_local and account_is_zeroconf(acc):
             continue
@@ -414,42 +428,54 @@ def get_enabled_accounts_with_labels(exclude_local=True, connected_only=False,
     accounts.sort(key=lambda xs: str.lower(xs[1]))
     return accounts
 
-def get_account_label(account):
+
+def get_account_label(account: str) -> str:
     return settings.get_account_setting(account, 'account_label') or account
 
-def account_is_zeroconf(account):
+
+def account_is_zeroconf(account: str) -> bool:
     return connections[account].is_zeroconf
 
-def account_supports_private_storage(account):
+
+def account_supports_private_storage(account: str) -> bool:
     # If Delimiter module is not available we can assume
     # Private Storage is not available
     return connections[account].get_module('Delimiter').available
 
-def account_is_connected(account):
+
+def account_is_connected(account: str) -> bool:
     if account not in connections:
         return False
     return (connections[account].state.is_connected or
             connections[account].state.is_available)
 
-def account_is_available(account):
+
+def account_is_available(account: str) -> bool:
     if account not in connections:
         return False
     return connections[account].state.is_available
 
-def account_is_disconnected(account):
+
+def account_is_disconnected(account: str) -> bool:
     return not account_is_connected(account)
 
-def zeroconf_is_connected():
+
+def zeroconf_is_connected() -> bool:
     return account_is_connected(ZEROCONF_ACC_NAME) and \
             settings.get_account_setting(ZEROCONF_ACC_NAME, 'is_zeroconf')
 
-def in_groupchat(account, room_jid):
+
+def in_groupchat(account: str, room_jid: Union[str, JID]) -> bool:
     room_jid = str(room_jid)
     if room_jid not in gc_connected[account]:
         return False
     return gc_connected[account][room_jid]
 
-def get_transport_name_from_jid(jid, use_config_setting=True):
+
+def get_transport_name_from_jid(
+        jid: str,
+        use_config_setting: bool = True) -> Optional[str]:
+
     """
     Returns 'gg', 'irc' etc
 
@@ -483,13 +509,14 @@ def get_transport_name_from_jid(jid, use_config_setting=True):
         return 'facebook'
     return None
 
-def jid_is_transport(jid):
+def jid_is_transport(jid: str) -> bool:
     # if not '@' or '@' starts the jid then it is transport
     if jid.find('@') <= 0:
         return True
     return False
 
-def get_jid_from_account(account_name):
+
+def get_jid_from_account(account_name: str) -> str:
     """
     Return the jid we use in the given account
     """
@@ -498,12 +525,14 @@ def get_jid_from_account(account_name):
     jid = name + '@' + hostname
     return jid
 
-def get_account_from_jid(jid):
+
+def get_account_from_jid(jid: str) -> Optional[str]:
     for account in settings.get_accounts():
         if jid == get_jid_from_account(account):
             return account
 
-def get_hostname_from_account(account_name, use_srv=False):
+
+def get_hostname_from_account(account_name: str, use_srv: bool = False) -> str:
     """
     Returns hostname (if custom hostname is used, that is returned)
     """
@@ -513,33 +542,32 @@ def get_hostname_from_account(account_name, use_srv=False):
         return settings.get_account_setting(account_name, 'custom_host')
     return settings.get_account_setting(account_name, 'hostname')
 
-def get_notification_image_prefix(jid):
+
+def get_notification_image_prefix(jid: str) -> str:
     """
     Returns the prefix for the notification images
     """
     transport_name = get_transport_name_from_jid(jid)
-    if transport_name in ('icq', 'facebook'):
+    if transport_name in ['icq', 'facebook']:
         prefix = transport_name
     else:
         prefix = 'jabber'
     return prefix
 
 
-def get_recent_groupchats(account):
+def get_recent_groupchats(account: str) -> list[RecentGroupchat]:
     recent_groupchats = settings.get_account_setting(
         account, 'recent_groupchats').split()
 
-    RecentGroupchat = namedtuple('RecentGroupchat',
-                                 ['room', 'server', 'nickname'])
-
-    recent_list = []
+    recent_list: list[RecentGroupchat] = []
     for groupchat in recent_groupchats:
         jid = nbxmpp.JID.from_string(groupchat)
         recent = RecentGroupchat(jid.localpart, jid.domain, jid.resource)
         recent_list.append(recent)
     return recent_list
 
-def add_recent_groupchat(account, room_jid, nickname):
+
+def add_recent_groupchat(account: str, room_jid: str, nickname: str) -> None:
     recent = settings.get_account_setting(
         account, 'recent_groupchats').split()
     full_jid = room_jid + '/' + nickname
@@ -551,7 +579,8 @@ def add_recent_groupchat(account, room_jid, nickname):
     config_value = ' '.join(recent)
     settings.set_account_setting(account, 'recent_groupchats', config_value)
 
-def get_priority(account, show):
+
+def get_priority(account: str, show: str) -> int:
     """
     Return the priority an account must have
     """
@@ -569,22 +598,26 @@ def get_priority(account, show):
         prio = 127
     return prio
 
-def log(domain):
+
+def log(domain: str) -> logging.Logger:
     if domain != 'gajim':
         domain = 'gajim.%s' % domain
     return logging.getLogger(domain)
 
-def prefers_app_menu():
+
+def prefers_app_menu() -> bool:
     if sys.platform == 'darwin':
         return True
     if sys.platform == 'win32':
         return False
     return app.prefers_app_menu()
 
-def load_css_config():
+
+def load_css_config() -> None:
     global css_config
     from gajim.gui.css_config import CSSConfig
     css_config = CSSConfig()
+
 
 def set_debug_mode(enable: bool) -> None:
     debug_folder = configpaths.get('DEBUG')
@@ -595,10 +628,12 @@ def set_debug_mode(enable: bool) -> None:
         if debug_enabled.exists():
             debug_enabled.unlink()
 
+
 def get_debug_mode() -> bool:
     debug_folder = configpaths.get('DEBUG')
     debug_enabled = debug_folder / 'debug-enabled'
     return debug_enabled.exists()
+
 
 def get_stored_bob_data(algo_hash: str) -> Optional[bytes]:
     try:
@@ -616,7 +651,7 @@ def register_task(self, task):
     _tasks[id(self)].append(task)
 
 
-def remove_task(task, id_):
+def remove_task(task, id_) -> None:
     try:
         _tasks[id_].remove(task)
     except Exception:
@@ -626,7 +661,7 @@ def remove_task(task, id_):
             del _tasks[id_]
 
 
-def cancel_tasks(obj):
+def cancel_tasks(obj: Any) -> None:
     id_ = id(obj)
     if id_ not in _tasks:
         return
@@ -636,9 +671,10 @@ def cancel_tasks(obj):
         task.cancel()
 
 
-def check_finalize(obj):
+def check_finalize(obj: Any) -> None:
     if 'GAJIM_LEAK' not in os.environ:
         return
+
     name = obj.__class__.__name__
     logger = logging.getLogger('gajim.leak')
     finalizer = weakref.finalize(obj, logger.info, f'{name} has been finalized')
