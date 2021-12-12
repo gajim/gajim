83eb5f6cdfc30717c71b6deaa0a496dfa27738cc
wurstsalat <mailtrash@posteo.de>
2021-12-12 18:45:48 +0100

    GroupchatNick: Typing

--- gajim/gtk/groupchat_nick.py
+++ gajim/gtk/groupchat_nick.py
@@ -21,7 +21,7 @@ from .util import get_builder
 
 
 class NickChooser(Gtk.MenuButton):
-    def __init__(self):
+    def __init__(self) -> None:
         Gtk.MenuButton.__init__(self)
         self._ui = get_builder('groupchat_nick_chooser.ui')
         self.add(self._ui.button_content)
@@ -34,24 +34,24 @@ class NickChooser(Gtk.MenuButton):
         self._ui.entry.connect('changed', self._on_nickname_changed)
         self._ui.apply_button.connect('clicked', self._on_apply_nickname)
 
-    def get_text(self):
+    def get_text(self) -> str:
         return self._ui.entry.get_text()
 
-    def set_text(self, text):
+    def set_text(self, text: str) -> None:
         self._ui.entry.set_text(text)
         self._ui.label.set_text(text)
 
-    def _on_nickname_button_toggled(self, _widget):
+    def _on_nickname_button_toggled(self, _widget: Gtk.MenuButton) -> None:
         self._ui.entry.grab_focus()
 
-    def _on_nickname_changed(self, entry):
+    def _on_nickname_changed(self, entry: Gtk.Entry) -> None:
         try:
             validate_resourcepart(entry.get_text())
             self._ui.apply_button.set_sensitive(True)
         except InvalidJid:
             self._ui.apply_button.set_sensitive(False)
 
-    def _on_apply_nickname(self, _button):
+    def _on_apply_nickname(self, _button: Gtk.Button) -> None:
         nickname = self._ui.entry.get_text()
         self._ui.popover.popdown()
         self._ui.label.set_text(nickname)
