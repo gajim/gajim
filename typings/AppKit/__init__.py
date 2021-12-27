86fb79624202ba07e8854a3be3de40f9bf5be422
lovetox <philipp@hoerist.com>
2021-12-27 19:02:42 +0100

    Typing: Add AppKit stubs

---
+++ typings/AppKit/__init__.py
@@ -0,0 +1,16 @@
+
+from __future__ import annotations
+
+from typing import Any
+
+
+class NSLocale:
+    @classmethod
+    def currentLocale(cls) -> Any: ...
+
+
+class NSSound:
+    @classmethod
+    def alloc(cls) -> NSSound: ...
+    def initWithContentsOfFile_byReference_(self, url: str, byref: bool) -> None: ...
+    def play(self) -> None: ...
