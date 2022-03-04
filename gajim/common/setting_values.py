fc39aba7310395ef0e6ee135288b8b8494c7a67f
wurstsalat <mailtrash@posteo.de>
2022-03-04 22:59:36 +0100

    Settings values: Typing, add license header

--- gajim/common/setting_values.py
+++ gajim/common/setting_values.py
@@ -1,3 +1,16 @@
+# This file is part of Gajim.
+#
+# Gajim is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation; version 3 only.
+#
+# Gajim is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Gajim. If not, see <http://www.gnu.org/licenses/>.
 
 from typing import Literal
 from typing import TypedDict
@@ -504,7 +517,9 @@ WORKSPACE_SETTINGS: WorkspaceSettings = {
 }
 
 
-INITAL_WORKSPACE = {str(uuid.uuid4()): {}}
+INITAL_WORKSPACE: dict[str, dict[str, WorkspaceSettings]] = {
+    str(uuid.uuid4()): {}
+}
 
 
 PLUGIN_SETTINGS = {
