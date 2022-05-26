2191fd9d398f1dab8ffb29f2a82e6486159efaa7
wurstsalat <mailtrash@posteo.de>
2022-05-26 22:44:44 +0200

    chore: Receipts: Add type annotations

--- gajim/common/modules/receipts.py
+++ gajim/common/modules/receipts.py
@@ -14,18 +14,24 @@
 
 # XEP-0184: Message Delivery Receipts
 
+from __future__ import annotations
+
 import nbxmpp
 from nbxmpp.structs import StanzaHandler
 from nbxmpp.namespaces import Namespace
 from nbxmpp.modules.receipts import build_receipt
+from nbxmpp.protocol import JID
+from nbxmpp.protocol import Message
+from nbxmpp.structs import MessageProperties
 
 from gajim.common import app
+from gajim.common import types
 from gajim.common.events import ReceiptReceived
 from gajim.common.modules.base import BaseModule
 
 
 class Receipts(BaseModule):
-    def __init__(self, con):
+    def __init__(self, con: types.Client) -> None:
         BaseModule.__init__(self, con)
 
         self.handlers = [
@@ -35,10 +41,15 @@ class Receipts(BaseModule):
                           priority=46),
         ]
 
-    def _process_message_receipt(self, _con, stanza, properties):
+    def _process_message_receipt(self,
+                                 _con: types.xmppClient,
+                                 stanza: Message,
+                                 properties: MessageProperties
+                                 ) -> None:
         if not properties.is_receipt:
             return
 
+        assert properties.receipt is not None
         if properties.type.is_error:
             if properties.receipt.is_request:
                 return
@@ -94,9 +105,9 @@ class Receipts(BaseModule):
 
             raise nbxmpp.NodeProcessed
 
-    def _should_answer(self, properties):
+    def _should_answer(self, properties: MessageProperties) -> bool:
         if properties.is_muc_pm:
             return True
 
-        contact = self._get_contact(properties.jid.bare)
+        contact = self._get_contact(JID.from_string(properties.jid.bare))
         return contact.is_subscribed
