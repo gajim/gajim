ec7a7e227c1d7d67c126842a4916fde15c26b2cc
wurstsalat <mailtrash@posteo.de>
2022-05-27 00:11:43 +0200

    chore: PEP: Add type annotations

--- gajim/common/modules/pep.py
+++ gajim/common/modules/pep.py
@@ -14,17 +14,21 @@
 
 # XEP-0163: Personal Eventing Protocol
 
-from gajim.common.types import ConnectionT
+from __future__ import annotations
+
+from nbxmpp.structs import DiscoInfo
+
+from gajim.common import types
 from gajim.common.modules.base import BaseModule
 
 
 class PEP(BaseModule):
-    def __init__(self, con: ConnectionT) -> None:
+    def __init__(self, con: types.Client) -> None:
         BaseModule.__init__(self, con)
 
         self.supported = False
 
-    def pass_disco(self, info):
+    def pass_disco(self, info: DiscoInfo) -> None:
         for identity in info.identities:
             if identity.category == 'pubsub':
                 if identity.type == 'pep':
