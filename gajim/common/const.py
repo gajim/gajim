2242a9d8c55ede7387fc43a0e64906a0c9cb4ae6
lovetox <philipp@hoerist.com>
2022-07-24 10:48:33 +0200

    refactor: Make credit constants lists

--- gajim/common/const.py
+++ gajim/common/const.py
@@ -465,85 +465,88 @@ VOWELS = 'aeiou'
 CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
 
 
-THANKS = """\
-Alexander Futász
-Alexander V. Butenko
-Alexey Nezhdanov
-Alfredo Junix
-Anaël Verrier
-Anders Ström
-Andrew Sayman
-Anton Shmigirilov
-Christian Bjälevik
-Christophe Got
-Christoph Neuroth
-David Campey
-Dennis Craven
-Fabian Neumann
-Filippos Papadopoulos
-Francisco Alburquerque Parra (Membris Khan)
-Frederic Lory
-Fridtjof Bussefor
-Geobert Quach
-Guillaume Morin
-Gustavo J. A. M. Carneiro
-Ivo Anjo
-Josef Vybíral
-Juraj Michalek
-Kjell Braden
-Luis Peralta
-Michael Scherer
-Michele Campeotto
-Mike Albon
-Miguel Fonseca
-Norman Rasmussen
-Oscar Hellström
-Peter Saint-Andre
-Petr Menšík
-Sergey Kuleshov
-Stavros Giannouris
-Stian B. Barmen
-Thilo Molitor
-Thomas Klein-Hitpaß
-Urtzi Alfaro
-Witold Kieraś
-Yakov Bezrukov
-Yavor Doganov
-""".strip().split("\n")
+THANKS = [
+    'Alexander Futász',
+    'Alexander V. Butenko',
+    'Alexey Nezhdanov',
+    'Alfredo Junix',
+    'Anaël Verrier',
+    'Anders Ström',
+    'Andrew Sayman',
+    'Anton Shmigirilov',
+    'Christian Bjälevik',
+    'Christophe Got',
+    'Christoph Neuroth',
+    'David Campey',
+    'Dennis Craven',
+    'Fabian Neumann',
+    'Filippos Papadopoulos',
+    'Francisco Alburquerque Parra (Membris Khan)',
+    'Frederic Lory',
+    'Fridtjof Bussefor',
+    'Geobert Quach',
+    'Guillaume Morin',
+    'Gustavo J. A. M. Carneiro',
+    'Ivo Anjo',
+    'Josef Vybíral',
+    'Juraj Michalek',
+    'Kjell Braden',
+    'Luis Peralta',
+    'Michael Scherer',
+    'Michele Campeotto',
+    'Mike Albon',
+    'Miguel Fonseca',
+    'Norman Rasmussen',
+    'Oscar Hellström',
+    'Peter Saint-Andre',
+    'Petr Menšík',
+    'Sergey Kuleshov',
+    'Stavros Giannouris',
+    'Stian B. Barmen',
+    'Thilo Molitor',
+    'Thomas Klein-Hitpaß',
+    'Urtzi Alfaro',
+    'Witold Kieraś',
+    'Yakov Bezrukov',
+    'Yavor Doganov',
+]
 
-ARTISTS = """\
-Anders Ström
-Christophe Got
-Dennis Craven
-Dmitry Korzhevin
-Guillaume Morin
-Gvorcek Spajreh
-Josef Vybíral
-Membris Khan
-Rederick Asher
-Jakub Szypulka
-""".strip().split("\n")
 
-DEVS_CURRENT = """\
-Yann Leboulanger (asterix AT lagaule.org)
-Philipp Hörist (philipp AT hoerist.com)
-Daniel Brötzmann (wurstsalat AT posteo.de)
-André Apitzsch
-""".strip().split("\n")
+ARTISTS = [
+    'Anders Ström',
+    'Christophe Got',
+    'Dennis Craven',
+    'Dmitry Korzhevin',
+    'Guillaume Morin',
+    'Gvorcek Spajreh',
+    'Josef Vybíral',
+    'Membris Khan',
+    'Rederick Asher',
+    'Jakub Szypulka',
+]
 
-DEVS_PAST = """\
-Stefan Bethge (stefan AT lanpartei.de)
-Alexander Cherniuk (ts33kr AT gmail.com)
-Stephan Erb (steve-e AT h3c.de)
-Vincent Hanquez (tab AT snarc.org)
-Dimitur Kirov (dkirov AT gmail.com)
-Nikos Kouremenos (kourem AT gmail.com)
-Julien Pivotto (roidelapluie AT gmail.com)
-Jonathan Schleifer (js-gajim AT webkeks.org)
-Travis Shirk (travis AT pobox.com)
-Brendan Taylor (whateley AT gmail.com)
-Jean-Marie Traissard (jim AT lapin.org)
-""".strip().split("\n")
+
+DEVS_CURRENT = [
+    'Yann Leboulanger (asterix AT lagaule.org)',
+    'Philipp Hörist (philipp AT hoerist.com)',
+    'Daniel Brötzmann (wurstsalat AT posteo.de)',
+    'André Apitzsch',
+]
+
+
+DEVS_PAST = [
+    'Stefan Bethge (stefan AT lanpartei.de)',
+    'Alexander Cherniuk (ts33kr AT gmail.com)',
+    'Stephan Erb (steve-e AT h3c.de)',
+    'Vincent Hanquez (tab AT snarc.org)',
+    'Dimitur Kirov (dkirov AT gmail.com)',
+    'Nikos Kouremenos (kourem AT gmail.com)',
+    'Julien Pivotto (roidelapluie AT gmail.com)',
+    'Jonathan Schleifer (js-gajim AT webkeks.org)',
+    'Travis Shirk (travis AT pobox.com)',
+    'Brendan Taylor (whateley AT gmail.com)',
+    'Jean-Marie Traissard (jim AT lapin.org)',
+]
 
 
 RFC5646_LANGUAGE_TAGS = {
