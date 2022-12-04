9e9101a738b29a7e246ada1ba72b9dfaa207e385
Philipp Hörist <philipp@hoerist.com>
2022-12-04 23:50:58 +0100

    ci: Repace pylint/flake8 with ruff

--- scripts/pre-push-tests.sh
+++ scripts/pre-push-tests.sh
@@ -6,10 +6,10 @@ set -e
 GREEN='\033[0;32m'
 
 
-./scripts/dev/pylint-ci.sh gajim
-echo "${GREEN}pylint: \t OK"
-mypy gajim
-echo "${GREEN}mypy: \t\t OK"
-codespell -I codespell.conf --skip="*__pycache__*,gajim/data/icons,gajim/data/sounds,gajim/data/emoticons" gajim
+pyright
+echo "${GREEN}pyright: \t OK"
+ruff .
+echo "${GREEN}ruff: \t\t OK"
+codespell gajim
 echo "${GREEN}codespell: \t OK"
 echo "${GREEN}ready to push"
