0bdce8acbd60c56b358328f68a45f3a2d7e4502e
Philipp Hörist <philipp@hoerist.com>
2022-12-13 19:40:15 +0100

    other: Scripts: Execute pyright last in pre-push-tests
    It takes the most time, so execute it last

--- scripts/pre-push-tests.sh
+++ scripts/pre-push-tests.sh
@@ -6,10 +6,13 @@ set -e
 GREEN='\033[0;32m'
 
 
-pyright
-echo "${GREEN}pyright: \t OK"
 ruff .
 echo "${GREEN}ruff: \t\t OK"
+
 codespell gajim
 echo "${GREEN}codespell: \t OK"
+
+pyright
+echo "${GREEN}pyright: \t OK"
+
 echo "${GREEN}ready to push"
