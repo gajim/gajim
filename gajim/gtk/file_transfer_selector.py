gajim/gajim

commit 84ff274e345da79e075589d48026ec767729eb8e

feat: Allow sending files by copy-pasting them from file managers

gajim/gtk/file_transfer_selector.py
@@ -17,7 +17,6 @@ from __future__ import annotations
 from typing import Any
 from typing import cast
 from typing import Optional
-from typing import Union
 
 import logging
 from pathlib import Path
@@ -154,12 +153,13 @@ class FileTransferSelector(Gtk.Box):
 
         return catalog
 
-    def add_files(self, file_paths: Union[list[Path], list[str]]) -> None:
-        for path in file_paths:
-            if isinstance(path, str):
-                path = Path(path)
-
-            if path.is_dir():
+    def add_files(self, uris: list[str]) -> None:
+        for uri in uris:
+            path = get_file_path_from_dnd_dropped_uri(uri)
+            if path is None or not path.is_file():
+                self._add_warning_message(
+                    'Could not add %s'
+                    % (str(path) if path else uri))
                 continue
 
             size_warning = bool(self._method == 'httpupload' and
@@ -171,6 +171,9 @@ class FileTransferSelector(Gtk.Box):
             app.settings.set('last_send_dir', str(path))
 
         self._ui.listbox.show_all()
+
+    def _add_warning_message(self, msg: str) -> None:
+        log.warning(msg)  # TODO: replace with UI
 
     def _on_destroy(self, _widget: FileTransferSelector) -> None:
         app.check_finalize(self)
@@ -195,16 +198,7 @@ class FileTransferSelector(Gtk.Box):
             return
 
         if target_type == 80:
-            uris = selection.get_uris()
-            paths: list[Path] = []
-            for uri in uris:
-                path = get_file_path_from_dnd_dropped_uri(uri)
-                if path is None or not path.is_file():
-                    log.warning('Could not add file %s', str(path))
-                    continue
-                paths.append(path)
-
-            self.add_files(paths)
+            self.add_files(selection.get_uris())
 
     def _on_files_changed(self, _listbox: Gtk.ListBox, _row: FileRow) -> None:
         file_paths = self._get_file_paths()

gajim/gtk/filechoosers.py
@@ -51,14 +51,11 @@ class BaseFileChooser:
     def _on_response(self,
                      dialog: Union[Gtk.FileChooser, Gtk.FileChooserNative],
                      response: Gtk.ResponseType,
-                     accept_cb: Callable[..., Any],
+                     accept_cb: Callable[[list[str]], None],
                      cancel_cb: Optional[Callable[..., Any]]
                      ) -> None:
         if response == Gtk.ResponseType.ACCEPT:
-            if self.get_select_multiple():
-                accept_cb(dialog.get_filenames())
-            else:
-                accept_cb(dialog.get_filename())
+            accept_cb(dialog.get_uris())
 
         if response in (Gtk.ResponseType.CANCEL,
                         Gtk.ResponseType.DELETE_EVENT):
@@ -182,7 +179,7 @@ class GtkFileChooserDialog(Gtk.FileChooserDialog, BaseFileChooser):
     _preview_size = (200, 200)
 
     def __init__(self,
-                 accept_cb: Callable[..., Any],
+                 accept_cb: Callable[[list[str]], None],
                  cancel_cb: Optional[Callable[..., Any]] = None,
                  transient_for: Optional[Gtk.Window] = None,
                  path: Optional[str] = None,

gajim/gtk/chat_stack.py
@@ -574,11 +574,11 @@ class ChatStack(Gtk.Stack, EventHelper):
             if 'jingle' in name:
                 method = 'jingle'
 
-            file_paths = None
+            uris = None
             if param is not None:
-                file_paths = param.get_strv() or None
+                uris = param.get_strv() or None
             self._show_chat_function_page(
-                FunctionMode.SEND_FILE, method, file_paths)
+                FunctionMode.SEND_FILE, method, uris)
 
         elif action_name == 'add-to-roster':
             if (isinstance(contact, GroupchatParticipant) and

gajim/gtk/control.py
@@ -36,7 +36,6 @@ from gajim.common import events
 from gajim.common import ged
 from gajim.common import helpers
 from gajim.common import types
-from gajim.common.helpers import get_file_path_from_dnd_dropped_uri
 from gajim.common.helpers import to_user_string
 from gajim.common.i18n import _
 from gajim.common.ged import EventHelper
@@ -496,18 +495,8 @@ class ChatControl(EventHelper):
         self.reset_view()
 
     def drag_data_file_transfer(self, selection: Gtk.SelectionData) -> None:
-        uris = selection.get_uris()
-        paths: list[str] = []
-        for uri in uris:
-            path = get_file_path_from_dnd_dropped_uri(uri)
-            if path is None or not path.is_file():
-                self.add_info_message(
-                    _('You dropped something Gajim could not '
-                      'process: %s' % path))
-                continue
-            paths.append(str(path))
-
-        app.window.activate_action('send-file', GLib.Variant('as', paths))
+        app.window.activate_action('send-file',
+            GLib.Variant('as', selection.get_uris()))
 
     def get_our_nick(self) -> str:
         if isinstance(self.contact, GroupchatParticipant):

gajim/gtk/message_actions_box.py
@@ -17,7 +17,7 @@ from __future__ import annotations
 from typing import Any
 from typing import Optional
 
-import os
+from pathlib import Path
 import tempfile
 import logging
 import uuid
@@ -531,22 +531,30 @@ class MessageActionsBox(Gtk.Grid):
         return False
 
     def _on_paste_clipboard(self,
-                            _texview: MessageInputTextView
+                            texview: MessageInputTextView
                             ) -> None:
 
         clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+
+        uris = clipboard.wait_for_uris()
+        if uris:
+            app.window.activate_action('send-file', GLib.Variant('as', uris))
+            # prevent TextView from pasting the URIs as text:
+            texview.stop_emission_by_name('paste-clipboard')
+            return
+        log.info('No URIs pasted')
+
         image = clipboard.wait_for_image()
         if image is None:
             log.info('No image pasted')
             return
 
-        temp_dir = tempfile.gettempdir()
-        image_path = os.path.join(temp_dir, f'{uuid.uuid4()}.png')
+        temp_dir = Path(tempfile.gettempdir())
+        image_path = temp_dir / f'{uuid.uuid4()}.png'
 
-        image.savev(image_path, 'png', [], [])
-        if image is None:
+        if not image.savev(str(image_path), 'png', [], []):
             log.error('Could not process pasted image')
             return
 
         app.window.activate_action(
-            'send-file', GLib.Variant('as', [image_path]))
+            'send-file', GLib.Variant('as', [image_path.as_uri()]))

commit 2ba73b51ba80ecefc271c74dbd18c008c19913d2

cfix: File choosers: Fix type mismatches in accept callback
Fixes the regression introduced in 84ff274e345da79e075589d48026ec767729eb8e

gajim/gtk/file_transfer_selector.py
@@ -223,7 +223,9 @@ class FileTransferSelector(Gtk.Box):
         self.emit('changed', state)
 
     def _on_choose_files_clicked(self, _button: Gtk.Button) -> None:
-        FileChooserDialog(self.add_files,
+        def _on_chosen(paths: list[str]) -> None:
+            self.add_files([Path(p).as_uri() for p in paths])
+        FileChooserDialog(_on_chosen,
                           select_multiple=True,
                           transient_for=app.window,
                           path=app.settings.get('last_send_dir') or None)

gajim/gtk/filechoosers.py
@@ -36,6 +36,10 @@ from gajim.common.i18n import _
 
 from .const import Filter
 
+SingleCallbackT = Callable[[str], None]
+MultplCallbackT = Callable[[list[str]], None]
+AcceptCallbackT = Union[SingleCallbackT,MultplCallbackT]
+
 
 def _require_native() -> bool:
     if app.is_flatpak():
@@ -60,11 +64,16 @@ class BaseFileChooser:
     def _on_response(self,
                      dialog: Union[Gtk.FileChooser, Gtk.FileChooserNative],
                      response: Gtk.ResponseType,
-                     accept_cb: Callable[[list[str]], None],
+                     accept_cb: AcceptCallbackT,
                      cancel_cb: Optional[Callable[..., Any]]
                      ) -> None:
         if response == Gtk.ResponseType.ACCEPT:
-            accept_cb(dialog.get_uris())
+            if dialog.get_select_multiple():
+                cast(MultplCallbackT, accept_cb)(dialog.get_filenames())
+            else:
+                fn = dialog.get_filename()
+                assert fn is not None
+                cast(SingleCallbackT, accept_cb)(fn)
 
         if response in (Gtk.ResponseType.CANCEL,
                         Gtk.ResponseType.DELETE_EVENT):
@@ -120,7 +129,7 @@ class NativeFileChooserDialog(Gtk.FileChooserNative, BaseFileChooser):
     _action = Gtk.FileChooserAction.OPEN
 
     def __init__(self,
-                 accept_cb: Callable[..., Any],
+                 accept_cb: AcceptCallbackT,
                  cancel_cb: Optional[Callable[..., Any]] = None,
                  transient_for: Optional[Gtk.Window] = None,
                  path: Optional[str] = None,
@@ -180,7 +189,7 @@ class GtkFileChooserDialog(Gtk.FileChooserDialog, BaseFileChooser):
     _preview_size = (200, 200)
 
     def __init__(self,
-                 accept_cb: Callable[[list[str]], None],
+                 accept_cb: AcceptCallbackT,
                  cancel_cb: Optional[Callable[..., Any]] = None,
                  transient_for: Optional[Gtk.Window] = None,
                  path: Optional[str] = None,