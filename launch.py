3baa6b3376ace3979c345d8ceba490f3303e6aa4
Philipp Hörist <philipp@hoerist.com>
2023-05-27 17:28:13 +0200

    refactor: Simplify launch.py

--- launch.py
+++ launch.py
@@ -2,16 +2,14 @@
 
 import subprocess
 
-from gajim import gajim
+import gajim
+from gajim.gajim import main
 
 try:
-    p = subprocess.Popen('git rev-parse --short=12 HEAD', shell=True,
-                         stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-    node = p.communicate()[0]
-    if node:
-        import gajim as g
-        g.__version__ += '+' + node.decode('utf-8').strip()
+    res = subprocess.check_output(
+        ['git', 'rev-parse', '--short=12', 'HEAD'])  # noqa: S603, S607
+    gajim.__version__ += f'+{res.decode().strip()}'
 except Exception:
     pass
 
-gajim.main()
+main()
