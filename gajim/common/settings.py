0e0d6712040411087e36ae0a575023501cacbfa0
Philipp Hörist <philipp@hoerist.com>
2024-06-20 21:21:07 +0200

    cfix: Settings: Use invalid domain for fallback

--- gajim/common/settings.py
+++ gajim/common/settings.py
@@ -398,7 +398,7 @@ class Settings:
                     address = JID.from_string(f'{localpart}@{domain}')
                 except Exception as error:
                     log.warning('Unable to migrate address: %s', error)
-                    address = JID.from_string('unknown@unknown.com')
+                    address = JID.from_string('unknown@unknown.invalid')
 
                 settings['address'] = str(address)
 
