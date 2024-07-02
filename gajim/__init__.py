dde60cbc77b911272dc9509d0036f93a72ded584
Philipp Hörist <philipp@hoerist.com>
2024-07-02 20:11:41 +0200

    cfix: Windows: Catch error when checking for ms store version

--- gajim/__init__.py
+++ gajim/__init__.py
@@ -15,7 +15,7 @@ try:
     from winrt.windows.applicationmodel import AppInfo
     _current_appinfo = AppInfo.current()  # type: ignore
     has_ms_appinfo = True
-except (ImportError, OSError):
+except (AttributeError, ImportError, OSError):
     pass
 
 IS_MS_STORE = has_ms_appinfo
