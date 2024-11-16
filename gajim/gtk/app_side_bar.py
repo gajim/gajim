bebbe9a0d268d04111bb010ce83ce495a8c506ad
Philipp Hörist <philipp@hoerist.com>
2024-11-16 16:15:13 +0100

    refactor: AppSideBar: Port to Gtk4

--- gajim/gtk/app_side_bar.py
+++ gajim/gtk/app_side_bar.py
@@ -8,7 +8,6 @@ from gi.repository import Gtk
 from gajim.common import app
 
 from gajim.gtk.app_page import AppPage
-from gajim.gtk.util import load_icon_surface
 
 
 class AppSideBar(Gtk.ListBox):
@@ -16,24 +15,20 @@ class AppSideBar(Gtk.ListBox):
         Gtk.ListBox.__init__(self)
         self.set_valign(Gtk.Align.START)
         self.set_selection_mode(Gtk.SelectionMode.SINGLE)
-        self.get_style_context().add_class('workspace-sidebar')
+        self.add_css_class("workspace-sidebar")
 
-        self.connect('row-activated', self._on_app_row_activated)
+        self.connect("row-activated", self._on_app_row_activated)
 
-        app_page.connect('unread-count-changed', self._on_unread_count_changed)
+        app_page.connect("unread-count-changed", self._on_unread_count_changed)
 
         self._app_row = AppRow()
-        self.add(self._app_row)
-
-        self.show_all()
+        self.append(self._app_row)
 
         # Use idle_add to unselect listbox selection on startup
         GLib.idle_add(self.unselect_all)
 
     @staticmethod
-    def _on_app_row_activated(_listbox: Gtk.ListBox,
-                              _row: Gtk.ListBoxRow
-                              ) -> None:
+    def _on_app_row_activated(_listbox: Gtk.ListBox, _row: Gtk.ListBoxRow) -> None:
         app.window.show_app_page()
 
     def _on_unread_count_changed(self, _app_page: AppPage, count: int) -> None:
@@ -43,37 +38,34 @@ class AppSideBar(Gtk.ListBox):
 class AppRow(Gtk.ListBoxRow):
     def __init__(self) -> None:
         Gtk.ListBoxRow.__init__(self)
-        self.get_style_context().add_class('workspace-sidebar-item')
-        self.get_style_context().add_class('app-sidebar-item')
+        self.add_css_class("workspace-sidebar-item")
+        self.add_css_class("app-sidebar-item")
 
         self._unread_label = Gtk.Label()
-        self._unread_label.get_style_context().add_class(
-            'unread-counter')
-        self._unread_label.set_no_show_all(True)
+        self._unread_label.add_css_class("unread-counter")
+        self._unread_label.set_visible(False)
         self._unread_label.set_halign(Gtk.Align.END)
         self._unread_label.set_valign(Gtk.Align.START)
 
-        surface = load_icon_surface('gajim', 32)
-        image = Gtk.Image.new_from_surface(surface)
+        image = Gtk.Image(icon_name="gajim", pixel_size=32)
 
         selection_bar = Gtk.Box()
         selection_bar.set_size_request(6, -1)
-        selection_bar.get_style_context().add_class('selection-bar')
+        selection_bar.add_css_class("selection-bar")
 
         item_box = Gtk.Box()
-        item_box.add(selection_bar)
-        item_box.add(image)
+        item_box.append(selection_bar)
+        item_box.append(image)
 
         overlay = Gtk.Overlay()
-        overlay.add(item_box)
+        overlay.set_child(item_box)
         overlay.add_overlay(self._unread_label)
 
-        self.add(overlay)
-        self.show_all()
+        self.set_child(overlay)
 
     def set_unread_count(self, count: int) -> None:
         if count < 1000:
             self._unread_label.set_text(str(count))
         else:
-            self._unread_label.set_text('999+')
+            self._unread_label.set_text("999+")
         self._unread_label.set_visible(bool(count))
