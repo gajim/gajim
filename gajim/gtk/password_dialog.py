65303291e1db8fb16e3b0603870e5b87e8c2e3dd
Philipp Hörist <philipp@hoerist.com>
2024-11-16 16:19:50 +0100

    refactor: PasswordDialog: Port to Gtk4

--- gajim/gtk/password_dialog.py
+++ gajim/gtk/password_dialog.py
@@ -4,7 +4,6 @@
 
 import logging
 
-from gi.repository import Gdk
 from gi.repository import Gtk
 
 from gajim.common import app
@@ -13,55 +12,52 @@ from gajim.common.events import PasswordRequired
 from gajim.common.i18n import _
 
 from gajim.gtk.builder import get_builder
+from gajim.gtk.widgets import GajimAppWindow
 
-log = logging.getLogger('gajim.gtk.pass_dialog')
+log = logging.getLogger("gajim.gtk.password_dialog")
 
 
-class PasswordDialog(Gtk.ApplicationWindow):
+class PasswordDialog(GajimAppWindow):
     def __init__(self, event: PasswordRequired) -> None:
-        Gtk.ApplicationWindow.__init__(self)
-        self.set_application(app.app)
-        self.set_type_hint(Gdk.WindowTypeHint.DIALOG)
-        self.set_position(Gtk.WindowPosition.CENTER)
-        self.set_default_size(400, -1)
-        self.set_show_menubar(False)
-        self.set_name('PasswordDialog')
-        self.set_title(_('Password Required'))
+        GajimAppWindow.__init__(
+            self,
+            name="PasswordDialog",
+            title=_("Password Required"),
+            default_width=400,
+        )
 
-        self._ui = get_builder('password_dialog.ui')
-        self.add(self._ui.pass_box)
+        self._ui = get_builder("password_dialog.ui")
+        self.set_child(self._ui.pass_box)
+
+        self._connect(self._ui.cancel_button, "clicked", self._on_cancel)
+        self._connect(self._ui.ok_button, "clicked", self._on_ok)
 
         self.account = event.client.account
         self._client = app.get_client(event.client.account)
         self._event = event
 
-        self.connect('key-press-event', self._on_key_press)
-        self._ui.connect_signals(self)
-        self.show_all()
-        self._ui.ok_button.grab_default()
+        self.set_default_widget(self._ui.ok_button)
 
         self._process_event()
 
-    def _on_key_press(self, _widget: Gtk.Widget, event: Gdk.EventKey) -> None:
-        if event.keyval == Gdk.KEY_Escape:
-            self.destroy()
+    def _cleanup(self) -> None:
+        pass
 
     def _process_event(self) -> None:
         own_jid = self._client.get_own_jid().bare
         account_label = app.get_account_label(self.account)
 
-        self._ui.header.set_text(_('Password Required'))
+        self._ui.header.set_text(_("Password Required"))
         self._ui.message_label.set_text(
-            _('Please enter your password for\n'
-              '%(jid)s\n(Account: %(account)s)') % {
-                'jid': own_jid,
-                'account': account_label})
+            _("Please enter your password for\n" "%(jid)s\n(Account: %(account)s)")
+            % {"jid": own_jid, "account": account_label}
+        )
         self._ui.save_pass_checkbutton.show()
 
         is_keyring_available = passwords.is_keyring_available()
         self._ui.save_pass_checkbutton.set_sensitive(
-            not app.settings.get('use_keyring') or
-            is_keyring_available)
+            not app.settings.get("use_keyring") or is_keyring_available
+        )
         if not is_keyring_available:
             self._ui.keyring_hint.show()
 
@@ -69,11 +65,11 @@ class PasswordDialog(Gtk.ApplicationWindow):
         password = self._ui.pass_entry.get_text()
         savepass = self._ui.save_pass_checkbutton.get_active()
 
-        app.settings.set_account_setting(self.account, 'savepass', savepass)
+        app.settings.set_account_setting(self.account, "savepass", savepass)
         passwords.save_password(self.account, password)
 
         self._event.on_password()
-        self.destroy()
+        self.close()
 
     def _on_cancel(self, _button: Gtk.Button) -> None:
-        self.destroy()
+        self.close()
