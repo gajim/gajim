618afc3a472ff9de3672e048d6ee0f02e5ec6e94
Philipp Hörist <philipp@hoerist.com>
2024-11-16 16:22:28 +0100

    refactor: ApplyButtonBox: Port to Gtk4

--- gajim/gtk/apply_button_box.py
+++ gajim/gtk/apply_button_box.py
@@ -10,26 +10,34 @@ from collections.abc import Callable
 
 from gi.repository import Gtk
 
+from gajim.common import app
 
-class ApplyButtonBox(Gtk.Box):
-    def __init__(self,
-                 button_text: str,
-                 on_clicked: Callable[[Gtk.Button], Any]) -> None:
+from gajim.gtk.util import SignalManager
 
-        Gtk.Box.__init__(self,
-                         orientation=Gtk.Orientation.HORIZONTAL,
-                         spacing=6)
 
-        self._status_image = Gtk.Image(no_show_all=True)
-        self._spinner = Gtk.Spinner(no_show_all=True)
+class ApplyButtonBox(Gtk.Box, SignalManager):
+    def __init__(
+        self, button_text: str, on_clicked: Callable[[Gtk.Button], Any]
+    ) -> None:
+
+        Gtk.Box.__init__(self, orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        SignalManager.__init__(self)
+
+        self._status_image = Gtk.Image(visible=False)
+        self._spinner = Gtk.Spinner(visible=False)
         self._button = Gtk.Button(label=button_text, sensitive=False)
-        self._button.get_style_context().add_class('suggested-action')
-        self._button.connect('clicked', self._on_clicked)
-        self._button.connect('clicked', on_clicked)
+        self._button.add_css_class("suggested-action")
+        self._connect(self._button, "clicked", self._on_clicked)
+        self._connect(self._button, "clicked", on_clicked)
 
-        self.add(self._status_image)
-        self.add(self._spinner)
-        self.add(self._button)
+        self.append(self._button)
+        self.append(self._status_image)
+        self.append(self._spinner)
+
+    def do_unroot(self) -> None:
+        self._disconnect_all()
+        Gtk.Box.do_unroot(self)
+        app.check_finalize(self)
 
     def _on_clicked(self, button: Gtk.Button) -> None:
         button.set_sensitive(False)
@@ -44,23 +52,23 @@ class ApplyButtonBox(Gtk.Box):
     def set_success(self) -> None:
         self._spinner.stop()
         self._spinner.hide()
-        self._set_status_image('success')
+        self._set_status_image("success")
 
     def set_error(self, tooltip_text: str):
         self._spinner.stop()
         self._spinner.hide()
-        self._set_status_image('error', tooltip_text)
+        self._set_status_image("error", tooltip_text)
         self._button.set_sensitive(True)
 
-    def _set_status_image(self, state: str, tooltip_text: str = '') -> None:
-        icon_name = 'feather-check-symbolic'
-        css_class = 'success-color'
+    def _set_status_image(self, state: str, tooltip_text: str = "") -> None:
+        icon_name = "feather-check-symbolic"
+        css_class = "success-color"
 
-        if state == 'error':
-            icon_name = 'dialog-warning-symbolic'
-            css_class = 'warning-color'
+        if state == "error":
+            icon_name = "dialog-warning-symbolic"
+            css_class = "warning-color"
 
-        self._status_image.set_from_icon_name(icon_name, Gtk.IconSize.MENU)
-        self._status_image.get_style_context().add_class(css_class)
+        self._status_image.set_from_icon_name(icon_name)
+        self._status_image.add_css_class(css_class)
         self._status_image.set_tooltip_text(tooltip_text)
         self._status_image.show()
