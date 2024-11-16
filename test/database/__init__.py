eef28d7523fa057665bfce3b1c40a93ddfc632ff
wurstsalat <mailtrash@posteo.de>
2024-11-16 15:57:09 +0100

    chore: Tests: Add license headers

--- test/database/__init__.py
+++ test/database/__init__.py
@@ -1,22 +1,11 @@
-import gi
+# This file is part of Gajim.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from gajim.main import gi_require_versions
+
+gi_require_versions()
 
 from gajim.common import configpaths
 
 configpaths.init()
-
-
-def require_versions():
-    gi.require_versions(
-        {
-            'Gdk': '3.0',
-            'GLib': '2.0',
-            'Gio': '2.0',
-            'Gtk': '3.0',
-            'GtkSource': '4',
-            'GObject': '2.0',
-            'Pango': '1.0',
-        }
-    )
-
-
-require_versions()
--- test/database/util.py
+++ test/database/util.py
@@ -1,3 +1,7 @@
+# This file is part of Gajim.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from __future__ import annotations
 
 from datetime import datetime
