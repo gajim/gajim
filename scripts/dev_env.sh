1c1a1ccf5278e673b3538bb560a1ed242da9dd22
Philipp Hörist <philipp@hoerist.com>
2025-01-08 22:38:07 +0100

    other: Simplify dev env install

--- scripts/dev_env.sh
+++ scripts/dev_env.sh
@@ -2,7 +2,7 @@ echo -e "\e[32mCreate virtualenv\e[0m"
 python -m venv .venv
 source .venv/bin/activate
 echo -e "\e[32mInstall dependencies into virtualenv\e[0m"
-PYGOBJECT_STUB_CONFIG=Gtk4,Gdk4,GtkSource5 pip install -e .[dev]
+pip install -e .[dev]
 deactivate
 echo -e "\e[32mFinshed\e[0m"
 echo -e "\e[34mUse 'source .venv/bin/activate' to activate the virtualenv\e[0m"
