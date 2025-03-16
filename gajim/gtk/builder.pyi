0183fdfc9bf7a847b64e9c067759d0f805041302
wurstsalat <mailtrash@posteo.de>
2025-03-16 20:17:50 +0100

    cfix: Fix showing main menu

--- gajim/gtk/builder.pyi
+++ gajim/gtk/builder.pyi
@@ -579,6 +579,7 @@ class HistoryExportBuilder(Builder):
 
 class MainBuilder(Builder):
     main_view: Adw.ToolbarView
+    main_menu_bar: Gtk.PopoverMenuBar
     main_grid: Gtk.Grid
     left_grid: Gtk.Grid
     activity_box: Gtk.Box
