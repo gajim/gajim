7d2e899ef4397713cd6b33a0eea1efdfe625569d
wurstsalat <mailtrash@posteo.de>
2025-06-10 13:32:37 +0200

    ci: Windows: Reorganize build file
    - Summarize commands where appropriate
    - Order cleanup by type and alphabetically
    - Add more log output and comments

--- win/_base.sh
+++ win/_base.sh
@@ -5,6 +5,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 set -e
+
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd "${DIR}"
 
@@ -96,21 +97,22 @@ function build_compileall {
 }
 
 function install_pre_deps {
-    pacman -S --needed --noconfirm p7zip \
-        wget \
+    pacman -S --needed --noconfirm \
         intltool \
+        p7zip \
+        wget \
         mingw-w64-x86_64-nsis \
-        "${MINGW_PACKAGE_PREFIX}"-librsvg \
-        "${MINGW_PACKAGE_PREFIX}"-toolchain \
-        "${MINGW_PACKAGE_PREFIX}"-python
+        ${MINGW_PACKAGE_PREFIX}-librsvg \
+        ${MINGW_PACKAGE_PREFIX}-python \
+        ${MINGW_PACKAGE_PREFIX}-toolchain
 }
 
 function create_root {
     mkdir -p "${BUILD_ROOT}"
 
+    mkdir -p "${BUILD_ROOT}"/tmp
     mkdir -p "${BUILD_ROOT}"/var/lib/pacman
     mkdir -p "${BUILD_ROOT}"/var/log
-    mkdir -p "${BUILD_ROOT}"/tmp
 
     build_pacman -Syu
     build_pacman --noconfirm -S base
@@ -137,7 +139,6 @@ function post_install_deps {
 
     # Compile GLib schemas
     "${MINGW_ROOT}"/bin/glib-compile-schemas.exe "${MINGW_ROOT}"/share/glib-2.0/schemas
-
 }
 
 function install_gajim {
@@ -162,12 +163,6 @@ function install_gajim {
     curl -o "${BUILD_ROOT}"/speller_dicts.zip https://gajim.org/downloads/snap/win/build/speller_dicts.zip
     7z x -o"${MINGW_ROOT}"/share "${BUILD_ROOT}"/speller_dicts.zip
 
-    # Install themes
-    # rm -Rf "${MINGW_ROOT}"/etc
-    # rm -Rf "${MINGW_ROOT}"/share/themes
-    # cp -r win/etc "${MINGW_ROOT}"
-    # cp -r win/themes "${MINGW_ROOT}"/share
-
     # Install our own icons
     rm -Rf "${MINGW_ROOT}/share/icons/hicolor"
     cp -r gajim/data/icons/hicolor "${MINGW_ROOT}"/share/icons
@@ -175,87 +170,99 @@ function install_gajim {
     # Update icon cache
     "${MINGW_ROOT}"/bin/gtk4-update-icon-cache.exe --force \
         "${MINGW_ROOT}/share/icons/hicolor"
-
 }
 
 function cleanup_install {
+    # Cleanup build directory to minimize setup file size
 
-    build_pacman --noconfirm -Rdd "${MINGW_PACKAGE_PREFIX}"-shared-mime-info \
-        "${MINGW_PACKAGE_PREFIX}"-python-pip "${MINGW_PACKAGE_PREFIX}"-ncurses || true
-    build_pacman --noconfirm -Rdd "${MINGW_PACKAGE_PREFIX}"-tk || true
-    build_pacman --noconfirm -Rdd "${MINGW_PACKAGE_PREFIX}"-tcl || true
-    build_pacman --noconfirm -Rdd "${MINGW_PACKAGE_PREFIX}"-frei0r-plugins || true
-    build_pacman --noconfirm -Rdd "${MINGW_PACKAGE_PREFIX}"-abseil-cpp || true
+    build_pacman --noconfirm -Rdd \
+        ${MINGW_PACKAGE_PREFIX}-abseil-cpp \
+        ${MINGW_PACKAGE_PREFIX}-frei0r-plugins \
+        ${MINGW_PACKAGE_PREFIX}-ncurses \
+        ${MINGW_PACKAGE_PREFIX}-python-pip \
+        ${MINGW_PACKAGE_PREFIX}-shared-mime-info \
+        ${MINGW_PACKAGE_PREFIX}-tcl \
+        ${MINGW_PACKAGE_PREFIX}-tk \
+        || true
 
-    #delete translations we don't support
+    # Remove directories
+    rm -Rf "${MINGW_ROOT}"/include
+    rm -Rf "${MINGW_ROOT}"/lib/"${PYTHON_ID}".*/test
+    rm -Rf "${MINGW_ROOT}"/lib/"${PYTHON_ID}".*/dist-packages/Ogre
+    rm -Rf "${MINGW_ROOT}"/lib/cmake
+    rm -Rf "${MINGW_ROOT}"/lib/gettext
+    rm -Rf "${MINGW_ROOT}"/lib/installed-tests
+    rm -Rf "${MINGW_ROOT}"/lib/libthai
+    rm -Rf "${MINGW_ROOT}"/lib/mpg123
+    rm -Rf "${MINGW_ROOT}"/lib/OGRE
+    rm -Rf "${MINGW_ROOT}"/lib/p11-kit
+    rm -Rf "${MINGW_ROOT}"/lib/python2.*
+    rm -Rf "${MINGW_ROOT}"/lib/ruby
+    rm -Rf "${MINGW_ROOT}"/lib/tabset
+    rm -Rf "${MINGW_ROOT}"/lib/tcl8
+    rm -Rf "${MINGW_ROOT}"/lib/tcl8.6
+    rm -Rf "${MINGW_ROOT}"/lib/terminfo
+    rm -Rf "${MINGW_ROOT}"/libexec
+    rm -Rf "${MINGW_ROOT}"/share/aclocal
+    rm -Rf "${MINGW_ROOT}"/share/appdata
+    rm -Rf "${MINGW_ROOT}"/share/bash-completion
+    rm -Rf "${MINGW_ROOT}"/share/bullet
+    rm -Rf "${MINGW_ROOT}"/share/common-lisp
+    rm -Rf "${MINGW_ROOT}"/share/dbus-1
+    rm -Rf "${MINGW_ROOT}"/share/doc
+    rm -Rf "${MINGW_ROOT}"/share/emacs
+    rm -Rf "${MINGW_ROOT}"/share/ffmpeg
+    rm -Rf "${MINGW_ROOT}"/share/fontconfig
+    rm -Rf "${MINGW_ROOT}"/share/gdb
+    rm -Rf "${MINGW_ROOT}"/share/gettext
+    rm -Rf "${MINGW_ROOT}"/share/gettext-*
+    rm -Rf "${MINGW_ROOT}"/share/gir-1.0
+    rm -Rf "${MINGW_ROOT}"/share/gnome-shell
+    rm -Rf "${MINGW_ROOT}"/share/gtk-doc
+    rm -Rf "${MINGW_ROOT}"/share/info
+    rm -Rf "${MINGW_ROOT}"/share/libcaca
+    rm -Rf "${MINGW_ROOT}"/share/libtool
+    rm -Rf "${MINGW_ROOT}"/share/licenses
+    rm -Rf "${MINGW_ROOT}"/share/man
+    rm -Rf "${MINGW_ROOT}"/share/mime
+    rm -Rf "${MINGW_ROOT}"/share/nghttp2
+    rm -Rf "${MINGW_ROOT}"/share/OGRE
+    rm -Rf "${MINGW_ROOT}"/share/opencv4
+    rm -Rf "${MINGW_ROOT}"/share/pixmaps
+    rm -Rf "${MINGW_ROOT}"/share/readline
+    rm -Rf "${MINGW_ROOT}"/share/terminfo
+    rm -Rf "${MINGW_ROOT}"/share/tessdata
+    rm -Rf "${MINGW_ROOT}"/share/vala
+    rm -Rf "${MINGW_ROOT}"/share/vulkan
+    rm -Rf "${MINGW_ROOT}"/share/xml/docbook
+    rm -Rf "${MINGW_ROOT}"/share/zsh
+    rm -Rf "${MINGW_ROOT}"/var
+
+    find "${MINGW_ROOT}"/lib/"${PYTHON_ID}".* -type d -name "test*" \
+        -prune -exec rm -rf {} \;
+    find "${MINGW_ROOT}"/lib/"${PYTHON_ID}".* -type d -name "*_test*" \
+        -prune -exec rm -rf {} \;
+
+    # Remove translations we don't support
     for d in "${MINGW_ROOT}"/share/locale/*/LC_MESSAGES; do
         if [ ! -f "${d}"/gajim.mo ]; then
             rm -Rf "${d}"
         fi
     done
 
-    KEEP="gajim|gajim-debug|python3|gdbus|gspawn-win64-helper"
-
-    echo "deleting .exe files"
+    # Remove EXE files
+    echo "Removing .exe files"
+    RETAINED_EXES="gajim|gajim-debug|python3|gdbus|gspawn-win64-helper"
     find "${MINGW_ROOT}" -regextype "posix-extended" -name "*.exe" -and ! \
-        -iregex ".*/(${KEEP})\.exe" -exec rm -f {} \; -print
+        -iregex ".*/(${RETAINED_EXES})\.exe" -exec rm -f {} \; -print
 
-    rm -Rf "${MINGW_ROOT}"/libexec
-    rm -Rf "${MINGW_ROOT}"/include
-    rm -Rf "${MINGW_ROOT}"/var
-    rm -Rf "${MINGW_ROOT}"/share/gtk-doc
-    rm -Rf "${MINGW_ROOT}"/share/xml/docbook
-    rm -Rf "${MINGW_ROOT}"/share/zsh
-    rm -Rf "${MINGW_ROOT}"/share/pixmaps
-    rm -Rf "${MINGW_ROOT}"/share/gnome-shell
-    rm -Rf "${MINGW_ROOT}"/share/dbus-1
-    rm -Rf "${MINGW_ROOT}"/share/gir-1.0
-    rm -Rf "${MINGW_ROOT}"/share/doc
-    rm -Rf "${MINGW_ROOT}"/share/man
-    rm -Rf "${MINGW_ROOT}"/share/info
-    rm -Rf "${MINGW_ROOT}"/share/mime
-    rm -Rf "${MINGW_ROOT}"/share/gettext
-    rm -Rf "${MINGW_ROOT}"/share/libtool
-    rm -Rf "${MINGW_ROOT}"/share/licenses
-    rm -Rf "${MINGW_ROOT}"/share/appdata
-    rm -Rf "${MINGW_ROOT}"/share/aclocal
-    rm -Rf "${MINGW_ROOT}"/share/ffmpeg
-    rm -Rf "${MINGW_ROOT}"/share/vala
-    rm -Rf "${MINGW_ROOT}"/share/readline
-    rm -Rf "${MINGW_ROOT}"/share/bash-completion
-    rm -Rf "${MINGW_ROOT}"/share/common-lisp
-    rm -Rf "${MINGW_ROOT}"/share/emacs
-    rm -Rf "${MINGW_ROOT}"/share/gdb
-    rm -Rf "${MINGW_ROOT}"/share/libcaca
-    rm -Rf "${MINGW_ROOT}"/share/gettext
-    rm -Rf "${MINGW_ROOT}"/share/nghttp2
-    rm -Rf "${MINGW_ROOT}"/share/fontconfig
-    rm -Rf "${MINGW_ROOT}"/share/gettext-*
-    rm -Rf "${MINGW_ROOT}"/share/terminfo
-    rm -Rf "${MINGW_ROOT}"/share/bullet
-    rm -Rf "${MINGW_ROOT}"/share/OGRE
-    rm -Rf "${MINGW_ROOT}"/share/opencv4
-    rm -Rf "${MINGW_ROOT}"/share/tessdata
-    rm -Rf "${MINGW_ROOT}"/share/vulkan
-
-    find "${MINGW_ROOT}"/share/glib-2.0 -type f ! \
-        -name "*.compiled" -exec rm -f {} \;
-
-    rm -Rf "${MINGW_ROOT}"/lib/"${PYTHON_ID}".*/test
-    rm -Rf "${MINGW_ROOT}"/lib/"${PYTHON_ID}".*/dist-packages/Ogre
-    rm -Rf "${MINGW_ROOT}"/lib/cmake
-    rm -Rf "${MINGW_ROOT}"/lib/gettext
-    rm -Rf "${MINGW_ROOT}"/lib/mpg123
-    rm -Rf "${MINGW_ROOT}"/lib/p11-kit
-    rm -Rf "${MINGW_ROOT}"/lib/ruby
-    rm -Rf "${MINGW_ROOT}"/lib/tcl8
-    rm -Rf "${MINGW_ROOT}"/lib/tcl8.6
-    rm -Rf "${MINGW_ROOT}"/lib/terminfo
-    rm -Rf "${MINGW_ROOT}"/lib/installed-tests
-    rm -Rf "${MINGW_ROOT}"/lib/tabset
-    rm -Rf "${MINGW_ROOT}"/lib/libthai
-    rm -Rf "${MINGW_ROOT}"/lib/OGRE
-
+    # Remove individual files
+    rm -f "${MINGW_ROOT}"/bin/libBulletCollision.dll
+    rm -f "${MINGW_ROOT}"/bin/libBulletDynamics.dll
+    rm -f "${MINGW_ROOT}"/bin/libharfbuzz-icu-0.dll
+    rm -f "${MINGW_ROOT}"/bin/libopencv_*
+    rm -f "${MINGW_ROOT}"/bin/Ogre*
+    rm -f "${MINGW_ROOT}"/bin/xvidcore.dll
     rm -f "${MINGW_ROOT}"/lib/gstreamer-1.0/libgstvpx.dll
     rm -f "${MINGW_ROOT}"/lib/gstreamer-1.0/libgstdaala.dll
     rm -f "${MINGW_ROOT}"/lib/gstreamer-1.0/libgstdvdread.dll
@@ -269,64 +276,54 @@ function cleanup_install {
     rm -f "${MINGW_ROOT}"/lib/gstreamer-1.0/libgstcacasink.dll
     # Crashes often on Windows machines
     rm -f "${MINGW_ROOT}"/lib/gstreamer-1.0/libgstamfcodec.dll
-
-    rm -f "${MINGW_ROOT}"/bin/libBulletCollision.dll
-    rm -f "${MINGW_ROOT}"/bin/libBulletDynamics.dll
-    rm -f "${MINGW_ROOT}"/bin/libharfbuzz-icu-0.dll
-    rm -f "${MINGW_ROOT}"/bin/libopencv_*
-    rm -f "${MINGW_ROOT}"/bin/Ogre*
-    rm -f "${MINGW_ROOT}"/bin/xvidcore.dll
-
     rm -f "${MINGW_ROOT}"/lib/"${PYTHON_ID}".*/lib-dynload/_tkinter*
 
-    rm -Rf "${MINGW_ROOT}"/lib/python2.*
-
-    find "${MINGW_ROOT}" -name "*.a" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -name "*.whl" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -name "*.h" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -name "*.la" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -name "*.sh" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -name "*.jar" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -name "*.def" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -name "*.cmd" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -name "*.cmake" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -name "*.pc" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -name "*.desktop" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -name "*.manifest" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -name "*.pyo" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -name "*.am" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -name ".gitignore" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -name "pylint.rc" -exec rm -f {} \;
-
+    # Remove files by rules
     find "${MINGW_ROOT}"/bin -name "*-config" -exec rm -f {} \;
-    find "${MINGW_ROOT}"/bin -name "easy_install*" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -regex ".*/bin/[^.]+" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -regex ".*/bin/[^.]+\\.[0-9]+" -exec rm -f {} \;
-
-    find "${MINGW_ROOT}" -name "gettext-tools.mo" -exec rm -rf {} \;
-
-    find "${MINGW_ROOT}" -name "old_root.pem" -exec rm -rf {} \;
-    find "${MINGW_ROOT}" -name "weak.pem" -exec rm -rf {} \;
-
-    find "${MINGW_ROOT}"/lib/"${PYTHON_ID}".* -type d -name "test*" \
-        -prune -exec rm -rf {} \;
-
-    find "${MINGW_ROOT}"/lib/"${PYTHON_ID}".* -type d -name "*_test*" \
-        -prune -exec rm -rf {} \;
-
-    find "${MINGW_ROOT}"/bin -name "*.pyo" -exec rm -f {} \;
     find "${MINGW_ROOT}"/bin -name "*.pyc" -exec rm -f {} \;
+    find "${MINGW_ROOT}"/bin -name "*.pyo" -exec rm -f {} \;
+    find "${MINGW_ROOT}"/bin -name "easy_install*" -exec rm -f {} \;
     # This file is not able to compile because of syntax errors
     find "${MINGW_ROOT}"/bin -name "glib-gettextize-script.py" -exec rm -f {} \;
-    build_compileall -q "${MINGW_ROOT}"
-    find "${MINGW_ROOT}" -name "*.py" ! -name "*theme.py" -exec rm -f {} \;
-    find "${MINGW_ROOT}"/bin -name "*.pyc" -exec rm -f {} \;
-    find "${MINGW_ROOT}" -type d -name "__pycache__" -prune -exec rm -rf {} \;
 
+    find "${MINGW_ROOT}" -regex ".*/bin/[^.]+" -exec rm -f {} \; -print
+    find "${MINGW_ROOT}" -regex ".*/bin/[^.]+\\.[0-9]+" -exec rm -f {} \; -print
+
+    find "${MINGW_ROOT}"/share/glib-2.0 -type f ! -name "*.compiled" -exec rm -f {} \;
+
+    find "${MINGW_ROOT}" -name ".gitignore" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "gettext-tools.mo" -exec rm -rf {} \;
+    find "${MINGW_ROOT}" -name "old_root.pem" -exec rm -rf {} \;
+    find "${MINGW_ROOT}" -name "pylint.rc" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "weak.pem" -exec rm -rf {} \;
+
+    find "${MINGW_ROOT}" -name "*.a" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "*.am" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "*.cmake" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "*.cmd" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "*.def" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "*.desktop" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "*.h" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "*.jar" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "*.la" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "*.manifest" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "*.pc" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "*.pyo" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "*.sh" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "*.whl" -exec rm -f {} \;
+
+    # Byte-compile Python files and clean up afterwards
+    echo "Byte-compile Python files"
+    build_compileall -q "${MINGW_ROOT}"
+
+    find "${MINGW_ROOT}" -type d -name "__pycache__" -prune -exec rm -rf {} \;
+    find "${MINGW_ROOT}"/bin -name "*.pyc" -exec rm -f {} \;
+    find "${MINGW_ROOT}" -name "*.py" ! -name "*theme.py" -exec rm -f {} \;
+
+    echo "Run depcheck.py"
     build_python "${MISC}/depcheck.py"
 
     find "${MINGW_ROOT}" -type d -empty -delete
-
 }
 
 function makeappx {
@@ -340,7 +337,6 @@ function makepri {
 function build_exe_installer {
     MSYSTEM='MINGW64' /usr/bin/bash -lc "cd ${BUILD_ROOT} && makensis -NOCD -DVERSION=\"$QL_VERSION_DESC\" -DARCH=\"${MSYSTEM_CARCH}\" -DPREFIX=\"${MSYSTEM_PREFIX:1}\" ${MISC}/gajim.nsi"
     MSYSTEM='MINGW64' /usr/bin/bash -lc "cd ${BUILD_ROOT} && makensis -NOCD -DVERSION=\"$QL_VERSION_DESC\" -DARCH=\"${MSYSTEM_CARCH}\" -DPREFIX=\"${MSYSTEM_PREFIX:1}\" ${MISC}/gajim-portable.nsi"
-
 }
 
 function build_msix_installer {
