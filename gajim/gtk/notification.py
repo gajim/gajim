a7112f16d9637cc6f2ab9ab60509d498d7d9ebf2
Philipp Hörist <philipp@hoerist.com>
2025-07-03 19:03:16 +0200

    new: Notifications: Improve logging

--- gajim/gtk/notification.py
+++ gajim/gtk/notification.py
@@ -17,6 +17,7 @@
 from __future__ import annotations
 
 from typing import Any
+from typing import cast
 from typing import TYPE_CHECKING
 
 import hashlib
@@ -305,9 +306,10 @@ class Linux(NotificationBackend):
         self._notifications_supported: bool = False
         self._caps: list[str] = []
         self._detect_dbus_caps()
-        log.info("Detected notification capabilities: %s", self._caps)
 
     def _detect_dbus_caps(self) -> None:
+        log.info("Desktop Env: %s, Flatpak: %s", app.desktop_env, app.is_flatpak())
+
         if app.is_flatpak() or app.desktop_env == "gnome":
             # Gnome Desktop does not use org.freedesktop.Notifications.
             # It has its own API at org.gtk.Notifications, which is not an
@@ -319,17 +321,19 @@ class Linux(NotificationBackend):
             # https://flatpak.github.io/xdg-desktop-portal/docs
             self._caps = ["actions"]
             self._notifications_supported = True
+            log.info("Detected notification capabilities: %s", self._caps)
             return
 
         def on_proxy_ready(_source: Gio.DBusProxy, res: Gio.AsyncResult) -> None:
             try:
                 proxy = Gio.DBusProxy.new_finish(res)
-                self._caps = proxy.GetCapabilities()  # pyright: ignore
+                self._caps = cast(list[str], proxy.GetCapabilities())  # pyright: ignore
             except GLib.Error as error:
                 log.warning("Notifications D-Bus not available: %s", error)
             else:
                 self._notifications_supported = True
                 log.info("Notifications D-Bus connected")
+                log.info("Detected notification capabilities: %s", self._caps)
 
         log.info("Connecting to Notifications D-Bus")
         Gio.DBusProxy.new_for_bus(
