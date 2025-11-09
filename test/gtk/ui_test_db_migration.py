a6db66dfe929492c337434dcf0e2e4cda59ff330
wurstsalat <mailtrash@posteo.de>
2025-11-09 22:35:27 +0100

    cfix: DBMigrationTest: Fix changing state after progress has been enabled

--- test/gtk/ui_test_db_migration.py
+++ test/gtk/ui_test_db_migration.py
@@ -18,46 +18,59 @@ from gajim.gtk.db_migration import DBMigration
 from . import util
 
 
-def _on_progress_clicked(_button: Gtk.Button) -> None:
-    count = 100000
-    for i in range(100000):
-        time.sleep(0.0000001)
-        if i % 1000 == 0:
-            app.ged.raise_event(DBMigrationProgress(count=count, progress=i))
+class DBMigrationTest:
+    def __init__(self) -> None:
+        self._progressing = False
 
+        window = DBMigration()
+        box = cast(Gtk.Box, util.get_content_widget(window))
+        box.set_orientation(Gtk.Orientation.VERTICAL)
 
-def _on_finish_clicked(_button: Gtk.Button) -> None:
-    app.ged.raise_event(DBMigrationFinished())
+        button_box = Gtk.Box(spacing=12, halign=Gtk.Align.CENTER)
+        box.append(button_box)
 
+        progress_button = Gtk.Button(label="Progress")
+        progress_button.connect("clicked", self._on_progress_clicked)
+        button_box.append(progress_button)
 
-def _on_error_clicked(_button: Gtk.Button) -> None:
-    try:
-        test = 1 / 0  # type: ignore # noqa: F841
-    except ZeroDivisionError as e:
-        app.ged.raise_event(DBMigrationError(exception=e))
+        finish_button = Gtk.Button(label="Finish")
+        finish_button.connect("clicked", self._on_finish_clicked)
+        button_box.append(finish_button)
+
+        error_button = Gtk.Button(label="Error")
+        error_button.connect("clicked", self._on_error_clicked)
+        button_box.append(error_button)
+
+        window.show()
+
+    def _on_progress_clicked(self, _button: Gtk.Button) -> None:
+        self._progressing = True
+
+        count = 100000
+        for i in range(100000):
+            if not self._progressing:
+                break
+
+            time.sleep(0.0000001)
+            if i % 1000 == 0:
+                app.ged.raise_event(DBMigrationProgress(count=count, progress=i))
+
+    def _on_finish_clicked(self, _button: Gtk.Button) -> None:
+        self._progressing = False
+
+        app.ged.raise_event(DBMigrationFinished())
+
+    def _on_error_clicked(self, _button: Gtk.Button) -> None:
+        self._progressing = False
+
+        try:
+            test = 1 / 0  # type: ignore # noqa: F841
+        except ZeroDivisionError as e:
+            app.ged.raise_event(DBMigrationError(exception=e))
 
 
 util.init_settings()
 
-window = DBMigration()
-box = cast(Gtk.Box, util.get_content_widget(window))
-box.set_orientation(Gtk.Orientation.VERTICAL)
-
-button_box = Gtk.Box(spacing=12, halign=Gtk.Align.CENTER)
-box.append(button_box)
-
-progress_button = Gtk.Button(label="Progress")
-progress_button.connect("clicked", _on_progress_clicked)
-button_box.append(progress_button)
-
-finish_button = Gtk.Button(label="Finish")
-finish_button.connect("clicked", _on_finish_clicked)
-button_box.append(finish_button)
-
-error_button = Gtk.Button(label="Error")
-error_button.connect("clicked", _on_error_clicked)
-button_box.append(error_button)
-
-window.show()
+DBMigrationTest()
 
 util.run_app()
