7699841e0b8a37ade6afcc1bfd5f0de998c003ae
Philipp Hörist <philipp@hoerist.com>
2026-01-05 17:09:01 +0100

    fix: MessageInput: Fix case where emojis were missing from the emoji chooser
    It seems that with a too low priority Gtk is not able to load the emojis if we change the font size
    
    Fixes #12390

--- gajim/common/const.py
+++ gajim/common/const.py
@@ -109,7 +109,7 @@ class StyleAttr(Enum):
 
 @unique
 class CSSPriority(IntEnum):
-    PRE_THEME = 199
+    PRE_THEME = 599
     APPLICATION = 600
     APPLICATION_DARK = 601
     DEFAULT_THEME = 610
