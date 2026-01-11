e596d6febcdf5a95dc6450113adbdb31c81ff25c
Philipp Hörist <philipp@hoerist.com>
2026-01-11 19:04:28 +0100

    cfix: DBMigration: Remove present() call after finish
    We set the window as transient to the main window as soon as it is available,
    this raises the window automatically to the top

--- gajim/gtk/db_migration.py
+++ gajim/gtk/db_migration.py
@@ -104,7 +104,6 @@ class DBMigration(Adw.ApplicationWindow, EventHelper, SignalManager):
 
     def _on_finished(self, _event: DBMigrationFinished) -> None:
         self._ui.stack.set_visible_child_name("success-page")
-        GLib.timeout_add_seconds(2, self.present)
 
     def _on_error(self, event: DBMigrationError) -> None:
         trace = StringIO()
