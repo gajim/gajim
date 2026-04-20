831bd378296942d70e1b4228b6073c23568b159f
cal0pteryx <cal0pteryx@posteo.de>
2026-04-20 22:44:22 +0200

    imprv: OpenPGP: Improve setup wizard welcome page

--- gajim/gtk/openpgp_wizard.py
+++ gajim/gtk/openpgp_wizard.py
@@ -9,6 +9,7 @@ from typing import overload
 
 import logging
 
+from gi.repository import GObject
 from gi.repository import Gtk
 
 from gajim.common import app
@@ -39,7 +40,7 @@ class OpenPGPWizard(Assistant):
         self.add_button(
             "import", _("Import Key"), complete=True, css_class="suggested-action"
         )
-        self.add_button("generate", _("Generate Key"), css_class="suggested-action")
+        self.add_button("generate", _("Create New Key"), css_class="suggested-action")
         self.add_button(
             "restore", _("Restore RestoreBackupPage"), css_class="suggested-action"
         )
@@ -55,6 +56,9 @@ class OpenPGPWizard(Assistant):
         )
 
         self.add_default_page("progress")
+
+        welcome_page = self.get_page("welcome")
+        self._connect(welcome_page, "clicked", self._on_button_clicked)
 
         self._connect(self, "button-clicked", self._on_assistant_button_clicked)
         self._connect(self, "page-changed", self._on_page_changed)
@@ -79,35 +83,19 @@ class OpenPGPWizard(Assistant):
     def _on_assistant_button_clicked(
         self, _assistant: Assistant, button_name: str
     ) -> None:
-        page = self.get_current_page()
         match button_name:
             case "import":
-                if page == "import":
-                    key_str = self.get_page(page).get_key_string()
-                    try:
-                        self._client.get_module("OpenPGP").import_key(key_str)
-                    except Exception as error:
-                        self._show_error_page(
-                            _("Import Error"), _("Import Error"), str(error)
-                        )
-
-                else:
-                    self.show_page("import", Gtk.StackTransitionType.SLIDE_LEFT)
+                key_str = self.get_page("import").get_key_string()
+                try:
+                    self._client.get_module("OpenPGP").import_key(key_str)
+                except Exception as error:
+                    self._show_error_page(
+                        _("Import Error"), _("Import Error"), str(error)
+                    )
 
             case "restore":
+                # TODO
                 self.show_page("progress", Gtk.StackTransitionType.SLIDE_LEFT)
-
-            case "generate":
-                try:
-                    self._client.get_module("OpenPGP").generate_key()
-                except Exception as error:
-                    self._show_error_page(_("Error"), _("Error"), str(error))
-                else:
-                    self.get_page("success").set_title(_("Setup Complete"))
-                    self.get_page("success").set_text(
-                        _("You can now chat encrypted using OpenPGP.")
-                    )
-                    self.show_page("success", Gtk.StackTransitionType.SLIDE_LEFT)
 
             case "back":
                 self.show_page("welcome", Gtk.StackTransitionType.SLIDE_RIGHT)
@@ -118,9 +106,32 @@ class OpenPGPWizard(Assistant):
             case _:
                 raise ValueError
 
+    def _on_button_clicked(self, _page: Gtk.Widget, button_name: str) -> None:
+        if button_name == "import":
+            self.show_page("import", Gtk.StackTransitionType.SLIDE_LEFT)
+
+        elif button_name == "restore_backup":
+            # TODO
+            self.show_page("progress", Gtk.StackTransitionType.SLIDE_LEFT)
+
+        elif button_name == "generate":
+            try:
+                self._client.get_module("OpenPGP").generate_key()
+            except Exception as error:
+                self._show_error_page(_("Error"), _("Error"), str(error))
+            else:
+                self._show_success_page()
+
     def _on_page_changed(self, _assistant: Assistant, page_name: str) -> None:
         if page_name == "import":
             self.get_page("import").clear()
+
+    def _show_success_page(self) -> None:
+        self.get_page("success").set_title(_("Setup Complete"))
+        self.get_page("success").set_text(
+            _("Your OpenPGP key has been created. Your chat is now encrypted.")
+        )
+        self.show_page("success", Gtk.StackTransitionType.SLIDE_LEFT)
 
     def _show_error_page(self, title: str, heading: str, text: str) -> None:
         self.get_page("error").set_title(title)
@@ -132,13 +143,30 @@ class OpenPGPWizard(Assistant):
 @Gtk.Template(string=get_ui_string("openpgp/welcome.ui"))
 class WelcomePage(AssistantPage):
     __gtype_name__ = "OpenPGPWizardWelcome"
+    __gsignals__ = {
+        "clicked": (GObject.SignalFlags.RUN_LAST, None, (str,)),
+    }
+
+    _import_button: Gtk.Button = Gtk.Template.Child()
+    _restore_backup_button: Gtk.Button = Gtk.Template.Child()
+    _generate_button: Gtk.Button = Gtk.Template.Child()
 
     def __init__(self) -> None:
         AssistantPage.__init__(self)
-        self.title = _("Setup OpenPGP")
+        self.title = _("Setup OpenPGP Encryption")
 
-    def get_visible_buttons(self) -> list[str]:
-        return ["generate", "import", "restore"]
+        self._connect(self._import_button, "clicked", self._on_import)
+        self._connect(self._restore_backup_button, "clicked", self._on_restore_backup)
+        self._connect(self._generate_button, "clicked", self._on_generate)
+
+    def _on_import(self, _button: Gtk.Button) -> None:
+        self.emit("clicked", "import")
+
+    def _on_restore_backup(self, _button: Gtk.Button) -> None:
+        self.emit("clicked", "restore_backup")
+
+    def _on_generate(self, _button: Gtk.Button) -> None:
+        self.emit("clicked", "generate")
 
 
 @Gtk.Template(string=get_ui_string("openpgp/import.ui"))
