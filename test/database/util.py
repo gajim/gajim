7f936954bb200cd7958c7e91a6d62847069f27b4
Philipp Hörist <philipp@hoerist.com>
2026-05-17 18:32:59 +0200

    new: Tests: Add util method for enabling logging

--- test/database/util.py
+++ test/database/util.py
@@ -4,9 +4,18 @@
 
 from __future__ import annotations
 
+import logging
+import sys
 from datetime import datetime
 from datetime import UTC
 
 
 def mk_utc_dt(timestamp: int) -> datetime:
     return datetime.fromtimestamp(timestamp, UTC)
+
+
+def attach_debug_logger(log_module: str) -> None:
+    logger = logging.getLogger(log_module)
+    logger.setLevel(logging.DEBUG)
+    stream_handler = logging.StreamHandler(sys.stdout)
+    logger.addHandler(stream_handler)
