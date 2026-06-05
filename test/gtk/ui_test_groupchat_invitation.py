0e834a22cc7ec661891bc403a8d3af861b223785
Philipp Hörist <philipp@hoerist.com>
2026-06-05 19:23:29 +0200

    refactor: Tests: Port group chat invitation to GajimTestApplication

--- test/gtk/ui_test_groupchat_invitation.py
+++ test/gtk/ui_test_groupchat_invitation.py
@@ -1,42 +1,21 @@
 # This file is part of Gajim.
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-from typing import Any
 
-import random
-from unittest.mock import MagicMock
-
-from gi.repository import Gdk
-from gi.repository import GLib
 from gi.repository import Gtk
 from nbxmpp import JID
 from nbxmpp.const import InviteType
 from nbxmpp.modules.discovery import parse_disco_info
 from nbxmpp.protocol import Iq
-from nbxmpp.structs import BookmarkData
 
 from gajim.common import app
-from gajim.common.const import AvatarSize
-from gajim.common.const import CSSPriority
 from gajim.common.events import MucInvitation
-from gajim.common.modules.contacts import BareContact
-from gajim.common.modules.contacts import GroupchatContact
+from test.gtk.application import GajimTestApplication
 
-from gajim.gtk.avatar import convert_surface_to_texture
-from gajim.gtk.avatar import generate_default_avatar
 from gajim.gtk.groupchat_invitation import GroupChatInvitation
 from gajim.gtk.window import GajimAppWindow
 
-from . import util
-
 MUC_JID = JID.from_string("group@conference.example.org")
-INVITER_JID = JID.from_string("contact@example.org")
-
-accounts: list[list[str]] = []
-for i in range(2):
-    accounts.append(
-        [f"testacc{i}", f"Account {i}"],
-    )
 
 stanza = Iq(
     node=f"""
@@ -119,6 +98,8 @@ subject = (
 
 disco_info = parse_disco_info(stanza)
 
+ACCOUNT = "testacc0"
+
 
 class TestGroupchatInvitation(GajimAppWindow):
     def __init__(self):
@@ -132,125 +113,41 @@ class TestGroupchatInvitation(GajimAppWindow):
             header_bar=True,
         )
 
-        self._main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=18)
-        self._main_box.set_valign(Gtk.Align.FILL)
+        main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=18)
+        main_box.set_valign(Gtk.Align.FILL)
 
         event = MucInvitation(
-            account=accounts[0][0],
+            account=ACCOUNT,
             info=disco_info,
             muc=MUC_JID,
-            from_=INVITER_JID,
+            from_=JID.from_string("contact@example.org"),
             reason="Reason",
             password=None,
             type=InviteType.MEDIATED,
             continued=False,
             thread=None,
         )
-        invitation_box = GroupChatInvitation(accounts[0][0], event)
+        invitation_box = GroupChatInvitation(ACCOUNT, event)
         invitation_box.set_vexpand(True)
 
-        self._main_box.append(invitation_box)
+        main_box.append(invitation_box)
 
-        self.set_child(self._main_box)
+        self.set_child(main_box)
 
     def _cleanup(self) -> None:
         pass
 
 
-class TestContactsModule:
-    def __init__(self) -> None:
-        pass
-
-    @staticmethod
-    def get_contact(
-        jid: str | JID, groupchat: bool = False
-    ) -> BareContact | GroupchatContact:
-        if jid == MUC_JID:
-            contact = MagicMock(spec_set=GroupchatContact)
-            contact.jid = MUC_JID
-            contact.name = "Group Chat Name"
-            contact.is_groupchat = True
-            avatar = convert_surface_to_texture(
-                generate_default_avatar("G", (0.2, 0.7, 0.4), AvatarSize.GROUP_INFO, 1)
-            )
-        else:
-            contact = MagicMock(spec_set=BareContact)
-            contact.jid = INVITER_JID
-            contact.name = "Inviter Name"
-            contact.is_groupchat = False
-            avatar = convert_surface_to_texture(
-                generate_default_avatar("I", (0.2, 0.1, 0.7), AvatarSize.SMALL, 1)
-            )
-
-        contact.get_avatar = MagicMock(return_value=avatar)
-        contact.account = accounts[0]
-
-        return contact
+def init(app: GajimTestApplication) -> None:
+    app.add_account("testacc0")
+    app.add_account("testacc1")
+    window = TestGroupchatInvitation()
+    window.present()
 
 
-class TestBookmarksModule:
-    def __init__(self) -> None:
-        pass
-
-    def get_bookmark(self, jid: str | JID) -> BookmarkData:
-        return BookmarkData(jid=MUC_JID, name="Group Chat Name", nick="Nickname")
-
-    @staticmethod
-    def get_name_from_bookmark(jid: JID | str) -> str:
-        return "Group Chat Name"
-
-
-class TestClient:
-    def __init__(self, account: str) -> None:
-        self._account = account
-
-        self._contacts_module = TestContactsModule()
-        self._bookmarks_module = TestBookmarksModule()
-
-    def get_module(self, module: str) -> Any:
-        if module == "Contacts":
-            return self._contacts_module
-        if module == "Bookmarks":
-            return self._bookmarks_module
-        return MagicMock()
-
-
-def _get_dynamic_class(account: str) -> str:
-    color = app.settings.get_account_setting(account, "account_color")
-    css_class = f"gajim_class_{account}"
-    css = f".{css_class} {{ background-color: {color}; }}\n"
-    dynamic_provider = Gtk.CssProvider()
-    dynamic_provider.load_from_bytes(GLib.Bytes.new(css.encode("utf-8")))
-    display = Gdk.Display.get_default()
-    assert display is not None
-    Gtk.StyleContext.add_provider_for_display(
-        display, dynamic_provider, CSSPriority.APPLICATION
-    )
-    return css_class
-
-
-app.css_config = MagicMock()
-app.css_config.get_value = MagicMock(return_value="rgb(100, 100, 255)")
-app.css_config.get_dynamic_class = MagicMock(side_effect=_get_dynamic_class)
-
-app.get_enabled_accounts_with_labels = MagicMock(return_value=[1, 2])
-
-test_client = TestClient(accounts[0][0])
-app.get_client = MagicMock(return_value=test_client)
-
-util.init_settings()
-
-for account in accounts:
-    app.settings.add_account(account[0])
-    app.settings.set_account_setting(
-        account[0],
-        "account_color",
-        f"rgb({random.randrange(255)}, {random.randrange(255)}, {random.randrange(255)})",  # noqa: E501
-    )
-    app.settings.set_account_setting(account[0], "account_label", account[1])
-
-
-window = TestGroupchatInvitation()
-window.show()
-
-util.run_app()
+if __name__ == "__main__":
+    # Protect the entry point of the application because we use
+    # the multiprocessing module with "spawn"
+    app = GajimTestApplication()
+    app.connect("activate", init)
+    app.run(None)
